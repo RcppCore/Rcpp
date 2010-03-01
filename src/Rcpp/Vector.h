f69a68f388923c1b22f7fc388a2e175b59104802
Romain François <romain@r-enthusiasts.com>
2010-03-01 08:31:12 +0000

    more Matrix<> typedefs

--- src/Rcpp/Vector.h
+++ src/Rcpp/Vector.h
@@ -1125,6 +1125,7 @@ typedef Vector<INTSXP> IntegerVector ;
 typedef Vector<LGLSXP> LogicalVector ;
 typedef Vector<REALSXP> NumericVector ;
 typedef Vector<RAWSXP> RawVector ;
+
 typedef Vector<STRSXP> CharacterVector ;   
 typedef Vector<STRSXP> StringVector ;
 typedef Vector<VECSXP> GenericVector ;
@@ -1136,6 +1137,12 @@ typedef Matrix<INTSXP> IntegerMatrix ;
 typedef Matrix<LGLSXP> LogicalMatrix ;
 typedef Matrix<REALSXP> NumericMatrix ;
 typedef Matrix<RAWSXP> RawMatrix ;
+
+typedef Matrix<STRSXP> CharacterMatrix ;   
+typedef Matrix<STRSXP> StringMatrix ;
+typedef Matrix<VECSXP> GenericMatrix ;
+typedef Matrix<VECSXP> ListMatrix ;
+typedef Matrix<EXPRSXP> ExpressionMatrix ;
 
 namespace internal{
 	
