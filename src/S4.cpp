ac7aa98c529608f5491989fb003e815d8d3bbdcc
Romain François <romain@r-enthusiasts.com>
2010-03-13 08:59:42 +0000

    new class Rcpp::S4 whose construtor checks that the object is an S4 object

---
+++ src/S4.cpp
@@ -0,0 +1,45 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 8 -*-
+//
+// S4.cpp: Rcpp R/C++ interface class library -- S4 objects
+//
+// Copyright (C) 2010	Dirk Eddelbuettel and Romain Francois
+//
+// This file is part of Rcpp.
+//
+// Rcpp is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Rcpp is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <Rcpp/S4.h>
+
+namespace Rcpp {
+
+	S4::S4() : RObject(){}
+	
+	S4::S4(SEXP x) : RObject(){
+		if( ! ::Rf_isS4(x) ){
+			throw not_s4() ;
+		} else{
+			setSEXP( x) ;
+		}
+	}
+	
+	S4::S4( const S4& other) : RObject(){
+		setSEXP( other.asSexp() ) ;	
+	}
+	
+	S4& S4::operator=( const S4& other){
+		setSEXP( other.asSexp() ) ;
+		return *this ;
+	}
+	
+} // namespace Rcpp
