285324dde59b23474d35871f50726414987f570b
Romain François <romain@r-enthusiasts.com>
2010-03-15 17:57:31 +0000

    maximum # of arguments is upgraded to 20 (thanks to new codebloat script)

--- src/Rcpp/Pairlist.h
+++ src/Rcpp/Pairlist.h
@@ -39,21 +39,72 @@ public:
 template<typename... Args> 
 	Pairlist( const Args&... args) : DottedPair(args...) {}
 #else
-/* <code-bloat> */
+/* <code-bloat>
+
+template <TYPENAMES> 
+Pairlist( ARGUMENTS) : DottedPair(PARAMETERS) {} 
+
+*/
 template <typename T1> 
 Pairlist( const T1& t1) : DottedPair(t1) {} 
 
-template <typename T1, typename T2>
-Pairlist( const T1& t1, const T2& t2) : DottedPair(t1,t2){}
+template <typename T1, typename T2> 
+Pairlist( const T1& t1, const T2& t2) : DottedPair(t1, t2) {} 
 
-template <typename T1, typename T2, typename T3>
-Pairlist( const T1& t1, const T2& t2, const T3& t3): DottedPair(t1,t2,t3) {}
+template <typename T1, typename T2, typename T3> 
+Pairlist( const T1& t1, const T2& t2, const T3& t3) : DottedPair(t1, t2, t3) {} 
 
-template <typename T1, typename T2, typename T3, typename T4>
-Pairlist( const T1& t1, const T2& t2, const T3& t3, const T4& t4): DottedPair(t1,t2,t3,t4){}
+template <typename T1, typename T2, typename T3, typename T4> 
+Pairlist( const T1& t1, const T2& t2, const T3& t3, const T4& t4) : DottedPair(t1, t2, t3, t4) {} 
 
-template <typename T1, typename T2, typename T3, typename T4, typename T5>
-Pairlist( const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5) : DottedPair(t1,t2,t3,t4,t5) {}
+template <typename T1, typename T2, typename T3, typename T4, typename T5> 
+Pairlist( const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5) : DottedPair(t1, t2, t3, t4, t5) {} 
+
+template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6> 
+Pairlist( const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5, const T6& t6) : DottedPair(t1, t2, t3, t4, t5, t6) {} 
+
+template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7> 
+Pairlist( const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5, const T6& t6, const T7& t7) : DottedPair(t1, t2, t3, t4, t5, t6, t7) {} 
+
+template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8> 
+Pairlist( const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5, const T6& t6, const T7& t7, const T8& t8) : DottedPair(t1, t2, t3, t4, t5, t6, t7, t8) {} 
+
+template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9> 
+Pairlist( const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5, const T6& t6, const T7& t7, const T8& t8, const T9& t9) : DottedPair(t1, t2, t3, t4, t5, t6, t7, t8, t9) {} 
+
+template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10> 
+Pairlist( const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5, const T6& t6, const T7& t7, const T8& t8, const T9& t9, const T10& t10) : DottedPair(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10) {} 
+
+template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11> 
+Pairlist( const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5, const T6& t6, const T7& t7, const T8& t8, const T9& t9, const T10& t10, const T11& t11) : DottedPair(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11) {} 
+
+template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12> 
+Pairlist( const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5, const T6& t6, const T7& t7, const T8& t8, const T9& t9, const T10& t10, const T11& t11, const T12& t12) : DottedPair(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12) {} 
+
+template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13> 
+Pairlist( const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5, const T6& t6, const T7& t7, const T8& t8, const T9& t9, const T10& t10, const T11& t11, const T12& t12, const T13& t13) : DottedPair(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13) {} 
+
+template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14> 
+Pairlist( const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5, const T6& t6, const T7& t7, const T8& t8, const T9& t9, const T10& t10, const T11& t11, const T12& t12, const T13& t13, const T14& t14) : DottedPair(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14) {} 
+
+template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15> 
+Pairlist( const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5, const T6& t6, const T7& t7, const T8& t8, const T9& t9, const T10& t10, const T11& t11, const T12& t12, const T13& t13, const T14& t14, const T15& t15) : DottedPair(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15) {} 
+
+template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15, typename T16> 
+Pairlist( const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5, const T6& t6, const T7& t7, const T8& t8, const T9& t9, const T10& t10, const T11& t11, const T12& t12, const T13& t13, const T14& t14, const T15& t15, const T16& t16) : DottedPair(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16) {} 
+
+template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15, typename T16, typename T17> 
+Pairlist( const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5, const T6& t6, const T7& t7, const T8& t8, const T9& t9, const T10& t10, const T11& t11, const T12& t12, const T13& t13, const T14& t14, const T15& t15, const T16& t16, const T17& t17) : DottedPair(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17) {} 
+
+template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15, typename T16, typename T17, typename T18> 
+Pairlist( const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5, const T6& t6, const T7& t7, const T8& t8, const T9& t9, const T10& t10, const T11& t11, const T12& t12, const T13& t13, const T14& t14, const T15& t15, const T16& t16, const T17& t17, const T18& t18) : DottedPair(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18) {} 
+
+template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15, typename T16, typename T17, typename T18, typename T19> 
+Pairlist( const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5, const T6& t6, const T7& t7, const T8& t8, const T9& t9, const T10& t10, const T11& t11, const T12& t12, const T13& t13, const T14& t14, const T15& t15, const T16& t16, const T17& t17, const T18& t18, const T19& t19) : DottedPair(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19) {} 
+
+template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9, typename T10, typename T11, typename T12, typename T13, typename T14, typename T15, typename T16, typename T17, typename T18, typename T19, typename T20> 
+Pairlist( const T1& t1, const T2& t2, const T3& t3, const T4& t4, const T5& t5, const T6& t6, const T7& t7, const T8& t8, const T9& t9, const T10& t10, const T11& t11, const T12& t12, const T13& t13, const T14& t14, const T15& t15, const T16& t16, const T17& t17, const T18& t18, const T19& t19, const T20& t20) : DottedPair(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20) {} 
+
 /* </code-bloat> */
 #endif
 
