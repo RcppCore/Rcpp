955145a9ac38d0d02bf5e28f3ac8c3e30c67b49a
Romain François <romain@r-enthusiasts.com>
2010-04-02 07:58:14 +0000

    new class Rcpp::Formula

---
+++ inst/unitTests/runit.Formula.R
@@ -0,0 +1,45 @@
+#!/usr/bin/r -t
+#
+# Copyright (C) 2010	Dirk Eddelbuettel and Romain Francois
+#
+# This file is part of Rcpp.
+#
+# Rcpp is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Rcpp is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
+.setUp <- function(){
+	suppressMessages( require( inline ) )
+}
+
+test.Formula <- function(){
+	funx <- cfunction(signature(), '
+	Formula f( "x ~ y + z" ) ;
+	return f;
+	',
+	Rcpp=TRUE, verbose=FALSE, includes = "using namespace Rcpp;")
+	checkEquals( funx(), x ~ y + z, msg = "Formula( string )" )
+}
+
+test.Formula.SEXP <- function(){
+	funx <- cfunction(signature( form = "ANY" ), '
+	Formula f(form) ;
+	return f;
+	',
+	Rcpp=TRUE, verbose=FALSE, includes = "using namespace Rcpp;")
+	checkEquals( funx( x ~ y + z), x ~ y + z, msg = "Formula( SEXP = formula )" )
+	checkEquals( funx( "x ~ y + z" ), x ~ y + z, msg = "Formula( SEXP = STRSXP )" )
+	checkEquals( funx( parse( text = "x ~ y + z") ), x ~ y + z, msg = "Formula( SEXP = EXPRSXP )" )
+	checkEquals( funx( list( "x ~ y + z") ), x ~ y + z, msg = "Formula( SEXP = VECSXP(1 = STRSXP) )" )
+	checkEquals( funx( list( x ~ y + z) ), x ~ y + z, msg = "Formula( SEXP = VECSXP(1 = formula) )" )
+	
+}
---
+++ src/Rcpp/Formula.h
@@ -0,0 +1,46 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 8 -*-
+//
+// Formula.h: Rcpp R/C++ interface class library -- formula
+//
+// Copyright (C) 2010	Dirk Eddelbuettel and Romain Francois
+//
+// This file is part of Rcpp.
+//
+// Rcpp is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Rcpp is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef Rcpp__Formula_h
+#define Rcpp__Formula_h
+
+#include <RcppCommon.h>
+#include <Rcpp/Language.h>
+
+namespace Rcpp{ 
+
+class Formula : public Language {
+public:
+
+	Formula() ;
+	
+	Formula(SEXP lang) ;
+
+	Formula(const Formula& other) ;
+	explicit Formula( const std::string& code ); 
+
+	Formula& operator=(const Formula& other) ;
+	
+} ; 
+
+} // namespace Rcpp
+
+#endif
