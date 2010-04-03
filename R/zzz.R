99de82fb4b00eda86a4840dae9a3573743acf178
Romain François <romain@r-enthusiasts.com>
2010-04-03 11:00:14 +0000

    remove code I did not intend to commit

--- R/zzz.R
+++ R/zzz.R
@@ -19,11 +19,3 @@
 	.Call( "initRcpp", PACKAGE = pkgname )
 }
 
-httpd_delegate <- function(path, query, ...){
-	if( grepl( "^/Rcpp/", path ) ){
-		clazz <- sub( "^/Rcpp/.*[.]html$", "\\1", path )
-		tf <- tempfile()
-		unit_test_examples( clazz, tf )
-		return( list( file = tf ) )
-	}
-}
