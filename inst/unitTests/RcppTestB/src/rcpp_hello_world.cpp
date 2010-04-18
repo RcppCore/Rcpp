463b687778393074e40088cc6827e65833561ea2
Romain François <romain@r-enthusiasts.com>
2010-04-18 21:20:54 +0000

    new test package

---
+++ inst/unitTests/RcppTestB/src/rcpp_hello_world.cpp
@@ -0,0 +1,15 @@
+#include "rcpp_hello_world.h"
+
+SEXP rcpp_hello_world(){
+	using namespace Rcpp ;
+	CharacterVector x = CharacterVector::create( "foo", "bar" )  ;
+	NumericVector y   = NumericVector::create( 0.0, 1.0 ) ;
+	List z = List::create( x, y ) ;
+	return z ;
+}
+
+#if defined(WIN32)
+extern "C" void R_init_RcppTestA( DllInfo* info){
+	std::set_terminate( forward_uncaught_exceptions_to_r ) ;
+}
+#endif
---
+++ inst/unitTests/RcppTestB/src/rcpp_hello_world.h
@@ -0,0 +1,8 @@
+#ifndef _RcppTestA_RCPP_HELLO_WORLD_H
+#define _RcppTestA_RCPP_HELLO_WORLD_H
+
+#include <Rcpp.h>
+
+RcppExport SEXP rcpp_hello_world() ;
+
+#endif
