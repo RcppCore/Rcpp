449a777c751ff0e0e36e61f7cb433595e3cf586e
Romain François <romain@r-enthusiasts.com>
2010-04-19 11:11:52 +0000

    move the inline hack away

--- R/cppfunction.R
+++ R/cppfunction.R
@@ -18,49 +18,14 @@
 NAMESPACE <- environment()
 HAVEINLINE <- FALSE
 cfunction <- function(...) stop( "inline not available" )
-compileCode <- function(...) stop( "inline not available" )
 
 init.inline <- function(){
-	hack_compileCode()
-
 	unlockBinding( "HAVEINLINE", NAMESPACE )
 	unlockBinding( "cfunction", NAMESPACE )
 	assignInNamespace( "HAVEINLINE", TRUE, NAMESPACE )
 	assignInNamespace( "cfunction" , get( "cfunction", asNamespace( "inline" )), NAMESPACE )
 	lockBinding( "HAVEINLINE", NAMESPACE )
 	lockBinding( "cfunction", NAMESPACE )
-}
-
-hack_compileCode <- function(){
-	inline <- asNamespace( "inline" )
-	
-	unlockBinding( "compileCode", inline )
-	compileCode <- get("compileCode", 	inline )
-	exprs <- append( 
-		list( as.name("{"), parse(text="code <- Rcpp:::compileCode_hook(f, code)")[[1]] ) , 
-		as.list( body( compileCode )[-1] )
-		)
-	body( compileCode ) <- as.call( exprs )
-	assignInNamespace( "compileCode", compileCode, inline )
-	unlockBinding( "compileCode", inline )
-}
-
-compileCode_hook <- function( f, code){
-	# we only do something if we are called from cppfunction
-	calls <- sys.calls()
-	functions <- sapply( calls, function(.){ if( is.name(.[[1]]) ) as.character( .[[1]]) else "" } )
-	if( "cppfunction" %in% functions ){
-sprintf( 
-'%s
-#if defined(WIN32)
-extern "C" void R_init_%s( DllInfo* info ){
-	std::set_terminate( forward_uncaught_exceptions_to_r ) ;
-}
-#endif
-', code, f )
-	} else {
-		code
-	}
 }
 
 cppfunction <- function (sig = character(), body = character(), includes = character(), 
