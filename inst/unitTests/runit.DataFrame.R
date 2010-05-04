6444df80847b1a553bd5725b0400a4961bcda0dc
Dirk Eddelbuettel <edd@debian.org>
2010-05-04 11:56:11 +0000

    test functions start with test., not runit.

--- inst/unitTests/runit.DataFrame.R
+++ inst/unitTests/runit.DataFrame.R
@@ -17,27 +17,27 @@
 # You should have received a copy of the GNU General Public License
 # along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
 
-runit.DataFrame.FromSEXP <- function() {
+test.DataFrame.FromSEXP <- function() {
     DF <- data.frame(a=1:3, b=c("a","b","c"))
-	fun <- cppfunction( signature(x='ANY'), '
-		DataFrame df(x) ;
-		return df;
-	' )
-	checkEquals( fun(DF), DF, msg = "DataFrame pass-through")
+    fun <- cppfunction( signature(x='ANY'), '
+	DataFrame df(x) ;
+	return df;
+    ' )
+    checkEquals( fun(DF), DF, msg = "DataFrame pass-through")
 }
 
-runit.DataFrame.create1 <- function() {
+test.DataFrame.CreateOne <- function() {
     DF <- data.frame(a=1:3)
-	fun <- cppfunction( signature(), '
+    fun <- cppfunction( signature(), '
         IntegerVector v = IntegerVector::create(1,2,3);
 		return DataFrame::create(Named("a")=v);
-	' )
-	checkEquals( fun(), DF, msg = "DataFrame create1")
+    ' )
+    checkEquals( fun(), DF, msg = "DataFrame create1")
 }
 
-runit.DataFrame.create2 <- function() {
+test.DataFrame.CreateTwo <- function() {
     DF <- data.frame(a=1:3, b=c("a","b","c"))
-	fun <- cppfunction( signature(), '
+    fun <- cppfunction( signature(), '
         IntegerVector v = IntegerVector::create(1,2,3);
         std::vector<std::string> s(3);
         s[0] = "a";
@@ -45,5 +45,5 @@ runit.DataFrame.create2 <- function() {
         s[2] = "c";
 		return DataFrame::create(Named("a")=v, Named("b")=s);
 	' )
-	checkEquals( fun(), DF, msg = "DataFrame create2")
+    checkEquals( fun(), DF, msg = "DataFrame create2")
 }
