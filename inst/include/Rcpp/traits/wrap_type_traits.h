e54689ea16689d294c5db545237684362929248d
Romain François <romain@r-enthusiasts.com>
2010-05-11 13:37:09 +0000

    adding support for (unsigned) long long int types (needed in win64)

--- inst/include/Rcpp/traits/wrap_type_traits.h
+++ inst/include/Rcpp/traits/wrap_type_traits.h
@@ -71,6 +71,14 @@ template <> struct wrap_type_traits<long double> { typedef wrap_type_primitive_t
 template <> struct wrap_type_traits<short> { typedef wrap_type_primitive_tag wrap_category; } ;
 template <> struct wrap_type_traits<unsigned short> { typedef wrap_type_primitive_tag wrap_category; } ;
 
+#ifdef LONG_LONG_MAX
+template <> struct wrap_type_traits<long long int> { typedef wrap_type_primitive_tag wrap_category; } ;
+#endif
+
+#ifdef ULONG_LONG_MAX
+template <> struct wrap_type_traits<unsigned long long int> { typedef wrap_type_primitive_tag wrap_category; } ;
+#endif 
+
 } // namespace traits
 } // namespace Rcpp
 #endif
