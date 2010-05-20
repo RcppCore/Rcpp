140a71b27b5c739591253f5eeff15d4fa15ba8ec
Romain François <romain@r-enthusiasts.com>
2010-05-20 17:08:42 +0000

    constness in Proxy_Iterator and saving Rcpp-unitTests.txt files in Rcpp.Rcheck directory when run under R CMD check

--- inst/unitTests/runTests.R
+++ inst/unitTests/runTests.R
@@ -16,7 +16,9 @@ if(require("RUnit", quietly = TRUE)) {
     ## --- Testing ---
 
     ## Define tests
-    testSuite <- defineTestSuite(name=paste(pkg, "unit testing"), dirs = path )
+    testSuite <- defineTestSuite(name=paste(pkg, "unit testing"), dirs = path
+    	# , testFileRegexp = "runit.Argument.R"
+    )
 
     if(interactive()) {
         cat("Now have RUnit Test Suite 'testSuite' for package '", pkg,
@@ -40,16 +42,22 @@ if(require("RUnit", quietly = TRUE)) {
         		}
         	}
         }
-        
-        # give a chance to the user to customize where he/she wants 
-        # the unit tests results to be stored with the --output= command 
-        # line argument
-        if( exists( "argv",  globalenv() ) ){
-        	# littler
-        	output <- process_args(argv)
-        } else {
-        	# Rscript
-        	output <- process_args(commandArgs(TRUE))
+               
+        # R CMD check uses this
+        if( exists( "Rcpp.unit.test.output.dir", globalenv() ) ){
+			output <- Rcpp.unit.test.output.dir
+		} else {
+          
+        	# give a chance to the user to customize where he/she wants 
+        	# the unit tests results to be stored with the --output= command 
+        	# line argument
+        	if( exists( "argv",  globalenv() ) ){
+        		# littler
+        		output <- process_args(argv)
+        	} else {
+        		# Rscript
+        		output <- process_args(commandArgs(TRUE))
+        	}
         }
         
         # if it did not work, try to use /tmp
--- tests/doRUnit.R
+++ tests/doRUnit.R
@@ -24,6 +24,8 @@ if(require("RUnit", quietly = TRUE)) {
   # without this, we get unit test failures
   Sys.setenv( R_TESTS = "" )
   
+  Rcpp.unit.test.output.dir <- getwd()
+  
   source(file.path(path, "runTests.R"), echo = TRUE)
   
 } else {
