64a828a34cf6a44981f7d6a186f49b2ef6fd49ae
Romain François <romain@r-enthusiasts.com>
2010-05-27 13:51:41 +0000

    add references and document using function that take a pointer as their first argument

--- inst/doc/snippets/WorldModule.cpp
+++ inst/doc/snippets/WorldModule.cpp
@@ -8,12 +8,17 @@ private:
     std::string msg;
 };
 
+void clearWorld( World* w){
+	w->set( "" ) ;
+}
+
 RCPP_MODULE(yada){
 	using namespace Rcpp ;
 	
 	class_<World>( "World" )
 		.method( "greet", &World::greet )
 		.method( "set", &World::set )
+		.method( "clear", &clearWorld )
 	;
 
 }                     
