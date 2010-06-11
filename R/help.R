ec3c231f94e8fedc914c6bd6cf73302b410d440f
Romain François <romain@r-enthusiasts.com>
2010-06-11 06:10:34 +0000

    work around to support accessing documentation through Rcpp ? something

--- R/help.R
+++ R/help.R
@@ -1,4 +1,4 @@
-# Copyright (C) 2009- 2010	Dirk Eddelbuettel and Romain Francois
+# Copyright (C) 2010	Dirk Eddelbuettel and Romain Francois
 #
 # This file is part of Rcpp.
 #
@@ -15,7 +15,29 @@
 # You should have received a copy of the GNU General Public License
 # along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
 
-.onLoad <- function(libname, pkgname){
-	install_help_workaround()
+.help <- function( what ){
+	print( sprintf( "Rcpp help for '%s'", what ) )
+	invisible(TRUE)
+}
+
+install_help_workaround <- function(){
+	utils <- asNamespace( "utils" )
+	unlockBinding( ".tryHelp", utils )
+	th <- get( ".tryHelp", utils )
+	b <- body( th )
+	b <- as.call( c( 
+			as.name( "{" ), 
+			quote( 
+				if( grepl( "[-]Rcpp$", topic ) ){
+					result <- Rcpp:::.help( sub( "[-]Rcpp$", "", topic ) )
+					if( result )
+						return( invisible(NULL) )
+				}
+			), 
+			as.list( b[-1] )
+		) )
+	body( th ) <- b
+	assign( ".tryHelp", th, utils )
+	lockBinding( ".tryHelp", utils )
 }
 
