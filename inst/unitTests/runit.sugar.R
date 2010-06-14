658479d62aa47e1f4b4aa0a604f8c2a79b324749
Romain François <romain@r-enthusiasts.com>
2010-06-14 13:13:32 +0000

    more sugar binary operators

--- inst/unitTests/runit.sugar.R
+++ inst/unitTests/runit.sugar.R
@@ -17,8 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
 
-
-test.any <- function( ){
+test.sugar.any.less <- function( ){
 
 	fx <- cxxfunction( signature( x = "numeric", y = "numeric" ), '
 	
@@ -30,8 +29,104 @@ test.any <- function( ){
 	', plugin = "Rcpp" )
 	
 	checkTrue( ! fx( 1, 0 ) )
-	checkTrue( ! fx( 1:10, 2:11 ) )
+	checkTrue( fx( 1:10, 2:11 ) )
 	checkTrue( fx( 0, 1 ) )
 	checkTrue( is.na( fx( NA, 1 ) ) )
 	
 }
+
+test.sugar.any.greater <- function( ){
+
+	fx <- cxxfunction( signature( x = "numeric", y = "numeric" ), '
+	
+		NumericVector xx(x) ;
+		NumericVector yy(y) ;
+		
+		return any( xx > yy ) ;
+	
+	', plugin = "Rcpp" )
+	
+	checkTrue( fx( 1, 0 ) )
+	checkTrue( fx( 2:11, 1:10 ) )
+	checkTrue( ! fx( 0, 1 ) )
+	checkTrue( is.na( fx( NA, 1 ) ) )
+	
+}
+
+test.sugar.any.less.or.equal <- function( ){
+
+	fx <- cxxfunction( signature( x = "numeric", y = "numeric" ), '
+	
+		NumericVector xx(x) ;
+		NumericVector yy(y) ;
+		
+		return any( xx <= yy ) ;
+	
+	', plugin = "Rcpp" )
+	
+	checkTrue( fx( 1, 1 ) )
+	checkTrue( fx( 1:2, c(1,1) ) )
+	checkTrue( fx( 0, 1 ) )
+	checkTrue( ! fx( 1, 0 ) )
+	checkTrue( is.na( fx( NA, 1 ) ) )
+	
+}
+
+test.sugar.any.greater.or.equal <- function( ){
+
+	fx <- cxxfunction( signature( x = "numeric", y = "numeric" ), '
+	
+		NumericVector xx(x) ;
+		NumericVector yy(y) ;
+		
+		return any( xx >= yy ) ;
+	
+	', plugin = "Rcpp" )
+	
+	checkTrue( fx( 1, 1 ) )
+	checkTrue( fx( 1:2, c(1,1) ) )
+	checkTrue( ! fx( 0, 1 ) )
+	checkTrue( fx( 1, 0 ) )
+	checkTrue( is.na( fx( NA, 1 ) ) )
+	
+}
+
+
+test.sugar.any.equal <- function( ){
+
+	fx <- cxxfunction( signature( x = "numeric", y = "numeric" ), '
+	
+		NumericVector xx(x) ;
+		NumericVector yy(y) ;
+		
+		return any( xx == yy ) ;
+	
+	', plugin = "Rcpp" )
+	
+	checkTrue( fx( 1, 1 ) )
+	checkTrue( fx( 1:2, c(1,1) ) )
+	checkTrue( ! fx( 0, 1 ) )
+	checkTrue( ! fx( 1, 0 ) )
+	checkTrue( is.na( fx( NA, 1 ) ) )
+	
+}
+
+test.sugar.any.not.equal <- function( ){
+
+	fx <- cxxfunction( signature( x = "numeric", y = "numeric" ), '
+	
+		NumericVector xx(x) ;
+		NumericVector yy(y) ;
+		
+		return any( xx != yy ) ;
+	
+	', plugin = "Rcpp" )
+	
+	checkTrue( ! fx( 1, 1 ) )
+	checkTrue( fx( 1:2, c(1,1) ) )
+	checkTrue( fx( 0, 1 ) )
+	checkTrue( fx( 1, 0 ) )
+	checkTrue( is.na( fx( NA, 1 ) ) )
+	
+}
+
