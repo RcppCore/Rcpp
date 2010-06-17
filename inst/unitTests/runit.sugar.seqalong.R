523d2183b2b64c83f159b14513b9e1c378e5633e
Romain François <romain@r-enthusiasts.com>
2010-06-17 18:46:25 +0000

    seq_along

---
+++ inst/unitTests/runit.sugar.seqalong.R
@@ -0,0 +1,34 @@
+#!/usr/bin/r -t
+#
+# Copyright (C) 2010	Dirk Eddelbuettel and Romain Francois
+#
+# This file is part of Rcpp.
+#
+# Rcpp is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Rcpp is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
+test.sugar.seqlaong <- function( ){
+
+	fx <- cxxfunction( signature( x = "numeric" ), '
+	
+		NumericVector xx(x) ;
+		IntegerVector res = seq_along( xx );
+		
+		return res ;
+	
+	', plugin = "Rcpp" )
+	
+	
+	checkEquals( fx( rnorm(10)) , 1:10  )
+}
+
