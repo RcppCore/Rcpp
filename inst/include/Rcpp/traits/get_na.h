9075b6efdf44f5a2a65b54f0ca13bbab5a2dc442
Romain François <romain@r-enthusiasts.com>
2010-06-18 20:39:30 +0000

    implementation of ifelse

--- inst/include/Rcpp/traits/get_na.h
+++ inst/include/Rcpp/traits/get_na.h
@@ -1,8 +1,9 @@
 // -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 8 -*-
+/* :tabSize=4:indentSize=4:noTabs=false:folding=explicit:collapseFolds=1: */
 //
-// functions.h: Rcpp R/C++ interface class library -- sugar functions
+// get_na.h: Rcpp R/C++ interface class library -- NA handling
 //
-// Copyright (C) 2010 Dirk Eddelbuettel and Romain Francois
+// Copyright (C) 2010	Dirk Eddelbuettel and Romain Francois
 //
 // This file is part of Rcpp.
 //
@@ -18,15 +19,37 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+   
+#ifndef Rcpp__traits__get_na__h
+#define Rcpp__traits__get_na__h
 
-#ifndef RCPP_SUGAR_FUNCTIONS_H
-#define RCPP_SUGAR_FUNCTIONS_H
+namespace Rcpp{
+namespace traits{
 
-#include <Rcpp/sugar/functions/any.h>
-#include <Rcpp/sugar/functions/all.h>
-#include <Rcpp/sugar/functions/is_na.h>
-#include <Rcpp/sugar/functions/seq_along.h>
-#include <Rcpp/sugar/functions/sapply.h>
-#include <Rcpp/sugar/functions/ifelse.h>
+template<int RTYPE> 
+typename storage_type<RTYPE>::type get_na() ;
+
+template<>
+inline int get_na<INTSXP>(){ return NA_INTEGER ; }
+
+template<>
+inline int get_na<LGLSXP>(){ return NA_LOGICAL ; }
+
+template<>
+inline double get_na<REALSXP>(){ return NA_REAL ; }
+
+template<>
+inline Rcomplex get_na<CPLXSXP>(){ 
+	Rcomplex x ;
+	x.r = NA_REAL ;
+	x.i = NA_REAL ;
+	return x ;
+}
+
+template<>
+inline SEXP get_na<STRSXP>(){ return NA_STRING ; }
+
+}
+}
 
 #endif
