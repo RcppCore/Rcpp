9b5a54935c2189bbf6fb7265467554b069763a15
Romain François <romain@r-enthusiasts.com>
2010-06-18 20:39:30 +0000

    implementation of ifelse

---
+++ inst/unitTests/runit.sugar.ifelse.R
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2010	Dirk Eddelbuettel and Romain Francois
+#
+# This file is part of Rcpp.
+#
+# Rcpp is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Rcpp is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
+test.sugar.ifelse <- function( ){
+
+	fx <- cxxfunction( signature( x = "numeric", y = "numeric" ), '
+	
+		NumericVector xx(x) ;
+		NumericVector yy(y) ;
+		
+		NumericVector res = ifelse( xx < yy, xx*xx, -(yy*yy) ) ;
+		return res ;
+	', plugin = "Rcpp" )
+
+	x <- 1:10
+	y <- 10:1
+	checkEquals( fx( x, y), ifelse( x<y, x*x, -(y*y) ) )
+	
+}
+
