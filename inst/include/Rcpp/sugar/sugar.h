0bbdb1677958cf5e6192fa59b5557279b8b46d93
Romain François <romain@r-enthusiasts.com>
2010-06-18 14:01:33 +0000

    move operators code into the sugar/operators directory

--- inst/include/Rcpp/sugar/sugar.h
+++ inst/include/Rcpp/sugar/sugar.h
@@ -29,16 +29,17 @@
 #include <Rcpp/sugar/seq_along.h>
 
 // binary operators
-#include <Rcpp/sugar/Comparator.h>
-#include <Rcpp/sugar/Comparator_With_One_Value.h>
-#include <Rcpp/sugar/logical_operators.h>
-#include <Rcpp/sugar/plus.h>
-#include <Rcpp/sugar/minus.h>
-#include <Rcpp/sugar/times.h>
-#include <Rcpp/sugar/divides.h>
+#include <Rcpp/sugar/operators/Comparator.h>
+#include <Rcpp/sugar/operators/Comparator_With_One_Value.h>
+
+#include <Rcpp/sugar/operators/logical_operators.h>
+#include <Rcpp/sugar/operators/plus.h>
+#include <Rcpp/sugar/operators/minus.h>
+#include <Rcpp/sugar/operators/times.h>
+#include <Rcpp/sugar/operators/divides.h>
 
 // unary operators
-#include <Rcpp/sugar/not.h>
-#include <Rcpp/sugar/unary_minus.h>
+#include <Rcpp/sugar/operators/not.h>
+#include <Rcpp/sugar/operators/unary_minus.h>
 
 #endif
