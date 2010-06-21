a2145dd2aefdd06724576cd0ae19931083d4c12f
Romain François <romain@r-enthusiasts.com>
2010-06-21 12:59:45 +0000

    (SingleLogicalResult) || bool

--- inst/include/Rcpp/sugar/logical/logical.h
+++ inst/include/Rcpp/sugar/logical/logical.h
@@ -22,6 +22,8 @@
 #ifndef RCPP_SUGAR_LOGICAL_H
 #define RCPP_SUGAR_LOGICAL_H
 
+#include <Rcpp/sugar/logical/can_have_na.h>
+
 #include <Rcpp/sugar/logical/SingleLogicalResult.h>
 #include <Rcpp/sugar/logical/not.h>
 #include <Rcpp/sugar/logical/and.h>
