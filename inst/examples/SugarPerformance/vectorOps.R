6be386543115d3ae3ac6b46104187d703590cead
Dirk Eddelbuettel <edd@debian.org>
2010-06-22 21:28:47 +0000

    some changes -- and sugar looses the performance race :-/

--- inst/examples/SugarPerformance/vectorOps.R
+++ inst/examples/SugarPerformance/vectorOps.R
@@ -2,21 +2,19 @@
 suppressMessages(library(inline))
 suppressMessages(library(Rcpp))
 
-
 # RcppExport SEXP vectorOps(SEXP runss, SEXP xs, SEXP ys) {
 src <- '
-    Rcpp::NumericVector x(xs);
-    Rcpp::NumericVector y(ys);
-    unsigned int runs = Rcpp::as<int>(runss);
+    NumericVector x(xs);
+    NumericVector y(ys);
+    unsigned int runs = as<int>(runss);
     int n = x.size() ;
 
-    Rcpp::NumericVector res(2);
-    Timer timerOne, timerTwo;
+    Timer timer;
 
     // approach one
-    timerOne.Start();
+    timer.Start();
     for (unsigned int i=0; i<runs; i++) {
-        Rcpp::NumericVector res1( n ) ;
+        NumericVector res1( n ) ;
         double x_ = 0.0 ;
         double y_ = 0.0 ;
         for( int i=0; i<n; i++){
@@ -29,94 +27,98 @@ src <- '
             }
         }
     }
-    timerOne.Stop();
-    //double t1 = timerOne.ElapsedTime();
-    res[0] = timerOne.ElapsedTime();
-    //timer.Reset();
+    timer.Stop();
+    double t1 = timer.ElapsedTime();
+    timer.Reset();
 
     // approach two
-    timerTwo.Start();
-    for (unsigned int i=0; i<runs; i++) {
-        Rcpp::NumericVector res2 = ifelse( x < y, x*x, -(y*y) ) ;
-    }
-    timer.Stop();
-    //double t2 = timerTwo.ElapsedTime();
-    res[1] = timer.ElapsedTime();
-
-    return Rcpp::wrap(res);
-    // FAILS:  return Rcpp::List(Rcpp::Named("one")=Rcpp::wrap(t1),
-    //                           Rcpp::Named("two")=Rcpp::wrap(t2));
-'
-
-
-srcOne <- '
-    Rcpp::NumericVector x(xs);
-    Rcpp::NumericVector y(ys);
-    unsigned int runs = Rcpp::as<int>(runss);
-    int n = x.size() ;
-
-    Timer timer;
-
     timer.Start();
     for (unsigned int i=0; i<runs; i++) {
-        Rcpp::NumericVector res1( n ) ;
-        double x_ = 0.0 ;
-        double y_ = 0.0 ;
-        for( int i=0; i<n; i++){
-            x_ = x[i] ;
-            y_ = y[i] ;
-            if( x_ < y_ ){
-                res1[i] = x_ * x_ ;
-            } else {
-                res1[i] = -( y_ * y_)  ;
-            }
-        }
+        NumericVector res2 = ifelse( x < y, x*x, -(y*y) ) ;
     }
     timer.Stop();
-    return Rcpp::wrap( timer.ElapsedTime() );
+    double t2 = timer.ElapsedTime();
+
+    // FIXME return List(Named("standard") = t1,
+    //            Named("sugar")    = t2);
+    NumericVector v(2); v[0] = t1, v[1] = t2;
+    return v;
 '
 
-srcTwo <- '
-    Rcpp::NumericVector x(xs);
-    Rcpp::NumericVector y(ys);
-    unsigned int runs = Rcpp::as<int>(runss);
+## srcOne <- '
+##     Rcpp::NumericVector x(xs);
+##     Rcpp::NumericVector y(ys);
+##     unsigned int runs = Rcpp::as<int>(runss);
+##     int n = x.size() ;
 
-    Timer timer;
+##     Timer timer;
 
-    timer.Start();
-    for (unsigned int i=0; i<runs; i++) {
-        Rcpp::NumericVector res = ifelse( x < y, x*x, -(y*y) ) ;
-    }
-    timer.Stop();
-    return Rcpp::wrap( timer.ElapsedTime() );
-'
+##     timer.Start();
+##     for (unsigned int i=0; i<runs; i++) {
+##         Rcpp::NumericVector res1( n ) ;
+##         double x_ = 0.0 ;
+##         double y_ = 0.0 ;
+##         for( int i=0; i<n; i++){
+##             x_ = x[i] ;
+##             y_ = y[i] ;
+##             if( x_ < y_ ){
+##                 res1[i] = x_ * x_ ;
+##             } else {
+##                 res1[i] = -( y_ * y_)  ;
+##             }
+##         }
+##     }
+##     timer.Stop();
+##     return Rcpp::wrap( timer.ElapsedTime() );
+## '
 
+## srcTwo <- '
+##     Rcpp::NumericVector x(xs);
+##     Rcpp::NumericVector y(ys);
+##     unsigned int runs = Rcpp::as<int>(runss);
 
+##     Timer timer;
+
+##     timer.Start();
+##     for (unsigned int i=0; i<runs; i++) {
+##         Rcpp::NumericVector res = ifelse( x < y, x*x, -(y*y) ) ;
+##     }
+##     timer.Stop();
+##     return Rcpp::wrap( timer.ElapsedTime() );
+## '
 
 settings <- getPlugin("Rcpp")
 settings$env$PKG_CXXFLAGS <- paste("-I", getwd(), " -O0", sep="")
 
-funOne <- cxxfunction(signature(runss="numeric", xs="numeric", ys="numeric"),
-                      srcOne,
-                      includes='#include "Timer.h"',
-                      plugin="Rcpp",
-                      settings=settings)
-funTwo <- cxxfunction(signature(runss="numeric", xs="numeric", ys="numeric"),
-                      srcTwo,
-                      includes='#include "Timer.h"',
-                      plugin="Rcpp",
-                      settings=settings)
+## funOne <- cxxfunction(signature(runss="numeric", xs="numeric", ys="numeric"),
+##                       srcOne,
+##                       includes='#include "Timer.h"',
+##                       plugin="Rcpp",
+##                       settings=settings)
+## funTwo <- cxxfunction(signature(runss="numeric", xs="numeric", ys="numeric"),
+##                       srcTwo,
+##                       includes='#include "Timer.h"',
+##                       plugin="Rcpp",
+##                       settings=settings)
 
 
-x <- runif(1e6)
-y <- runif(1e6)
-runs <- 10
+x <- runif(1e5)
+y <- runif(1e5)
+runs <- 500
 
-resOne <- funOne(runs, x, y)
-resTwo <- funTwo(runs, x, y)
-cat("Timings: ", resOne, "vs", resTwo, "\n")
+#resOne <- funOne(runs, x, y)
+#resTwo <- funTwo(runs, x, y)
+#cat("Timings: Explicit ", resOne, "vs Sugar:", resTwo, ifelse(resOne > resTwo, "win", "loss"), "\n")
 
-# Does order matter?
-resTwo <- funTwo(runs, x, y)
-resOne <- funOne(runs, x, y)
-cat("Timings: ", resOne, "vs", resTwo, "\n")
+#Does order matter?
+#resTwo <- funTwo(runs, x, y)
+#resOne <- funOne(runs, x, y)
+#cat("Timings: Explicit ", resOne, "vs Sugar:", resTwo, ifelse(resOne > resTwo, "win", "loss"), "\n")
+
+
+fun <- cxxfunction(signature(runss="numeric", xs="numeric", ys="numeric"),
+                   src,
+                   includes='#include "Timer.h"',
+                   plugin="Rcpp",
+                   settings=settings)
+print(fun(runs, x, y))
