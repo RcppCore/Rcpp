03390ed49a59d4b3b1230ae54c09f6ced7c974cd
Romain François <romain@r-enthusiasts.com>
2010-06-23 12:01:51 +0000

    using Timer

--- inst/examples/SugarPerformance/vectorOps.R
+++ inst/examples/SugarPerformance/vectorOps.R
@@ -8,12 +8,10 @@ src <- '
     unsigned int runs = as<int>(runss);
     int n = x.size() ;
 
-    // Timer timer;
-    // 
-    // // approach one
-    // timer.Start();
-    Function sys_time( "Sys.time") ;
-    double start = as<double>( sys_time( ) );
+    Timer timer;
+    
+    // approach one
+    timer.Start();
     for (unsigned int i=0; i<runs; i++) {
    NumericVector res1( n ) ;
         double x_ = 0.0 ;
@@ -30,34 +28,28 @@ src <- '
             }
         }
     }
-    // timer.Stop();
-    // double t1 = timer.ElapsedTime();
-    // timer.Reset();
-    double t1 = as<double>( sys_time( ) ) - start;
-
+    timer.Stop();
+    double t1 = timer.ElapsedTime();
+    
     // approach two
-    // timer.Start();   
+    timer.Reset(); timer.Start();   
     start = as<double>( sys_time( ) ) ;
     for (unsigned int i=0; i<runs; i++) {
         NumericVector res2 = ifelse( x < y, x*x, -(y*y) ) ;
     }
-    // timer.Stop();
-    // double t2 = timer.ElapsedTime();
-    double t2 = as<double>( sys_time( ) ) - start;
-
+    timer.Stop();
+    double t2 = timer.ElapsedTime();
     
     ExpressionVector rcode( "ifelse( x<y, x*x, -(y*y) )" ) ;
     Language call = rcode[0] ;
-    start = as<double>( sys_time( ) ) ;
+    
+    timer.Reset(); timer.Start();   
     for (unsigned int i=0; i<runs; i++) {
         NumericVector res2 = Rf_eval( call, R_GlobalEnv ) ;
     }
-    double t3 = as<double>( sys_time( ) ) - start ;
+    timer.Stop();
+    double t3 = timer.ElapsedTime();
     
-    // FIXME return List(Named("standard") = t1,
-    //            Named("sugar")    = t2);
-    // NumericVector v(2); v[0] = t1, v[1] = t2;
-    // return v;
     return NumericVector::create( 
     	_["hand written"] = t1, 
     	_["sugar"] = t2, 
