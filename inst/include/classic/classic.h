da27459dada6167606b4ca2649f77f1292d05fd9
Romain François <romain@r-enthusiasts.com>
2010-06-25 14:59:41 +0000

    moving the classic api in its own directory (step 3 of 4)

---
+++ inst/include/classic/classic.h
@@ -0,0 +1,42 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 8 -*-
+//
+// classic.h: Rcpp R/C++ interface class library -- 
+//
+// Copyright (C) 2010 Dirk Eddelbuettel and Romain Francois
+//
+// This file is part of Rcpp.
+//
+// Rcpp is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Rcpp is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef Rcpp__CLASSIC__API_h
+#define Rcpp__CLASSIC__API_h
+
+#include <classic/RcppDate.h>
+#include <classic/RcppDateVector.h>
+#include <classic/RcppDatetime.h>
+#include <classic/RcppDatetimeVector.h>
+#include <classic/RcppFrame.h>
+#include <classic/RcppFunction.h>
+#include <classic/RcppList.h>
+#include <classic/RcppMatrix.h>
+#include <classic/RcppMatrixView.h>
+#include <classic/RcppNumList.h>
+#include <classic/RcppParams.h>
+#include <classic/RcppResultSet.h>
+#include <classic/RcppStringVector.h>
+#include <classic/RcppStringVectorView.h>
+#include <classic/RcppVector.h>
+#include <classic/RcppVectorView.h>
+
+#endif
