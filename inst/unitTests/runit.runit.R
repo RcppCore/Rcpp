07d8745c5ec8094afbce10da1b91b46cf555a123
Romain François <romain@r-enthusiasts.com>
2010-07-05 19:31:59 +0000

    remove useless testé

--- inst/unitTests/runit.runit.R
+++
@@ -1,7 +0,0 @@
-
-test.runit <- function(){
-	checkTrue( TRUE, msg = "checking checkTrue" )
-	checkEquals( 1, 1, msg = "checking checkEquals" )
-	checkException( stop("boom"), msg = "checking checkException" )
-}
-
