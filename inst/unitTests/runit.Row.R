cd239158fd22285c261a2ef735dd8c2bcdc796ec
Romain François <romain@r-enthusiasts.com>
2010-07-05 19:29:14 +0000

    apply the one call to cxxfunction trick

--- inst/unitTests/runit.Row.R
+++ inst/unitTests/runit.Row.R
@@ -17,46 +17,122 @@
 # You should have received a copy of the GNU General Public License
 # along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
 
+.setUp <- function(){
+	if( ! exists( ".rcpp.row", globalenv() ) ){
+		# definition of all the functions at once
+		
+		sugar.functions <- list( 
+			"runit_NumericMatrix_row" = list( 
+				signature(x = "matrix" ), 
+				'
+					NumericMatrix m(x) ;
+					NumericMatrix::Row first_row = m.row(0) ;
+					return wrap( std::accumulate( first_row.begin(), first_row.end(), 0.0 ) ) ;
+				'), 
+			"runit_CharacterMatrix_row" = list( 
+				signature(x = "matrix" ), 
+				'
+					CharacterMatrix m(x) ;
+					CharacterMatrix::Row first_row = m.row(0) ;
+					std::string res( 
+						std::accumulate( 
+							first_row.begin(), first_row.end(), std::string() ) ) ;
+					return wrap(res) ;
+				'				
+			),           
+			"runit_GenericMatrix_row" = list( 
+				signature(x = "matrix" ), 
+				'
+					GenericMatrix m(x) ;
+					GenericMatrix::Row first_row = m.row(0) ;
+					IntegerVector out( first_row.size() ) ;
+					std::transform( 
+						first_row.begin(), first_row.end(),
+						out.begin(), 
+						unary_call<SEXP,int>( Function("length" ) ) ) ;
+					return wrap(out) ;
+				'
+			),
+			"runit_NumericMatrix_column" = list( 
+				signature(x = "matrix" ), 
+				'
+					NumericMatrix m(x) ;
+					NumericMatrix::Column col = m.column(0) ;
+					return wrap( std::accumulate( col.begin(), col.end(), 0.0 ) ) ;
+				'	
+			), 
+			"runit_CharacterMatrix_column" = list( 
+				signature(x = "matrix" ),
+					'
+						CharacterMatrix m(x) ;
+						CharacterMatrix::Column col = m.column(0) ;
+						std::string res( 
+							std::accumulate( 
+								col.begin(), col.end(), std::string() ) ) ;
+						return wrap(res) ;
+					'			
+			), 
+			"runit_GenericMatrix_column" = list( 
+				signature(x = "matrix" ), 
+				'
+					GenericMatrix m(x) ;
+					GenericMatrix::Column col = m.column(0) ;
+					IntegerVector out( col.size() ) ;
+					std::transform( 
+						col.begin(), col.end(),
+						out.begin(), 
+						unary_call<SEXP,int>( Function("length" ) ) ) ;
+					return wrap(out) ;
+				' 			
+			)
+		)
+		signatures <- lapply( sugar.functions, "[[", 1L )
+		bodies <- lapply( sugar.functions, "[[", 2L )
+		fx <- cxxfunction( signatures, bodies, plugin = "Rcpp" )
+		getDynLib( fx ) # just forcing loading the dll now
+		assign( ".rcpp.row", fx, globalenv() )
+	}
+}
+	
+			
+			
 test.NumericMatrix.row <- function(){
-	funx <- cppfunction(signature(x = "matrix" ), '
-		NumericMatrix m(x) ;
-		NumericMatrix::Row first_row = m.row(0) ;
-		return wrap( std::accumulate( first_row.begin(), first_row.end(), 0.0 ) ) ;
-	'  )
+	funx <- .rcpp.row$runit_NumericMatrix_row
 	x <- matrix( 1:16 + .5, ncol = 4 )
 	checkEquals( funx( x ), sum( x[1,] ), msg = "iterating over a row" )
 }
 
 test.CharacterMatrix.row <- function(){
-	funx <- cppfunction(signature(x = "matrix" ), '
-		CharacterMatrix m(x) ;
-		CharacterMatrix::Row first_row = m.row(0) ;
-		std::string res( 
-			std::accumulate( 
-				first_row.begin(), first_row.end(), std::string() ) ) ;
-		return wrap(res) ;
-	'  )
-	
+	funx <- .rcpp.row$runit_CharacterMatrix_row
 	m <- matrix( letters, ncol = 2 )
 	checkEquals( funx(m), paste( m[1,], collapse = "" ), msg = "CharacterVector::Row" )
 }
 
 test.List.row <- function(){
-	
-	funx <- cppfunction(signature(x = "matrix" ), '
-		GenericMatrix m(x) ;
-		GenericMatrix::Row first_row = m.row(0) ;
-		IntegerVector out( first_row.size() ) ;
-		std::transform( 
-			first_row.begin(), first_row.end(),
-			out.begin(), 
-			unary_call<SEXP,int>( Function("length" ) ) ) ;
-		return wrap(out) ;
-	'  )
-	
+	funx <- .rcpp.row$runit_GenericMatrix_row
 	m <- lapply( 1:16, function(i) seq(from=1, to = i ) )
 	dim( m ) <- c( 4, 4 )
 	checkEquals( funx( m ), 1 + 0:3*4, msg = "List::Row" )
+	
+}
+
+test.NumericMatrix.column <- function(){
+	funx <- .rcpp.row$runit_NumericMatrix_column
+	x <- matrix( 1:16 + .5, ncol = 4 )
+	checkEquals( funx( x ), sum( x[,1] ) , msg = "iterating over a column" )
+}
+
+test.CharacterMatrix.column <- function(){
+	funx <- .rcpp.row$runit_CharacterMatrix_column
+	m <- matrix( letters, ncol = 2 )
+	checkEquals( funx(m), paste( m[,1], collapse = "" ), msg = "CharacterVector::Column" )
+}
+
+test.List.column <- function(){
+	funx <- .rcpp.row$runit_GenericMatrix_column
+	m <- lapply( 1:16, function(i) seq(from=1, to = i ) )
+	dim( m ) <- c( 4, 4 )
+	checkEquals( funx( m ), 1:4, msg = "List::Column" )
 	
 }
 
