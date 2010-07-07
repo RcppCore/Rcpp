7264211eba9db5f11788ef50e7899c5225f3dc51
Romain François <romain@r-enthusiasts.com>
2010-07-07 14:12:19 +0000

    merge runit.CharacterVector into runit.Vector

--- inst/unitTests/runit.Vector.R
+++ inst/unitTests/runit.Vector.R
@@ -423,8 +423,163 @@
     					std::vector< std::complex<float> > v_float(10) ;
     					return List::create( _["float"] = v_float, _["double"] = v_double ) ;
     				'
-    			)
-              
+    			), 
+    			
+    			
+    			
+    			
+              "character_ctor"=list(
+                  signature(),
+                  'CharacterVector x(10) ;
+	           for( int i=0; i<10; i++) x[i] = "foo" ;
+	           return x ;')
+
+                  ,"character_STRSXP_"=list(
+                   signature(vec = "character" ),
+                   'CharacterVector x(vec) ;
+	            std::string st = "" ;
+	            for( int i=0; i<x.size(); i++) {
+		         st += x[i] ;
+	            }
+	            return wrap( st ) ;' )
+
+                  ,"character_plusequals"=list(
+                   signature(),
+                   'CharacterVector x(2) ;
+	            x[0] = "foo" ;
+	            x[1] = "bar" ;
+	            x[0] += "bar" ;
+	            x[1] += x[0] ;
+	            return x ;')
+
+                  ,"character_matrix_indexing"=list(
+                   signature(x = "character" ),
+                   'CharacterVector m(x) ;
+		    std::string trace;
+		    for( size_t i=0 ; i<4; i++){
+		        trace += m(i,i) ;
+		    }
+		    return wrap( trace ) ;')
+
+                  ,"character_matrix_indexing_lhs"=list(
+                   signature(x = "integer" ),
+                   'CharacterVector m(x) ;
+		    for( size_t i=0 ; i<4; i++){
+		        m(i,i) = "foo" ;
+		    }
+		    return m ;')
+
+                  ,"character_assign1"=list(
+                   signature(),
+                   'const char* x[] = { "foo", "bar", "bling", "boom" } ;
+		    CharacterVector y ;
+		    y.assign( x, x+4 ) ;
+		    return y;')
+
+                  ,"character_assign2"=list(
+                   signature(),
+                   'std::vector<std::string> vec(4) ;
+		    vec[0] = "foo";
+		    vec[1] = "bar";
+		    vec[2] = "bling";
+		    vec[3] = "boom" ;
+		    CharacterVector y ;
+		    y.assign( vec.begin(), vec.end() ) ;
+		    return y;')
+
+                  ,"character_range_ctor1"=list(
+                   signature(),
+                   'const char* x[] = { "foo", "bar", "bling", "boom" } ;
+		    CharacterVector y( x, x+4 ) ;
+		    return y;')
+
+                  ,"character_range_ctor2"=list(
+                   signature(),
+                   'std::vector<std::string> vec(4) ;
+		    vec[0] = "foo";
+		    vec[1] = "bar";
+		    vec[2] = "bling";
+		    vec[3] = "boom" ;
+		    CharacterVector y( vec.begin(), vec.end() ) ;
+		    return y; ')
+
+                  ,"character_dimension_ctor1"=list(
+                   signature(),
+                   'return CharacterVector( Dimension( 5 ) ) ;')
+
+                  ,"character_dimension_ctor2"=list(
+                   signature(),
+                   'return CharacterVector( Dimension( 5, 5 ) ) ;')
+
+                  ,"character_dimension_ctor3"=list(
+                   signature(),
+                   'return CharacterVector( Dimension( 2, 3, 4) ) ;')
+
+                  ,"character_iterator1"=list(
+                   signature(x = "character"),
+                   'CharacterVector letters(x) ;
+		    std::string res ;
+		    CharacterVector::iterator first = letters.begin() ;
+		    CharacterVector::iterator last = letters.end() ;
+		    while( first != last ){
+		        res += *first ;
+			++first ;
+		    }
+		    return wrap(res) ;' )
+
+                  ,"character_iterator2"=list(
+                   signature(x = "character"),
+                   'CharacterVector letters(x) ;
+		    std::string res(std::accumulate(letters.begin(), letters.end(), std::string()));
+		    return wrap(res) ;' )
+
+                  ,"character_reverse"=list(
+                   signature(x = "character"),
+                   'CharacterVector y(x) ;
+		    std::reverse( y.begin(), y.end() ) ;
+		    return y ;')
+
+                  ,"character_names_indexing"=list(
+                   signature(x = "character"),
+                   'CharacterVector y(x) ;
+		    std::string foo( y["foo"] ) ;
+		    return wrap(foo) ;')
+
+                  ,"character_comma"=list(
+                   signature(),
+                   'CharacterVector x(3) ;
+        	    x = "foo", "bar", "bling" ;
+        	    return x ;')
+
+                  ,"character_listOf"=list(
+                   signature(l = "list"),
+                   'List ll(l);
+                    CharacterVector cv1 = ll["foo"];
+                    CharacterVector cv2 = ll["bar"];
+                    std::string rv1 = std::string(cv1[0]) + cv1[1] + cv1[2];
+                    std::string rv2 = std::string(cv2[0]) + cv2[1] + cv2[2];
+                    return List::create(_["foo"] = rv1, _["bar"] = rv2); ')
+
+                  ,"character_find_"=list(
+                   signature(x = "character"),
+                   'CharacterVector y(x) ;
+	            CharacterVector::iterator it = std::find( y.begin(), y.end(), "foo" ) ;
+	            return wrap( std::distance( y.begin(), it )); '
+	            
+	            ), "character_create_" = list( 
+	            	signature(), 
+	            	'
+						List output(2); 
+						output[0] = CharacterVector::create( "foo", "bar" ) ;
+						output[1] = CharacterVector::create( 
+							_["foo"] = "bar", 
+							_["bar"] = "foo"
+							) ;
+						return output ;
+					'
+	            )
+
+                
                   
 
                   
@@ -474,7 +629,12 @@
 		   			SEXP x2 = PROTECT( Rf_ScalarInteger( 2 ) ) ;
 		   			List x = { x0, x1, x2} ;
 		   			UNPROTECT(3) ;
-		   			return x ;')
+		   			return x ;'), 
+		   			
+		   		"character_initializer_list"=list(
+                  signature(),
+                  'CharacterVector x = {"foo", "bar"} ;
+                  return x ;')
 
                   
         	)     
@@ -910,4 +1070,128 @@ test.List.stdcomplex <- function(){
 		list( float = rep(0+0i, 10), double = rep(0+0i, 10) ), 
 		msg = "range wrap over std::complex" )
 }
+           
+
+
+
+
+test.CharacterVector <- function(){
+    fun <- .rcpp.Vector$character_ctor
+    checkEquals( fun(), rep("foo",10L), msg = "CharacterVector" )
+}
+
+test.CharacterVector.STRSXP <- function(){
+    fun <- .rcpp.Vector$character_STRSXP_
+    checkEquals( fun(letters), paste(letters,collapse="" ), msg = "CharacterVector( STRSXP) " )
+}
+
+if (Rcpp:::capabilities()[["initializer lists"]]) {
+    test.CharacterVector.initializer.list <- function() {
+        fun <- .rcpp.Vector$character_initializer_list
+        checkEquals( fun(), c("foo","bar"), msg = "CharacterVector( initializer list) " )
+    }
+}
+
+test.CharacterVector.plusequals <- function(){
+    fun <- .rcpp.Vector$character_plusequals
+    checkEquals( fun(), c("foobar", "barfoobar"), msg = "StringProxy::operator+=" )
+}
+
+test.CharacterVector.matrix.indexing <- function() {
+    fun <- .rcpp.Vector$character_matrix_indexing
+    x <- matrix( as.character(1:16), ncol = 4 )
+    checkEquals( fun(x), paste(diag(x), collapse = ""), msg = "matrix indexing" )
+
+    y <- as.vector( x )
+    checkException( fun(y) , msg = "not a matrix" )
+
+    fun <- .rcpp.Vector$character_matrix_indexing_lhs
+    checkEquals( diag(fun(x)), rep("foo", 4) , msg = "matrix indexing lhs" )
+}
+
+test.CharacterVector.assign <- function(){
+    fun <- .rcpp.Vector$character_assign1
+    checkEquals( fun(), c("foo", "bar", "bling", "boom"), msg = "assign(char**, char**)" )
+
+    fun <- .rcpp.Vector$character_assign2
+    checkEquals( fun(), c("foo", "bar", "bling", "boom"), msg = "assign(char**, char**)" )
+
+}
+
+test.CharacterVector.range.constructors <- function(){
+    fun <- .rcpp.Vector$character_range_ctor1
+    checkEquals( fun(), c("foo", "bar", "bling", "boom"), msg = "assign(char**, char**)" )
+
+    fun <- .rcpp.Vector$character_range_ctor2
+    checkEquals( fun(), c("foo", "bar", "bling", "boom"), msg = "assign(char**, char**)" )
+}
+
+test.CharacterVector.Dimension.constructor <- function(){
+    fun <- .rcpp.Vector$character_dimension_ctor1
+    checkEquals(fun(),
+                character(5),
+                msg = "CharacterVector( Dimension(5))" )
+
+    fun <- .rcpp.Vector$character_dimension_ctor2
+    checkEquals(fun(),
+                matrix( "", ncol = 5, nrow = 5),
+                msg = "CharacterVector( Dimension(5,5))" )
+
+    fun <- .rcpp.Vector$character_dimension_ctor3
+    checkEquals(fun(),
+		array( "", dim = c(2,3,4) ) ,
+		msg = "CharacterVector( Dimension(2,3,4))" )
+}
+
+test.CharacterVector.iterator <- function(){
+    fun <- .rcpp.Vector$character_iterator1
+    checkEquals(fun(letters),
+		paste(letters, collapse=""),
+		msg = "CharacterVector::iterator explicit looping" )
+
+    fun <- .rcpp.Vector$character_iterator2
+    checkEquals(fun(letters),
+		paste(letters, collapse=""),
+		msg = "CharacterVector::iterator using std::accumulate" )
+}
+
+test.CharacterVector.reverse <- function(){
+    fun <- .rcpp.Vector$character_reverse
+    x <- c("foo", "bar", "bling")
+    x <- fun(x)
+    checkEquals( x, c("bling", "bar", "foo"), msg = "reverse" )
+    x <- fun(x)
+    checkEquals( x, c("foo", "bar", "bling"), msg = "reverse" )
+}
+
+test.CharacterVector.names.indexing <- function(){
+    fun <- .rcpp.Vector$character_names_indexing
+    x <- c( foo = "foo", bar = "bar" )
+    checkEquals( fun(x), "foo", msg = "CharacterVector names based indexing" )
+}
+
+test.CharacterVector.comma <- function(){
+    fun <- .rcpp.Vector$character_comma
+    checkEquals( fun(), c("foo","bar", "bling" ), msg = "CharacterVector comma operator" )
+}
+
+test.CharacterVector.listOf <- function() {
+    fun <- .rcpp.Vector$character_listOf
+    checkEquals(fun(list(foo=c("tic","tac","toe"),
+                         bar=c("Eenie","Meenie","Moe"))),
+                list(foo="tictactoe", bar="EenieMeenieMoe"),
+                msg="CharacterVector from list")
+}
+
+test.CharacterVector.find <- function(){
+    fun <- .rcpp.Vector$character_find_
+    checkEquals( fun( c("bar", "foo", "bob") ), 1L, msg = "support for std::find in CharacterVector" )
+}
+    
+test.CharacterVector.create <- function(){
+	fun <- .rcpp.Vector$character_create_
+    checkEquals( fun(), list( c( "foo", "bar" ), c(foo = "bar", bar = "foo" ) ), 
+	 	msg = "CharacterVector::create" )
+}
+
 
