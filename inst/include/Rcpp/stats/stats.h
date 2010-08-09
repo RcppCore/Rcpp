670b2e5782a63e43e010cbf8c1bdc92eda0aa678
Romain François <romain@r-enthusiasts.com>
2010-08-09 07:16:41 +0000

    (dpq)weibull (no tests yet)

--- inst/include/Rcpp/stats/stats.h
+++ inst/include/Rcpp/stats/stats.h
@@ -29,6 +29,7 @@
 #include <Rcpp/stats/beta.h>
 #include <Rcpp/stats/t.h>
 #include <Rcpp/stats/lnorm.h>
+#include <Rcpp/stats/weibull.h>
 
 #include <Rcpp/stats/binom.h>
 #include <Rcpp/stats/pois.h>
