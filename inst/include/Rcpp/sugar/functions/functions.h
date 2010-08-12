cca0600871cce1c828fc332197ddff929023958e
Romain François <romain@r-enthusiasts.com>
2010-08-12 04:48:31 +0000

    merge gamma.h and math.h

--- inst/include/Rcpp/sugar/functions/functions.h
+++ inst/include/Rcpp/sugar/functions/functions.h
@@ -26,7 +26,6 @@
 #include <cmath>
 
 #include <Rcpp/sugar/functions/SugarBlock.h>
-#include <Rcpp/sugar/functions/gamma.h>
 #include <Rcpp/sugar/functions/math.h>
 
 #include <Rcpp/sugar/functions/any.h>
