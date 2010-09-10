488d3c7cb2a15024d5fd86958417d716d6a37631
Romain François <romain@r-enthusiasts.com>
2010-09-10 11:43:00 +0000

    added John's testRcppModule package as part of Rcpp unit tests

---
+++ inst/unitTests/testRcppModule/src/Num.cpp
@@ -0,0 +1,28 @@
+#include "rcpp_hello_world.h"
+
+class Num{
+public:
+    Num() : x(0.0), y(0){} ;
+
+    double getX() { return x ; }
+    void setX(double value){ x = value ; }
+
+    int getY() { return y ; }
+
+private:
+    double x ;
+    int y ;
+};
+
+RCPP_MODULE(NumEx){
+	using namespace Rcpp ;
+
+	class_<Num>( "Num" )
+
+		// read and write property
+		.property( "x", &Num::getX, &Num::setX )
+
+		// read-only property
+		.property( "y", &Num::getY )
+	;
+}
