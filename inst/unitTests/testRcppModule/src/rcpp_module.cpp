dc74c86d7f9df2f4533e5de31b043fd0867b586a
Romain François <romain@r-enthusiasts.com>
2010-09-10 11:43:00 +0000

    added John's testRcppModule package as part of Rcpp unit tests

---
+++ inst/unitTests/testRcppModule/src/rcpp_module.cpp
@@ -0,0 +1,55 @@
+#include <Rcpp.h>
+
+std::string hello() {
+	throw std::range_error( "boom" ) ;
+}
+
+int bar( int x){
+	return x*2 ;
+}
+        
+double foo( int x, double y){
+	return x * y ;
+}
+
+void bla( ){
+	Rprintf( "hello\\n" ) ;
+}
+
+void bla1( int x){
+	Rprintf( "hello (x = %d)\\n", x ) ;
+}
+  
+void bla2( int x, double y){
+	Rprintf( "hello (x = %d, y = %5.2f)\\n", x, y ) ;
+}
+
+class World {
+public:
+    World() : msg("hello"){}
+    void set(std::string msg) { this->msg = msg; }
+    std::string greet() { return msg; }
+
+private:
+    std::string msg;
+};
+
+
+
+RCPP_MODULE(yada){
+	using namespace Rcpp ;
+	                  
+	function( "hello" , &hello ) ;
+	function( "bar"   , &bar   ) ;
+	function( "foo"   , &foo   ) ;
+	function( "bla"   , &bla   ) ;
+	function( "bla1"  , &bla1   ) ;
+	function( "bla2"  , &bla2   ) ;
+	
+	class_<World>( "World" )
+		.method( "greet", &World::greet )
+		.method( "set", &World::set )
+	;
+}                     
+
+
