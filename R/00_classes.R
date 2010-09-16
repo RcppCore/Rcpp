1108f43ee46ea9549fb94c1e214a9931a7eec874
Romain François <romain@r-enthusiasts.com>
2010-09-16 09:35:12 +0000

    introducing C++Field

---
+++ R/00_classes.R
@@ -0,0 +1,58 @@
+# Copyright (C)        2010 John Chambers, Dirk Eddelbuettel and Romain Francois
+#
+# This file is part of Rcpp.
+#
+# Rcpp is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Rcpp is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
+
+## "Module" class as an environment with "pointer", "moduleName", and "packageName"
+## Stands in for a reference class with those fields.
+setClass( "Module",  contains = "environment" )
+
+setClass( "C++Field", 
+    representation( 
+        pointer = "externalptr", 
+        cpp_class = "character", 
+        read_only = "logical"
+    )
+)
+
+setClass( "C++Class", 
+	representation( 
+	    pointer = "externalptr", 
+	    module  = "externalptr", 
+	    fields  = "list" 
+	), 
+	contains = "character"
+	)
+setClass( "C++ClassRepresentation", 
+    representation( pointer = "externalptr" ), 
+    contains = "classRepresentation" )
+
+# might not actually use this
+setClass( "C++Property" )	
+
+setClass( "C++Object", 
+	representation( 
+		module = "externalptr", 
+		cppclass = "externalptr", 
+		pointer = "externalptr"
+		)
+	)
+setClass( "C++Function", 
+	representation( pointer = "externalptr" ), 
+	contains = "function"
+)
+
+
