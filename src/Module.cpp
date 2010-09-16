7e159ad567f117e85db1cd7ce6488a7dee485da6
Romain François <romain@r-enthusiasts.com>
2010-09-16 19:00:21 +0000

    conflicting setClass calls

--- src/Module.cpp
+++ src/Module.cpp
@@ -1,4 +1,4 @@
-// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 8 -*-
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 4 -*-
 //
 // Module.cpp: Rcpp R/C++ interface class library -- Rcpp modules
 //
