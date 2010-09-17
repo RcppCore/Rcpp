098e0943fa30da1d5bcb7f9ba01fdaea28eb1c5c
Romain François <romain@r-enthusiasts.com>
2010-09-17 08:56:21 +0000

    add comments and move things around so that Module.R is kept to the essentials

---
+++ R/03_prompt.R
@@ -0,0 +1,62 @@
+# Copyright (C)        2010 John Chambers, Dirk Eddelbuettel and Romain Francois
+#
+# This file is part of Rcpp.
+#
+# Rcpp is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Rcpp is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
+setGeneric( "functions", function(object, ...) standardGeneric( "functions" ) )
+setMethod( "functions", "Module", function(object, ...){
+    pointer <- .getModulePointer(object)
+    if(identical(pointer, .badModulePointer))
+        stop(gettextf("Module \"%s\" has not been intialized:  try Module(object)",
+                      get("moduleName", envir = as.environment(object))), domain = NA)
+    else
+	.Call( "Module__funtions_arity", pointer, PACKAGE = "Rcpp" )
+} )
+
+setGeneric( "prompt" )
+setMethod( "prompt", "Module", function(object, filename = NULL, name = NULL, ...){
+	lines <- readLines( system.file( "prompt", "module.Rd", package = "Rcpp" ) )
+	if( is.null(name) ) name <- .Call( "Module__name", object@pointer, PACKAGE = "Rcpp" )
+	if( is.null(filename) ) filename <- sprintf( "%s-module.Rd", name )
+	lines <- gsub( "NAME", name, lines )
+	
+	info <- functions( object )
+	f.txt <- if( length( info ) ){
+		sprintf( "functions: \\\\describe{
+%s
+		}", paste( sprintf( "        \\\\item{%s}{ ~~ description of function %s ~~ }", names(info), names(info) ), collapse = "\n" ) )
+	} else {
+		"" 
+	}
+	lines <- sub( "FUNCTIONS", f.txt, lines )
+
+        ## at this point functions() would have failed if the
+        ## pointer in object was not valid
+        pointer <- .getModulePointer(object)
+	
+	classes <- .Call( "Module__classes_info", pointer, PACKAGE = "Rcpp" )
+	c.txt <- if( length( classes ) ){
+		sprintf( "classes: \\\\describe{
+%s
+		}", paste( sprintf( "        \\\\item{%s}{ ~~ description of class %s ~~ }", names(classes), names(classes) ), collapse = "\n" ) )
+	} else {
+		"" 
+	}
+	lines <- sub( "CLASSES", c.txt, lines )
+	
+	writeLines( lines, filename )
+	invisible(NULL)
+} )
+
