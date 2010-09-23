c939d7d87fdc0648ace34492721fa27539174a37
Romain François <romain@r-enthusiasts.com>
2010-09-23 15:48:59 +0000

    less C++ ioverhead if we register the symbol instead of doing extern C

--- inst/examples/ConvolveBenchmarks/overhead.r
+++ inst/examples/ConvolveBenchmarks/overhead.r
@@ -8,14 +8,20 @@ b <- rnorm(100)
 dyn.load("overhead_1.so")
 dyn.load("overhead_2.so")
 
-overhead_c <- function(a,b) .Call( "overhead_c", a, b )
-overhead_cpp <- function(a,b) .Call( "overhead_cpp", a, b )
+overhead_c_symbol <- getNativeSymbolInfo( "overhead_c" )
+overhead_cpp_symbol <- getNativeSymbolInfo( "overhead_cpp" )
+
+overhead_c <- function(a,b) .Call( overhead_c_symbol, a, b )
+overhead_cpp <- function(a,b) .Call( overhead_cpp_symbol, a, b )
 
 ## load benchmarkin helper function
 suppressMessages(library(rbenchmark))
 
-benchmark(overhead_c(a,b),
-            overhead_cpp(a,b),
+benchmark(
+
+    overhead_cpp(a,b),
+    overhead_c(a,b),
+    
             columns=c("test", "replications", "elapsed", "relative", "user.self", "sys.self"),
                 order="relative",
                 replications=10000)
