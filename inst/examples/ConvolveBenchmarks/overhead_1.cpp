05dc8c5d62f502db68e1c1aec1bd3136f6337ea5
Romain François <romain@r-enthusiasts.com>
2010-09-23 15:48:59 +0000

    less C++ ioverhead if we register the symbol instead of doing extern C

--- inst/examples/ConvolveBenchmarks/overhead_1.cpp
+++ inst/examples/ConvolveBenchmarks/overhead_1.cpp
@@ -3,9 +3,19 @@
 // This is a rewrite of the 'Writing R Extensions' section 5.10.1 example
 
 #include <Rcpp.h>
-using namespace Rcpp ;
+// using namespace Rcpp ;
 
-RcppExport SEXP overhead_cpp(SEXP a, SEXP b) {
+SEXP overhead_cpp(SEXP a, SEXP b) {
     return R_NilValue ;
+}
+
+extern "C" void R_init_overhead_1(DllInfo *info){
+
+     R_CallMethodDef callMethods[]  = {
+       {"overhead_cpp", (DL_FUNC) &overhead_cpp, 2},
+       {NULL, NULL, 0}
+     };
+
+     R_registerRoutines(info, NULL, callMethods, NULL, NULL);
 }
 
