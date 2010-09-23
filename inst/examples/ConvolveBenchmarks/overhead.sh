5500bb571ce8a3213424c78c569f34fb271f0bfb
Romain François <romain@r-enthusiasts.com>
2010-09-23 15:20:20 +0000

    trying to quantify C and C++ overhead

--- inst/examples/ConvolveBenchmarks/overhead.sh
+++ inst/examples/ConvolveBenchmarks/overhead.sh
@@ -3,18 +3,14 @@
 rm -f *.o *.so
 
 # build the shared library for the C variant
-R CMD SHLIB convolve2_c.c
-R CMD SHLIB convolve7_c.c
+R CMD SHLIB overhead_2.c
 
 # build the shared library for the C++ variant
 # we have to let R know where the Rcpp header and library are
 export PKG_CPPFLAGS=`Rscript -e "Rcpp:::CxxFlags()"`
 export PKG_LIBS=`Rscript -e "Rcpp:::LdFlags()"`
-R CMD SHLIB convolve2_cpp.cpp
-R CMD SHLIB convolve3_cpp.cpp
-R CMD SHLIB convolve4_cpp.cpp
-R CMD SHLIB convolve5_cpp.cpp
+R CMD SHLIB overhead_1.cpp
 
 # call R so that we get an interactive session
-Rscript exampleRCode.r
+Rscript overhead.r
 
