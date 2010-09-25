8ddee6ad56b57b2caa8bcb7294e1941dfdfd64cc
Dirk Eddelbuettel <edd@debian.org>
2010-09-25 23:11:33 +0000

    updated to section 5.10.1 of R 2.11.1

--- inst/examples/ConvolveBenchmarks/convolve7_c.c
+++ inst/examples/ConvolveBenchmarks/convolve7_c.c
@@ -6,19 +6,37 @@
 
 SEXP convolve7(SEXP a, SEXP b)
 {
-    int i, j, na, nb, nab;
+    /* int i, j, na, nb, nab; */
+    /* SEXP ab; */
+
+    /* PROTECT(a = AS_NUMERIC(a)); */
+    /* PROTECT(b = AS_NUMERIC(b)); */
+    /* na = LENGTH(a); nb = LENGTH(b); nab = na + nb - 1; */
+    /* PROTECT(ab = NEW_NUMERIC(nab)); */
+    /* for(i = 0; i < nab; i++) REAL(ab)[i] = 0.0; */
+    /* for(i = 0; i < na; i++) */
+    /* 	for(j = 0; j < nb; j++) REAL(ab)[i + j] += REAL(a)[i] * REAL(b)[j]; */
+    /* UNPROTECT(3); */
+    /* return(ab); */
+
+    R_len_t i, j, na, nb, nab;
+    double *xa, *xb, *xab;
     SEXP ab;
 
-    PROTECT(a = AS_NUMERIC(a));
-    PROTECT(b = AS_NUMERIC(b));
-    na = LENGTH(a); nb = LENGTH(b); nab = na + nb - 1;
-    PROTECT(ab = NEW_NUMERIC(nab));
-    for(i = 0; i < nab; i++) REAL(ab)[i] = 0.0;
+    PROTECT(a = coerceVector(a, REALSXP));
+    PROTECT(b = coerceVector(b, REALSXP));
+    na = length(a); nb = length(b); nab = na + nb - 1;
+    PROTECT(ab = allocVector(REALSXP, nab));
+    xa = REAL(a); xb = REAL(b);
+    xab = REAL(ab);
+    for(i = 0; i < nab; i++) xab[i] = 0.0;
     for(i = 0; i < na; i++)
-    	for(j = 0; j < nb; j++) REAL(ab)[i + j] += REAL(a)[i] * REAL(b)[j];
+	for(j = 0; j < nb; j++) xab[i + j] += xa[i] * xb[j];
     UNPROTECT(3);
     return(ab);
 }
+
+
 
 #include "loopmacro.h"
 LOOPMACRO_C(convolve7)
