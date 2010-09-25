159a8afbe0b71fe3ffc20a0cacd6c8ed9a59c8c5
Romain François <romain@r-enthusiasts.com>
2010-09-25 07:05:57 +0000

    going further with registration of routines (NAMESPACE, etc ...)

---
+++ inst/include/Rcpp/routines.h
@@ -0,0 +1,39 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 8 -*-
+//
+// routines.h: Rcpp R/C++ interface class library -- .Call exported routines
+//
+// Copyright (C) 2010	John Chambers, Dirk Eddelbuettel and Romain Francois
+//
+// This file is part of Rcpp.
+//
+// Rcpp is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Rcpp is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef Rcpp__routines_h
+#define Rcpp__routines_h
+
+// we have to do the ifdef __cplusplus dance because this file
+// is included both in C and C++ files
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+SEXP CppField__get(SEXP, SEXP, SEXP);
+SEXP CppField__set(SEXP, SEXP, SEXP, SEXP);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
