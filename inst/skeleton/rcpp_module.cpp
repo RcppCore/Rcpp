a28cccec3ed6876cca62e0453ed4aa135bec6454
Romain François <romain@r-enthusiasts.com>
2010-11-26 12:46:14 +0000

    update the module part of Rcpp.package.skeleton

--- inst/skeleton/rcpp_module.cpp
+++ inst/skeleton/rcpp_module.cpp
@@ -39,16 +39,26 @@ private:
 RCPP_MODULE(yada){
 	using namespace Rcpp ;
 	                  
-	function( "hello" , &hello ) ;
-	function( "bar"   , &bar   ) ;
-	function( "foo"   , &foo   ) ;
-	function( "bla"   , &bla   ) ;
-	function( "bla1"  , &bla1   ) ;
-	function( "bla2"  , &bla2   ) ;
+	function( "hello" , &hello  , "documentation for hello " ) ;
+	function( "bla"   , &bla    , "documentation for bla " ) ;
+	function( "bla1"  , &bla1   , "documentation for bla1 " ) ;
+	function( "bla2"  , &bla2   , "documentation for bla2 " ) ;
+	
+	// with formal arguments specification
+	function( "bar"   , &bar    , 
+	    List::create( _["x"] = 0.0 ), 
+	    "documentation for bar " ) ;
+	function( "foo"   , &foo    , 
+	    List::create( _["x"] = 1, _["y"] = 1.0 ),
+	    "documentation for foo " ) ;	
 	
 	class_<World>( "World" )
-		.method( "greet", &World::greet )
-		.method( "set", &World::set )
+	
+	    // expose the default constructor
+	    .constructor()    
+	    
+		.method( "greet", &World::greet , "get the message" )
+		.method( "set", &World::set     , "set the message" )
 	;
 }                     
 
