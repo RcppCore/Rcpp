b2affc4c1099ef9ca22d52f432b5d060f7cb78a4
Romain François <romain@r-enthusiasts.com>
2010-11-26 19:01:36 +0000

    fixed stupid bug

--- inst/include/Rcpp/macros/unroll.h
+++ inst/include/Rcpp/macros/unroll.h
@@ -43,5 +43,25 @@ switch (n - i){                                   \
       {}                                          \
 }
 
+#define RCPP_LOOP_UNROLL_LHSFUN(TARGET,FUN,SOURCE)           \
+int __trip_count = n >> 2 ;                       \
+int i = 0 ;                                       \
+for ( ; __trip_count > 0 ; --__trip_count) {      \
+	TARGET[FUN(i)] = SOURCE[i] ; i++ ;                 \
+	TARGET[FUN(i)] = SOURCE[i] ; i++ ;                 \
+	TARGET[FUN(i)] = SOURCE[i] ; i++ ;                 \
+	TARGET[FUN(i)] = SOURCE[i] ; i++ ;                 \
+}                                                 \
+switch (n - i){                                   \
+  case 3:                                         \
+      TARGET[FUN(i)] = SOURCE[i] ; i++ ;               \
+  case 2:                                         \
+      TARGET[FUN(i)] = SOURCE[i] ; i++ ;               \
+  case 1:                                         \
+      TARGET[FUN(i)] = SOURCE[i] ; i++ ;               \
+  case 0:                                         \
+  default:                                        \
+      {}                                          \
+}
 
 #endif
