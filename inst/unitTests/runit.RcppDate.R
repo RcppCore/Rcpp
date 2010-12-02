dba26599681fbb7e6e1f09f2def18b417f9963b0
Romain François <romain@r-enthusiasts.com>
2010-12-02 09:01:33 +0000

    adding deprecation hooks (7)

--- inst/unitTests/runit.RcppDate.R
+++ inst/unitTests/runit.RcppDate.R
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
 
-if( Rcpp::capabilities()["classic api"] ){
+if( Rcpp:::capabilities()["classic api"] ){
 
 .setUp <- function() {
     if( ! exists( ".rcpp.RcppDate", globalenv() )) {
