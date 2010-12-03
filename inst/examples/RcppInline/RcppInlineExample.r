0c77223a3b10fd2cc516ae0f0df23280af70b3d6
Romain François <romain@r-enthusiasts.com>
2010-12-03 00:00:24 +0000

    cleaning

--- inst/examples/RcppInline/RcppInlineExample.r
+++ inst/examples/RcppInline/RcppInlineExample.r
@@ -4,38 +4,21 @@ suppressMessages(library(Rcpp))
 suppressMessages(library(inline))
 
 foo <- '
-  SEXP  rl = R_NilValue;        // Use this when there is nothing to be returned.
-  char* exceptionMesg = NULL;   // msg var in case of error
-
-  try {
-    RcppVector<int> vec(v);     // vec parameter viewed as vector of ints.
-    int n = vec.size(), i = 0;
-    if (n != 10000) throw std::length_error("Wrong vector size");
+    IntegerVector vec(10000);     // vec parameter viewed as vector of ints.
+    int i = 0;
     for (int a = 0; a < 9; a++)
       for (int b = 0; b < 9; b++)
         for (int c = 0; c < 9; c++)
           for (int d = 0; d < 9; d++)
             vec(i++) = a*b - c*d;
 
-    RcppResultSet rs;           // Build result set to be returned as a list to R.
-    rs.add("vec", vec);         // vec as named element with name "vec"
-    rl = rs.getReturnList();    // Get the list to be returned to R.
-  } catch(std::exception& ex) {
-    exceptionMesg = copyMessageToR(ex.what());
-  } catch(...) {
-    exceptionMesg = copyMessageToR("unknown reason");
-  }
-
-  if (exceptionMesg != NULL) Rf_error(exceptionMesg);
-
-  return rl;
+    return vec; 
 '
 
-funx <- cxxfunction(signature(v="numeric"), foo, plugin = "Rcpp" )
+funx <- cxxfunction(signature(n="integer"), foo, plugin = "Rcpp" )
 
 dd.inline.rcpp <- function() {
-    x <- integer(10000)
-    res <- funx(v=x)[[1]]
+    res <- funx()
     tabulate(res)
 }
 
