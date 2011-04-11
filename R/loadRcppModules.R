3c2f564b5fe3e23341808b195177f96c609ee330
Romain François <romain@r-enthusiasts.com>
2011-04-11 09:34:06 +0000

    new R function : loadRcppModules

---
+++ R/loadRcppModules.R
@@ -0,0 +1,35 @@
+
+loadRcppModules <- function(){
+   # hunt for the namespace of the package that calls this
+   calls <- sys.calls()
+   w <- which( sapply( calls, function(call){
+      identical( call[[1L]], as.name( "runHook" ) ) 
+   } ) )
+   if( !length(w) ) 
+    stop( "loadRcppModules can only be used within a .onLoad function" )
+   w <- tail( w, 1L )
+   call <- calls[[w]]
+   if( !identical( call[[2L]], ".onLoad" ) ) 
+    stop( "loadRcppModules can only be used within a .onLoad function" )
+   f <- sys.frame( w )
+   ns <- get("env", f )
+   if( !isNamespace( ns ) ) 
+    stop( "loadRcppModules not called from a namespace" )
+   pkg <- get( "pkgname", f )
+    
+   # look for declared modules in the DESCRIPTION fields
+   description <- packageDescription( pkg )
+   modules <- description[["RcppModules"]]
+   if( !is.null( modules ) ){
+        modules <- strsplit( modules, "[[:space:]]*,[[:space:]]*")[[1L]]
+        for( m in modules ){
+            tryCatch( {
+                mod <- Module( m, pkg, mustStart = TRUE)
+                populate( mod, ns )
+            }, error = function(e){
+                stop( sprintf( "failed to load module %s from package %s", m, pkg ) )  
+            })
+        }
+   }
+    
+}
--- inst/unitTests/testRcppModule/R/zzz.R
+++ inst/unitTests/testRcppModule/R/zzz.R
@@ -1,8 +1,6 @@
 NAMESPACE <- environment()
 
 .onLoad <- function(libname, pkgname){
-    populate( Module("yada"), NAMESPACE )
-    populate( Module("stdVector"), NAMESPACE )
-    populate( Module("NumEx"), NAMESPACE )
+    loadRcppModules()
 }
 
