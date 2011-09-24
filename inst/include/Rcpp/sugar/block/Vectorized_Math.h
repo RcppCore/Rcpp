91a1be09afc4b4a05c6d527bcb70523a98a47211
Dirk Deelbuettel <edd@debian.org>
2011-09-24 17:35:28 +0000

    applied 2/3 of Martyn's patch for Solaris (excluded the reference vs full copy changes) upped Version to 0.9.6.2

--- inst/include/Rcpp/sugar/block/Vectorized_Math.h
+++ inst/include/Rcpp/sugar/block/Vectorized_Math.h
@@ -1,4 +1,4 @@
-// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 8 -*-
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
 //
 // SugarBlock.h: Rcpp R/C++ interface class library -- sugar functions
 //
@@ -25,54 +25,56 @@
 namespace Rcpp{
 namespace sugar{
 
-template <double Func(double), bool NA, typename VEC>
+extern "C" typedef double (*DDFun)(double);
+
+template <DDFun Func, bool NA, typename VEC>
 class Vectorized : public VectorBase<REALSXP, NA, Vectorized<Func,NA,VEC> >{
 public:
     typedef typename Rcpp::VectorBase<REALSXP,NA,VEC> VEC_TYPE ;
-	typedef typename Rcpp::traits::Extractor<REALSXP,NA,VEC>::type VEC_EXT ;
-	
-	Vectorized( const VEC_TYPE& object_) : object( object_.get_ref() ){}
-	inline double operator[]( int i) const {
-	    return Func( object[i] ) ;
-	}
-	inline int size() const { return object.size(); }
-	
+    typedef typename Rcpp::traits::Extractor<REALSXP,NA,VEC>::type VEC_EXT ;
+        
+    Vectorized( const VEC_TYPE& object_) : object( object_.get_ref() ){}
+    inline double operator[]( int i) const {
+        return Func( object[i] ) ;
+    }
+    inline int size() const { return object.size(); }
+        
 private:
-    const VEC_EXT& object ;	
+    const VEC_EXT& object ;     
 } ;
 
-template <double Func(double), bool NA, typename VEC>
+template <DDFun Func, bool NA, typename VEC>
 class Vectorized_INTSXP : public VectorBase<REALSXP, NA, Vectorized_INTSXP<Func,NA,VEC> >{
 public:
     typedef typename Rcpp::VectorBase<INTSXP,NA,VEC> VEC_TYPE ;
-	typedef typename Rcpp::traits::Extractor<INTSXP,NA,VEC>::type VEC_EXT ;
-	
-	Vectorized_INTSXP( const VEC_TYPE& object_) : object( object_.get_ref() ){}
-	inline double operator[]( int i) const {
-	    int x = object[i] ;
-	    if( x == NA_INTEGER ) return NA_REAL ;
-	    return Func( x ) ;
-	}
-	inline int size() const { return object.size(); }
-	
+    typedef typename Rcpp::traits::Extractor<INTSXP,NA,VEC>::type VEC_EXT ;
+        
+    Vectorized_INTSXP( const VEC_TYPE& object_) : object( object_.get_ref() ){}
+    inline double operator[]( int i) const {
+        int x = object[i] ;
+        if( x == NA_INTEGER ) return NA_REAL ;
+        return Func( x ) ;
+    }
+    inline int size() const { return object.size(); }
+        
 private:
-    const VEC_EXT& object ;	
+    const VEC_EXT& object ;     
 } ;
-template <double Func(double), typename VEC>
+template <DDFun Func, typename VEC>
 class Vectorized_INTSXP<Func,false,VEC> : 
     public VectorBase<REALSXP,false, Vectorized_INTSXP<Func,false,VEC> >{
 public:
     typedef typename Rcpp::VectorBase<INTSXP,false,VEC> VEC_TYPE ;
-	typedef typename Rcpp::traits::Extractor<INTSXP,false,VEC>::type VEC_EXT ;
-	
-	Vectorized_INTSXP( const VEC_TYPE& object_) : object( object_.get_ref() ){}
-	inline double operator[]( int i) const {
-	    return Func( object[i] ) ;
-	}
-	inline int size() const { return object.size(); }
-	
+    typedef typename Rcpp::traits::Extractor<INTSXP,false,VEC>::type VEC_EXT ;
+        
+    Vectorized_INTSXP( const VEC_TYPE& object_) : object( object_.get_ref() ){}
+    inline double operator[]( int i) const {
+        return Func( object[i] ) ;
+    }
+    inline int size() const { return object.size(); }
+        
 private:
-    const VEC_EXT& object ;	
+    const VEC_EXT& object ;     
 } ;
 
 } // sugar
@@ -80,18 +82,18 @@ private:
 
 #define VECTORIZED_MATH_1(__NAME__,__SYMBOL__)                               \
 namespace Rcpp{                                                              \
-	template <bool NA, typename T>                                           \
-	inline sugar::Vectorized<__SYMBOL__,NA,T>                                \
-	__NAME__( const VectorBase<REALSXP,NA,T>& t ){                           \
-		return sugar::Vectorized<__SYMBOL__,NA,T>( t ) ;                     \
-	}                                                                        \
-	inline sugar::Vectorized<__SYMBOL__,true,NumericVector>                  \
-	__NAME__( SEXP x){ return __NAME__( NumericVector( x ) ) ; }             \
-	template <bool NA, typename T>                                           \
-	inline sugar::Vectorized_INTSXP<__SYMBOL__,NA,T>                         \
-	__NAME__( const VectorBase<INTSXP,NA,T>& t	){                           \
-		return sugar::Vectorized_INTSXP<__SYMBOL__,NA,T>( t ) ;              \
-	}                                                                        \
+        template <bool NA, typename T>                                           \
+        inline sugar::Vectorized<__SYMBOL__,NA,T>                                \
+        __NAME__( const VectorBase<REALSXP,NA,T>& t ){                           \
+                return sugar::Vectorized<__SYMBOL__,NA,T>( t ) ;                     \
+        }                                                                        \
+        inline sugar::Vectorized<__SYMBOL__,true,NumericVector>                  \
+        __NAME__( SEXP x){ return __NAME__( NumericVector( x ) ) ; }             \
+        template <bool NA, typename T>                                           \
+        inline sugar::Vectorized_INTSXP<__SYMBOL__,NA,T>                         \
+        __NAME__( const VectorBase<INTSXP,NA,T>& t      ){                           \
+                return sugar::Vectorized_INTSXP<__SYMBOL__,NA,T>( t ) ;              \
+        }                                                                        \
 }
 
 
