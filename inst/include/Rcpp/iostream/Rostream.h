e9452e2f162b210ccacd23ad80c37bc9d80c772f
Dirk Deelbuettel <edd@debian.org>
2011-10-26 01:34:00 +0000

    Adds patch by Jelmer Ypma which implements a new device Rcout which acts like std::cout but is implemented using Rprintf to cooperate more cleanly with the output buffering by R

---
+++ inst/include/Rcpp/iostream/Rostream.h
@@ -0,0 +1,47 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+//
+// Rostream.h: Rcpp R/C++ interface class library -- output stream
+//
+// Copyright (C) 2011           Dirk Eddelbuettel, Romain Francois and Jelmer Ypma
+//
+// This file is part of Rcpp.
+//
+// Rcpp is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Rcpp is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef __ROSTREAM_H__
+#define __ROSTREAM_H__
+
+#include <RcppCommon.h>
+#include <Rcpp/iostream/Rstreambuf.h>
+
+// modified from 
+// http://stackoverflow.com/questions/243696/correctly-over-loading-a-stringbuf-to-replace-cout-in-a-matlab-mex-file
+
+namespace Rcpp {
+
+    class Rostream : public std::ostream {
+        
+    protected:
+        Rstreambuf buf;
+	
+    public:
+        Rostream();
+    };
+    
+    // declare global variable
+    extern Rostream Rcout;
+
+}
+
+#endif
---
+++ src/Rostream.cpp
@@ -0,0 +1,28 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+//
+// Rostream.cpp: Rcpp R/C++ interface class library -- output stream
+//
+// Copyright (C) 2011           Dirk Eddelbuettel, Romain Francois and Jelmer Ypma
+//
+// This file is part of Rcpp.
+//
+// Rcpp is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Rcpp is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <RcppCommon.h>
+#include <Rcpp/iostream/Rostream.h>
+
+Rcpp::Rostream::Rostream() : std::ostream( &buf ) {}
+
+// define global variable Rcout
+Rcpp::Rostream Rcpp::Rcout;
