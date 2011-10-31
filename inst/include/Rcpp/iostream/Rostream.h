a8a25c8108607fbc73872c13167f274df78396b3
Dirk Eddelbuettel <edd@debian.org>
2011-10-31 19:48:01 +0000

    committing (albeit commented-out) Jelmer's patch for operator<< -- this may need more work and/or go to RObject.h or ...

--- inst/include/Rcpp/iostream/Rostream.h
+++ inst/include/Rcpp/iostream/Rostream.h
@@ -22,8 +22,9 @@
 #ifndef __ROSTREAM_H__
 #define __ROSTREAM_H__
 
-#include <RcppCommon.h>
-#include <Rcpp/iostream/Rstreambuf.h>
+#include <iomanip>				// USES setw
+
+#include <Rcpp.h>
 
 // modified from 
 // http://stackoverflow.com/questions/243696/correctly-over-loading-a-stringbuf-to-replace-cout-in-a-matlab-mex-file
@@ -41,6 +42,45 @@ namespace Rcpp {
     
     // declare global variable
     extern Rostream Rcout;
+
+    // template <int RTYPE> std::ostream& operator<<(std::ostream& out, const Rcpp::Vector< RTYPE >& v) {
+    //     out << "[1] " << v[0];
+    //     for (int i=1;i<v.size();i++) {
+    //         out << " " << v[i];
+    //     }
+    //     return out;  // for multiple << operators.
+    // }
+
+    // template <int RTYPE> std::ostream& operator<<(std::ostream& out, const Rcpp::Matrix< RTYPE >& m) {
+    //     // width of columns showing values
+    //     int val_col_width = 12;
+	
+    //     // width of first column, showing row index
+    //     int max_row_idx = m.rows() + 1;
+    //     int idx_col_width = 0;
+    //     while(max_row_idx > 0) {
+    //         max_row_idx /= 10;
+    //         idx_col_width++;
+    //     }
+	
+    //     // print column headers (add 1 to have R indexing)
+    //     out << std::setw( val_col_width + idx_col_width + 3 ) << "[,1]";
+    //     for (int jcol=1;jcol<m.ncol();jcol++) {
+    //         out << std::setw( val_col_width-2 ) << "[," << jcol+1 << "]";
+    //     }
+    //     out << std::endl;
+    //     for (int irow=0;irow<m.nrow();irow++) {
+    //         // print row header (add 1 to have R indexing)
+    //         out << "[" << std::setw( idx_col_width ) << irow+1 << ",]";
+	
+    //         // print values in current row
+    //         for (int jcol=0;jcol<m.ncol();jcol++) {
+    //             out << std::setw( val_col_width ) << m( irow, jcol );
+    //         }
+    //         out << std::endl;
+    //     }
+    //     return out;  // for multiple << operators.
+    // }
 
 }
 
