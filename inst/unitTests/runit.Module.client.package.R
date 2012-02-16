775d932d4d4a858599c029fd7af0c924a43e79c5
Dirk Eddelbuettel <edd@debian.org>
2012-02-16 20:27:04 +0000

    with Doug's fix, re-enabling test for Rcpp 0.9.10

--- inst/unitTests/runit.Module.client.package.R
+++ inst/unitTests/runit.Module.client.package.R
@@ -31,10 +31,10 @@
 ## It now (Dec 2011) appears to fail on Windows too
 .onWindows <- .Platform$OS.type == "windows"
 
-## Disabling this test for now
-.runThisTest <- FALSE
+## Re-enabling this test for now
+.runThisTest <- TRUE
 
-if( .runThisTest && Rcpp:::capabilities()[["Rcpp modules"]] && ! .badOSX && ! .onWindows) {
+if (.runThisTest && Rcpp:::capabilities()[["Rcpp modules"]] && ! .badOSX && ! .onWindows) {
 
     test.Module.package <- function( ){
 
