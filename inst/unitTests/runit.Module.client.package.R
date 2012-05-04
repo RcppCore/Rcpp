1028e1158a402a5eb3200eb96c6a098cf517fddb
Dirk Eddelbuettel <edd@debian.org>
2012-05-04 13:43:19 +0000

    added new test, uncommented for now (and posted TODO to rcpp-core)

--- inst/unitTests/runit.Module.client.package.R
+++ inst/unitTests/runit.Module.client.package.R
@@ -67,4 +67,35 @@ if (.runThisTest && Rcpp:::capabilities()[["Rcpp modules"]] && ! .badOSX && ! .o
 
     }
 
+    ## ## added test for 'testRcppClass' example of extending C++ classes via R
+    ## test.Class.package <- function( ){
+
+    ##     td <- tempfile()
+    ##     cwd <- getwd()
+    ##     dir.create( td )
+    ##     file.copy( system.file( "unitTests", "testRcppClass", package = "Rcpp" ) , td, recursive = TRUE)
+    ##     setwd( td )
+    ##     on.exit( { setwd( cwd) ; unlink( td, recursive = TRUE ) } )
+    ##     R <- shQuote( file.path( R.home( component = "bin" ), "R" ))
+    ##     cmd <- paste( R , "CMD build testRcppClass" )
+    ##     system( cmd )
+    ##     dir.create( "templib" )
+    ##     install.packages( "testRcppClass_0.1.tar.gz", "templib", repos = NULL, type = "source" )
+    ##     require( "testRcppClass", "templib", character.only = TRUE )
+
+    ##     v <- stdNumeric$new()
+    ##     data <- 1:10
+    ##     v$assign(data)
+    ##     v$set(3L, v$at(3L) + 1)
+
+    ##     data[[4]] <- data[[4]] +1
+
+    ##     checkEquals( v$as.vector(), data )
+
+    ##     ## a few function calls
+    ##     checkEquals( bar(2), 4)
+    ##     checkEquals( foo(2,3), 6)
+
+    ## }
+
 }
