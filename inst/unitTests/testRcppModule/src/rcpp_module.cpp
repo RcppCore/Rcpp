3348c3fb07ce26519abf0df95c6560edf5b7314e
Dirk Eddelbuettel <edd@debian.org>
2012-05-24 13:51:13 +0000

    some editing and cleanup in main Module regression test file

--- inst/unitTests/testRcppModule/src/rcpp_module.cpp
+++ inst/unitTests/testRcppModule/src/rcpp_module.cpp
@@ -1,26 +1,49 @@
-#include <Rcpp.h>
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+//
+// rcpp_module.cpp: Rcpp R/C++ interface class library -- Rcpp Module examples
+//
+// Copyright (C) 2010 - 2012  Dirk Eddelbuettel and Romain Francois
+//
+// This file is part of Rcpp.
+//
+// Rcpp is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Rcpp is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
 
-int bar( int x){
-	return x*2 ;
+
+#include <Rcpp.h>               // need to include the main Rcpp header file only
+
+int bar(int x) {                // simple free function returning an int
+    return x*2;
 }
         
-double foo( int x, double y){
-	return x * y ;
+double foo(int x, double y) {   // simple free function returning a double
+    return x * y;
 }
 
-void bla( ){
-	Rprintf( "hello\\n" ) ;
+void bla( ) {                   // no input or return but output side effect
+    Rprintf( "hello\n" );
 }
 
-void bla1( int x){
-	Rprintf( "hello (x = %d)\\n", x ) ;
+void bla1(int x) {              // output reflecting a single input
+    Rprintf( "hello (x = %d)\n", x);
 }
   
-void bla2( int x, double y){
-	Rprintf( "hello (x = %d, y = %5.2f)\\n", x, y ) ;
+void bla2(int x, double y) {    // output reflecting two inputs
+    Rprintf( "hello (x = %d, y = %5.2f)\n", x, y);
 }
 
-class World {
+
+class World {                   // a simple class with a setter and getter
 public:
     World() : msg("hello"){}
     void set(std::string msg) { this->msg = msg; }
@@ -33,20 +56,18 @@ private:
 
 
 RCPP_MODULE(yada){
-	using namespace Rcpp ;
+    using namespace Rcpp;
 	                  
-	function( "bar"   , &bar   ) ;
-	function( "foo"   , &foo   ) ;
-	function( "bla"   , &bla   ) ;
-	function( "bla1"  , &bla1   ) ;
-	function( "bla2"  , &bla2   ) ;
+    function( "bar"   , &bar   );
+    function( "foo"   , &foo   );
+    function( "bla"   , &bla   );
+    function( "bla1"  , &bla1   );
+    function( "bla2"  , &bla2   );
 	
-	class_<World>( "World" )
-	
-	    .default_constructor()
-	    
-		.method( "greet", &World::greet )
-		.method( "set", &World::set )
+    class_<World>( "World" )
+        .default_constructor()
+        .method( "greet", &World::greet )
+        .method( "set", &World::set )
 	;
 }                     
 
--- inst/unitTests/testRcppModule/src/stdVector.cpp
+++ inst/unitTests/testRcppModule/src/stdVector.cpp
@@ -1,28 +1,49 @@
-#include "rcpp_hello_world.h"
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+//
+// stdVector.cpp: Rcpp R/C++ interface class library -- Rcpp Module class example
+//
+// Copyright (C) 2010 - 2012  Dirk Eddelbuettel and Romain Francois
+//
+// This file is part of Rcpp.
+//
+// Rcpp is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Rcpp is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <Rcpp.h>               // need to include the main Rcpp header file only
 
 // convenience typedef
-typedef std::vector<double> vec ;
+typedef std::vector<double> vec;
 
 // helpers
-void vec_assign( vec* obj, Rcpp::NumericVector data ){
-	obj->assign( data.begin(), data.end() ) ;
+void vec_assign( vec* obj, Rcpp::NumericVector data) {
+    obj->assign( data.begin(), data.end() ) ;
 }
 
-void vec_insert( vec* obj, int position, Rcpp::NumericVector data){
-	vec::iterator it = obj->begin() + position ;
-	obj->insert( it, data.begin(), data.end() ) ;
+void vec_insert( vec* obj, int position, Rcpp::NumericVector data) {
+    vec::iterator it = obj->begin() + position;
+    obj->insert( it, data.begin(), data.end() );
 }
 
-Rcpp::NumericVector vec_asR( vec* obj ){
-	return Rcpp::wrap( *obj ) ;
+Rcpp::NumericVector vec_asR( vec* obj) {
+    return Rcpp::wrap( *obj );
 }
 
-void vec_set( vec* obj, int i, double value ){
-	obj->at( i ) = value ;
+void vec_set( vec* obj, int i, double value) {
+    obj->at( i ) = value;
 }
 
-void vec_resize( vec* obj, int n){ obj->resize( n ) ; }
-void vec_push_back( vec* obj, double x ){ obj->push_back( x ); }
+void vec_resize( vec* obj, int n) { obj->resize( n ); }
+void vec_push_back( vec* obj, double x ) { obj->push_back( x ); }
 
 // Wrappers for member functions that return a reference
 // Required on Solaris 
@@ -34,36 +55,36 @@ RCPP_MODULE(stdVector){
     using namespace Rcpp ;
 
     // we expose the class std::vector<double> as "vec" on the R side
-    class_<vec>( "vec")
+    class_<vec>("vec")
     
-    // exposing the default constructor
-    .constructor() 
+        // exposing the default constructor
+        .constructor() 
 
-    // exposing member functions
-    .method( "size", &vec::size)
-    .method( "max_size", &vec::max_size)
-    .method( "capacity", &vec::capacity)
-    .method( "empty", &vec::empty)
-    .method( "reserve", &vec::reserve)
-    .method( "pop_back", &vec::pop_back )
-    .method( "clear", &vec::clear )
-
-    // specifically exposing const member functions
-    .method( "back", &vec_back )
-    .method( "front", &vec_front )
-    .method( "at", &vec_at )
+        // exposing member functions -- taken directly from std::vector<double>
+        .method( "size",     &vec::size)
+        .method( "max_size", &vec::max_size)
+        .method( "capacity", &vec::capacity)
+        .method( "empty",    &vec::empty)
+        .method( "reserve",  &vec::reserve)
+        .method( "pop_back", &vec::pop_back )
+        .method( "clear",    &vec::clear )
+        
+        // specifically exposing const member functions defined above
+        .method( "back",     &vec_back )
+        .method( "front",    &vec_front )
+        .method( "at",       &vec_at )
     
-    // exposing free functions taking a std::vector<double>*
-    // as their first argument
-    .method( "assign", &vec_assign )
-    .method( "insert", &vec_insert )
-    .method( "as.vector", &vec_asR )
-    .method( "push_back", &vec_push_back )
-    .method( "resize", &vec_resize)
+        // exposing free functions taking a std::vector<double>*
+        // as their first argument
+        .method( "assign",   &vec_assign )
+        .method( "insert",   &vec_insert )
+        .method( "as.vector",&vec_asR )
+        .method( "push_back",&vec_push_back )
+        .method( "resize",   &vec_resize)
     
-    // special methods for indexing
-    .method( "[[", &vec_at )
-    .method( "[[<-", &vec_set )
+        // special methods for indexing
+        .method( "[[",       &vec_at )
+        .method( "[[<-",     &vec_set )
 
 	;
 }
