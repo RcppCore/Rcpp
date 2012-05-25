8549bb94c97c4db256f6aab278d1686c8f3045d1
Dirk Eddelbuettel <edd@debian.org>
2012-05-25 13:46:36 +0000

    added commented-out call to evalqOnLoad() for R-devel

--- inst/skeleton/zzz.R
+++ inst/skeleton/zzz.R
@@ -1,7 +1,15 @@
 
+## For R 2.15.0 we still need, but note the R CMD check issue
 .onLoad <- function(libname, pkgname){
     require("methods")  ## needed, but upsets R CMD check
     loadRcppModules()
 }
+
+## For R 2.15.1 and later this will work
+#evalqOnLoad({
+#    loadModule("NumEx", TRUE)
+#    loadModule("yada", TRUE)
+#    loadModule("stdVector", TRUE)
+#})
 
 
