bc754c6473beb2041c083815a6057d75fd35c36a
Dirk Eddelbuettel <edd@debian.org>
2012-05-28 15:30:08 +0000

    whitespace / indentation

--- inst/unitTests/testRcppClass/R/load.R
+++ inst/unitTests/testRcppClass/R/load.R
@@ -8,31 +8,31 @@ setRcppClass("stdNumeric", "vec", "stdVector")
 ## some methods that use C++ methods
 evalqOnLoad({
     ##library(methods)
-     stdNumeric$methods(
-    getEl = function(i) {
-        i <- as.integer(i)
-        if(i < 1 || i > size())
-            NA_real_
-        else
-            at(i-1L)
-    },
-    setEl = function(i, value) {
-        value <- as.numeric(value)
-        if(length(value) != 1)
-            stop("Only assigns single values")
-        i <- as.integer(i)
-        if(i < 1 || i > size())
-            stop("index out of bounds")
-        else
-            set(i-1L, value)
-    },
-    initialize = function(data = numeric()) {
-        callSuper()
-        data <- as.double(data)
-        n <- as.integer(max(50, length(data) * 2))
-        reserve(n)
-        assign(data)
-    }
+    stdNumeric$methods(
+        getEl = function(i) {
+            i <- as.integer(i)
+            if(i < 1 || i > size())
+                NA_real_
+            else
+                at(i-1L)
+        },
+        setEl = function(i, value) {
+            value <- as.numeric(value)
+            if(length(value) != 1)
+                stop("Only assigns single values")
+            i <- as.integer(i)
+            if(i < 1 || i > size())
+                stop("index out of bounds")
+            else
+                set(i-1L, value)
+        },
+        initialize = function(data = numeric()) {
+            callSuper()
+            data <- as.double(data)
+            n <- as.integer(max(50, length(data) * 2))
+            reserve(n)
+            assign(data)
+        }
     )
 })
 
