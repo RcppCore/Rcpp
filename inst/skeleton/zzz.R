57c110ccb8f2f3d5746622d559853b1b04b5f685
Dirk Deelbuettel <edd@debian.org>
2012-06-21 14:20:12 +0000

    more explicit comments on what now works with R 2.15.1

--- inst/skeleton/zzz.R
+++ inst/skeleton/zzz.R
@@ -1,15 +1,16 @@
 
-## For R 2.15.0 we still need require(), but note the R CMD check issue
-.onLoad <- function(libname, pkgname){
-    require("methods")  ## needed, but upsets R CMD check
-    loadRcppModules()
-}
+## Up until R 2.15.0, the require("methods") is needed but (now)
+## triggers an warning from R CMD check
+#.onLoad <- function(libname, pkgname){
+#    #require("methods")  ## needed with R <= 2.15.0
+#    loadRcppModules()
+#}
 
-## For R 2.15.1 and later this will work. Note that calling loadModule() triggers
+
+## For R 2.15.1 and later this also works. Note that calling loadModule() triggers
 ## a load action, so this does not have to be placed in .onLoad() or evalqOnLoad().
-#  loadModule("NumEx", TRUE)
-#  loadModule("yada", TRUE)
-#  loadModule("stdVector", TRUE)
-#})
+loadModule("NumEx", TRUE)
+loadModule("yada", TRUE)
+loadModule("stdVector", TRUE)
 
 
