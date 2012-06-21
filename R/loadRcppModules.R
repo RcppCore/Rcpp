56951c322a7ef61ccf19999cb6452512469fe422
Dirk Eddelbuettel <edd@debian.org>
2012-06-21 11:56:11 +0000

    whitespace / indentation change use lib.loc on packageDescription() call as suggested by Kurt

--- R/loadRcppModules.R
+++ R/loadRcppModules.R
@@ -16,41 +16,41 @@
 # along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
 
 loadRcppModules <- function(direct=TRUE){
-   # hunt for the namespace of the package that calls this
-   calls <- sys.calls()
-   w <- which( sapply( calls, function(call){
-      identical( call[[1L]], as.name( "runHook" ) ) 
-   } ) )
-   if( !length(w) ) 
-    stop( "loadRcppModules can only be used within a .onLoad function" )
-   w <- w[ length(w) ]
-   call <- calls[[w]]
-   if( !identical( call[[2L]], ".onLoad" ) ) 
-    stop( "loadRcppModules can only be used within a .onLoad function" )
-   f <- sys.frame( w )
-   ns <- get("env", f )
-   if( !isNamespace( ns ) ) 
-    stop( "loadRcppModules not called from a namespace" )
-   pkg <- get( "pkgname", f )
-    
-   # look for declared modules in the DESCRIPTION fields
-   description <- packageDescription( pkg )
-   modules <- description[["RcppModules"]]
-   if( !is.null( modules ) ){
+    ## hunt for the namespace of the package that calls this
+    calls <- sys.calls()
+    w <- which( sapply( calls, function(call){
+        identical( call[[1L]], as.name( "runHook" ) )
+    } ) )
+    if( !length(w) )
+        stop( "loadRcppModules can only be used within a .onLoad function" )
+    w <- w[ length(w) ]
+    call <- calls[[w]]
+    if( !identical( call[[2L]], ".onLoad" ) )
+        stop( "loadRcppModules can only be used within a .onLoad function" )
+    f <- sys.frame( w )
+    ns <- get("env", f )
+    if( !isNamespace( ns ) )
+        stop( "loadRcppModules not called from a namespace" )
+    pkg <- get( "pkgname", f )
+    lib <- get( "libname", f )
+
+    ## look for declared modules in the DESCRIPTION fields
+    description <- packageDescription(pkg, lib.loc=lib)
+    modules <- description[["RcppModules"]]
+    if( !is.null( modules ) ){
         modules <- strsplit( modules, "[[:space:]]*,[[:space:]]*")[[1L]]
         for( m in modules ){
             tryCatch( {
                 mod <- Module( m, pkg, mustStart = TRUE)
                 if(isTRUE(direct)){
-                   populate( mod, ns )
+                    populate( mod, ns )
                 } else {
-                    forceAssignInNamespace( m, mod, ns ) 
+                    forceAssignInNamespace( m, mod, ns )
                 }
                 assign(.moduleMetaName(m), mod, envir = ns)
             }, error = function(e){
-                stop( sprintf( "failed to load module %s from package %s\n%s", m, pkg, conditionMessage(e) ) )  
+                stop( sprintf( "failed to load module %s from package %s\n%s", m, pkg, conditionMessage(e) ) )
             })
         }
    }
-    
 }
