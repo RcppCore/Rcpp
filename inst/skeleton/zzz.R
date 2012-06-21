5cc1f2ff2d963ccda651defa5280fec7a7096f34
Dirk Eddelbuettel <edd@debian.org>
2012-06-21 13:54:11 +0000

    added some Rcpp modules documentation updates for loadRcppModules and loadModule

--- inst/skeleton/zzz.R
+++ inst/skeleton/zzz.R
@@ -5,11 +5,11 @@
     loadRcppModules()
 }
 
-## For R 2.15.1 and later this will work
-#evalqOnLoad({
-#    loadModule("NumEx", TRUE)
-#    loadModule("yada", TRUE)
-#    loadModule("stdVector", TRUE)
+## For R 2.15.1 and later this will work. Note that calling loadModule() triggers
+## a load action, so this does not have to be placed in .onLoad() or evalqOnLoad().
+#  loadModule("NumEx", TRUE)
+#  loadModule("yada", TRUE)
+#  loadModule("stdVector", TRUE)
 #})
 
 
