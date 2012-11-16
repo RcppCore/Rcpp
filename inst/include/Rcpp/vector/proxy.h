d14b68c25fac25cb64c4918fda7a4b6d2c9d7f25
Romain François <romain@r-enthusiasts.com>
2012-11-16 21:28:09 +0000

    deal with const ness

--- inst/include/Rcpp/vector/proxy.h
+++ inst/include/Rcpp/vector/proxy.h
@@ -226,6 +226,20 @@ namespace traits {
 	} ;
 
 	template <int RTYPE>
+	struct r_vector_const_proxy{
+		typedef const typename storage_type<RTYPE>::type& type ;
+	} ;
+	template<> struct r_vector_const_proxy<STRSXP> {
+		typedef ::Rcpp::internal::string_proxy<STRSXP> type ;
+	} ;
+	template<> struct r_vector_const_proxy<EXPRSXP> {
+		typedef ::Rcpp::internal::generic_proxy<EXPRSXP> type ;
+	} ;
+	template<> struct r_vector_const_proxy<VECSXP> {
+		typedef ::Rcpp::internal::generic_proxy<VECSXP> type ;
+	} ;
+
+	template <int RTYPE>
 	struct r_vector_iterator {
 		typedef typename storage_type<RTYPE>::type* type ;
 	};
