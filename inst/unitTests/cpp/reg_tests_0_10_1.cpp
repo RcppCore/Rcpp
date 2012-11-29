ba039c685e4f4e7bf05f031387f6cb612c89715d
Romain François <romain@r-enthusiasts.com>
2012-11-29 13:32:12 +0000

    moving sugar tests to runit.sugar.R

--- inst/unitTests/cpp/reg_tests_0_10_1.cpp
+++ inst/unitTests/cpp/reg_tests_0_10_1.cpp
@@ -6,36 +6,3 @@ int DataFrame_nrows( DataFrame df){
     return df.nrows() ;
 }
 
-// [[Rcpp::export]]
-double RangeIndexer_sugar( NumericVector x ){
-    return max( x[ seq(0, 4) ] ) ;    
-}
-
-// [[Rcpp::export]]
-IntegerVector test_self_match( CharacterVector x){
-    return self_match( x ) ;
-}
-
-// [[Rcpp::export]]
-IntegerVector test_table( CharacterVector x){
-    return table( x ) ;
-}
-
-// [[Rcpp::export]]
-LogicalVector test_duplicated( CharacterVector x){
-    return duplicated( x ) ;
-}
-
-// [[Rcpp::export]]
-IntegerVector test_union( IntegerVector x, IntegerVector y){
-    return union_( x, y) ;
-}
-// [[Rcpp::export]]
-IntegerVector test_setdiff( IntegerVector x, IntegerVector y){
-    return setdiff( x, y) ;
-}
-// [[Rcpp::export]]
-IntegerVector test_intersect( IntegerVector x, IntegerVector y){
-    return intersect( x, y ) ;
-}
-
