8455a6d6895614ede2a3c776194b941e5a01913f
Dirk Eddelbuettel <edd@debian.org>
2012-12-03 13:04:11 +0000

    reactivate test now that we have Vector.cpp, but test fails...

--- inst/unitTests/runit.Vector.R
+++ inst/unitTests/runit.Vector.R
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
 
-.runThisTest <- FALSE ## Sys.getenv("RunAllRcppTests") == "yes"
+.runThisTest <- Sys.getenv("RunAllRcppTests") == "yes"
 
 if (.runThisTest) {
 
