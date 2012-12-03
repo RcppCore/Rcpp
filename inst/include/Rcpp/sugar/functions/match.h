831acf4d70e9dc4d3c2fac8786ee06e5e0630ab7
Romain François <romain@r-enthusiasts.com>
2012-12-03 23:04:10 +0000

    faster match, based on Simon's fastmatch

--- inst/include/Rcpp/sugar/functions/match.h
+++ inst/include/Rcpp/sugar/functions/match.h
@@ -23,69 +23,12 @@
 #define Rcpp__sugar__match_h
           
 namespace Rcpp{
-namespace sugar{
-
-template <typename HASH, typename STORAGE>
-class HashIndexInserter {
-public:
-    HashIndexInserter( HASH& hash_ ) : hash(hash_), index(1){}
-    
-    inline void operator()( STORAGE value ){
-        hash.insert( std::make_pair(value, index++) ) ;
-    }
-    
-private:
-    HASH& hash ;
-    int index;
-} ; 
-template <typename HASH, typename STORAGE>
-class HashIndexFinder {
-public:
-    HashIndexFinder( HASH& hash_) : hash(hash_), end(hash.end()) {}
-    
-    inline int operator()( STORAGE value ){
-        typename HASH::const_iterator it = hash.find(value);
-        if( it == end ){
-            return NA_INTEGER ;    
-        } else {
-            return it->second ;    
-        }
-    }
-    
-private:
-    HASH& hash ;
-    typename HASH::const_iterator end ;
-} ;
-
-    
-// version for INTSXP, REALSXP, RAWSXP, CPLXSXP
-template <int RTYPE, typename TABLE_T>        
-class IndexHash {
-public:
-    typedef typename Rcpp::traits::storage_type<RTYPE>::type STORAGE ;
-    
-    IndexHash( const TABLE_T& table ): hash() {
-        for_each( table.begin(), table.end(), Inserter(hash) ) ;
-    }
-    
-    template <typename T>
-    IntegerVector match( const T& obj ){
-        return IntegerVector( obj.begin(), obj.end(), Finder(hash) ) ;
-    }
- 
-private:
-    typedef RCPP_UNORDERED_MAP<STORAGE,int> HASH ;
-    typedef HashIndexInserter<HASH,STORAGE> Inserter ;
-    typedef HashIndexFinder<HASH,STORAGE> Finder ;
-    HASH hash ;    
-}; 
-    
-} // sugar
 
 template <int RTYPE, bool NA, typename T, bool RHS_NA, typename RHS_T>
-inline IntegerVector match( const VectorBase<RTYPE,NA,T>& x, const VectorBase<RTYPE,RHS_NA,RHS_T>& table ){
-    sugar::IndexHash<RTYPE,RHS_T> hash( table.get_ref() ) ;
-    return hash.match( x.get_ref() ) ;
+inline IntegerVector match( const VectorBase<RTYPE,NA,T>& x, const VectorBase<RTYPE,RHS_NA,RHS_T>& table_ ){
+    Vector<RTYPE> table = table_ ;
+    sugar::IndexHash<RTYPE> hash( table ) ;
+    return hash.lookup( x.get_ref() ) ;
 }
 
 } // Rcpp
