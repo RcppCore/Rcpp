e6a3f8c3117c6804e151ba281a354ebd765fb714
Romain François <romain@r-enthusiasts.com>
2012-12-04 17:32:46 +0000

    faster self_match

--- inst/include/Rcpp/sugar/functions/self_match.h
+++ inst/include/Rcpp/sugar/functions/self_match.h
@@ -23,51 +23,11 @@
 #define Rcpp__sugar__self_match_h
           
 namespace Rcpp{
-namespace sugar{
-
-template <typename HASH, typename STORAGE>
-class SelfInserter {
-public:
-    SelfInserter( HASH& hash_ ) : hash(hash_), index(0) {}
-    
-    inline int operator()( STORAGE value ){
-        typename HASH::iterator it = hash.find( value ) ;
-        if( it == hash.end() ){
-            hash.insert( std::make_pair(value, ++index) ) ;
-            return index ; 
-        } else {
-            return it->second ;
-        }   
-    }
-    
-private:
-    HASH& hash ;
-    int index;
-} ; 
-
-template <int RTYPE, typename TABLE_T>        
-class SelfMatch {
-public:
-    typedef typename Rcpp::traits::storage_type<RTYPE>::type STORAGE ;
-    
-    SelfMatch( const TABLE_T& table ): hash(), result(table.size()) {
-        std::transform( table.begin(), table.end(), result.begin(), Inserter(hash) ) ;
-    }
-    
-    inline operator IntegerVector() const { return result ; }
-    
-private:
-    typedef RCPP_UNORDERED_MAP<STORAGE, int> HASH ;
-    typedef SelfInserter<HASH,STORAGE> Inserter ;
-    HASH hash ; 
-    IntegerVector result ;
-}; 
-    
-} // sugar
 
 template <int RTYPE, bool NA, typename T>
 inline IntegerVector self_match( const VectorBase<RTYPE,NA,T>& x ){
-    return sugar::SelfMatch<RTYPE,T>(x.get_ref()) ;
+    Vector<RTYPE> vec(x) ;
+    return sugar::IndexHash<RTYPE>(vec).fill_and_self_match() ;
 }
 
 
