4bd2b9b6b94787e4ebe4d0b1d9f06b9a327d23da
Romain François <romain@r-enthusiasts.com>
2012-12-08 20:17:44 +0000

    more meat in Matrix

--- inst/include/Rcpp/vector/Matrix.h
+++ inst/include/Rcpp/vector/Matrix.h
@@ -26,9 +26,14 @@ class Dimension ;
 
 template <int RTYPE> 
 class Matrix : public Vector<RTYPE>, public MatrixBase<RTYPE,true, Matrix<RTYPE> > {
+    int nrows ; 
+    
 public:
     struct r_type : traits::integral_constant<int,RTYPE>{} ;
     struct can_have_na : traits::true_type{} ;
+    typedef MatrixRow<RTYPE> Row ;
+    typedef MatrixColumn<RTYPE> Column ;
+    typedef SubMatrix<RTYPE> Sub ;
         
     typedef Vector<RTYPE> VECTOR ;
     typedef typename VECTOR::iterator iterator ;
@@ -40,158 +45,25 @@ public:
     
     Matrix() : VECTOR() {}
         
-    Matrix(SEXP x) : VECTOR(), nrows(0) {
-        if( ! ::Rf_isMatrix(x) ) throw not_compatible("not a matrix") ;
-        SEXP y = r_cast<RTYPE>( x ) ;
-        VECTOR::setSEXP( y );
-        nrows = VECTOR::dims()[0] ;
-    }
+    Matrix(SEXP x) ;
         
+    Matrix( const int& n) ; 
     Matrix( const Dimension& dims)  ;
-        
     Matrix( const int& nrows_, const int& ncols)  ;
         
     template <typename Iterator>
     Matrix( const int& nrows_, const int& ncols, Iterator start ) ;
         
-    Matrix( const int& n) ; 
-        
-    Matrix( const Matrix& other) : VECTOR(), nrows(other.nrows) {
-        SEXP x = other.asSexp() ;
-        if( ! ::Rf_isMatrix(x) ) throw not_compatible("not a matrix") ;
-        VECTOR::setSEXP( x ) ;
-    }
-        
-    Matrix& operator=(const Matrix& other) {
-        SEXP x = other.asSexp() ;
-        if( ! ::Rf_isMatrix(x) ) not_compatible("not a matrix") ;
-        VECTOR::setSEXP( x ) ;
-        nrows = other.nrows ;
-        return *this ;
-    }
+    Matrix( const Matrix& other)  ;
         
     template <bool NA, typename MAT>
-    Matrix( const MatrixBase<RTYPE,NA,MAT>& other ) : VECTOR(), nrows(other.nrow()) {
-        int nc = other.ncol() ;
-        RObject::setSEXP( Rf_allocMatrix( RTYPE, nrows, nc ) ) ;
-        import_matrix_expression<NA,MAT>( other, nrows, nc ) ;
-    }
-        
-    // defined later
+    Matrix( const MatrixBase<RTYPE,NA,MAT>& other )  ;
+    
     Matrix( const SubMatrix<RTYPE>& ) ;
+    
+    Matrix& operator=(const Matrix& other)  ;
     Matrix& operator=( const SubMatrix<RTYPE>& ) ;
    
-private:
-        
-    template <bool NA, typename MAT>
-    void import_matrix_expression( const MatrixBase<RTYPE,NA,MAT>& other, int nr, int nc ){
-        iterator start = VECTOR::begin() ;
-        for( int j=0; j<nc; j++){
-            for( int i=0; i<nr; i++, ++start){
-                *start = other(i,j) ;
-            }
-        }
-    }
-
-public:
-        
-    template <typename U>
-    void fill_diag( const U& u){
-        fill_diag__dispatch( typename traits::is_trivial<RTYPE>::type(), u ) ;  
-    }
-        
-    template <typename U>
-    static Matrix diag( int size, const U& diag_value ){
-        Matrix res(size,size) ;
-        res.fill_diag( diag_value ) ;
-        return res ;
-    }
-    
-    inline Proxy operator[]( int i ){
-        return static_cast< Vector<RTYPE>* >( this )->operator[]( i ) ;
-    }
-    
-    inline const_Proxy operator[]( int i ) const {
-        return static_cast< const Vector<RTYPE>* >( this )->operator[]( i ) ;
-    }
-    
-    inline Proxy operator()( const size_t& i, const size_t& j) {
-        return static_cast< Vector<RTYPE>* >( this )->operator[]( 
-                                                                 offset( i, j )
-                                                                  ) ;
-    }
-        
-    inline const_Proxy operator()( const size_t& i, const size_t& j) const {
-        return static_cast< const Vector<RTYPE>* >( this )->operator[]( 
-                                                                       offset( i, j )
-                                                                        ) ;
-    }
-        
-    typedef MatrixRow<RTYPE> Row ;
-    typedef MatrixColumn<RTYPE> Column ;
-    typedef SubMatrix<RTYPE> Sub ;
-
-    inline Row operator()( int i, internal::NamedPlaceHolder ){
-        return Row( *this, i ) ;
-    }
-        
-    inline Column operator()( internal::NamedPlaceHolder, int i ){
-        return Column( *this, i ) ;
-    }
-    inline Column operator()( internal::NamedPlaceHolder, int i ) const {
-        return Column( *this, i ) ;
-    }
-        
-    inline Sub operator()( const Range& row_range, const Range& col_range){
-        return Sub( const_cast<Matrix&>(*this), row_range, col_range ) ;
-    }
-        
-    inline Sub operator()( internal::NamedPlaceHolder, const Range& col_range){
-        return Sub( const_cast<Matrix&>(*this), Range(0,nrow()-1) , col_range ) ;
-    }
-        
-    inline Sub operator()( const Range& row_range, internal::NamedPlaceHolder ){
-        return Sub( const_cast<Matrix&>(*this), row_range, Range(0,ncol()-1) ) ;
-    }
-        
-        
-private:
-    
-    inline int offset( int i, int j) const {
-        return i + nrows * j ;
-    }
-    
-    virtual void update(){
-        RCPP_DEBUG_1( "%s::update", DEMANGLE(Matrix) ) ;
-        VECTOR::update_vector() ;
-    }
-        
-    template <typename U>
-    void fill_diag__dispatch( traits::false_type, const U& u){
-        SEXP elem = PROTECT( converter_type::get( u ) ) ;
-        int n = Matrix::ncol() ;
-        int offset = n +1 ;
-        iterator it( VECTOR::begin()) ;
-        for( int i=0; i<n; i++){
-            *it = ::Rf_duplicate( elem );
-            it += offset; 
-        }
-        UNPROTECT(1); // elem
-    }
-        
-    template <typename U>
-    void fill_diag__dispatch( traits::true_type, const U& u){
-        stored_type elem = converter_type::get( u ) ;
-        int n = Matrix::ncol() ;
-        int offset = n + 1 ;
-        iterator it( VECTOR::begin()) ;
-        for( int i=0; i<n; i++){
-            *it = elem ;
-            it += offset; 
-        }
-    }
-
-public:
     inline int ncol() const {
         return VECTOR::dims()[1]; 
     }
@@ -212,9 +84,42 @@ public:
     inline const_iterator end() const{ return VECTOR::end() ; }
     inline iterator begin() { return VECTOR::begin() ; }
     inline iterator end() { return VECTOR::end() ; }
+
+    template <typename U> void fill_diag( const U& u) ;
+        
+    template <typename U> static Matrix diag( int size, const U& diag_value ) ;
     
+    inline Proxy operator[]( int i ) ;
+    inline const_Proxy operator[]( int i ) const  ;
+    
+    inline Proxy operator()( const size_t& i, const size_t& j) ; 
+    inline const_Proxy operator()( const size_t& i, const size_t& j) const  ;
+    
+    inline Row operator()( int i, internal::NamedPlaceHolder ) ;
+    inline Column operator()( internal::NamedPlaceHolder, int i ) ;
+    inline Column operator()( internal::NamedPlaceHolder, int i ) const  ;
+    inline Sub operator()( const Range& row_range, const Range& col_range) ;        
+    inline Sub operator()( internal::NamedPlaceHolder, const Range& col_range) ;
+    inline Sub operator()( const Range& row_range, internal::NamedPlaceHolder ) ;
+        
+        
 private:
-    int nrows ; 
+    
+    inline int offset( int i, int j) const { return i + nrows * j ; }
+    
+    virtual void update(){
+        RCPP_DEBUG_1( "%s::update", DEMANGLE(Matrix) ) ;
+        VECTOR::update_vector() ;
+    }
+        
+    template <typename U>
+    void fill_diag__dispatch( traits::false_type, const U& u) ;
+        
+    template <typename U>
+    void fill_diag__dispatch( traits::true_type, const U& u) ;
+
+    template <bool NA, typename MAT>
+    void import_matrix_expression( const MatrixBase<RTYPE,NA,MAT>& other, int nr, int nc ) ;
         
 } ;
 
