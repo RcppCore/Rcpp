860fda6d62d15a78ec3ffce09055c38bfc6f5501
Romain François <romain@r-enthusiasts.com>
2012-12-10 14:20:53 +0000

    more cleaning of Rcpp.h

--- inst/include/Rcpp/macros/macros.h
+++ inst/include/Rcpp/macros/macros.h
@@ -22,10 +22,34 @@
 #ifndef Rcpp_macros_macros_h
 #define Rcpp_macros_macros_h
 
-#include <Rcpp/macros/debug.h>
-#include <Rcpp/macros/unroll.h>
-
+#define RCPP_DECORATE(__FUN__) __FUN__##__rcpp__wrapper__
 #define RCPP_GET_NAMES(x)       Rf_getAttrib(x, R_NamesSymbol)
 #define RCPP_GET_CLASS(x)       Rf_getAttrib(x, R_ClassSymbol)
+
+#ifndef BEGIN_RCPP
+#define BEGIN_RCPP try{ 
+#endif 
+
+#ifndef VOID_END_RCPP
+#define VOID_END_RCPP } catch( std::exception& __ex__ ){ forward_exception_to_r( __ex__ ) ; } catch(...){ ::Rf_error( "c++ exception (unknown reason)" ) ; }
+#endif
+
+#ifndef END_RCPP
+#define END_RCPP VOID_END_RCPP return R_NilValue;
+#endif
+
+#ifndef END_RCPP_RETURN_ERROR
+#define END_RCPP_RETURN_ERROR } catch( std::exception& __ex__ ){ return exception_to_try_error( __ex__ ) ; } catch(...){ return string_to_try_error( "c++ exception (unknown reason)" ) ; } return R_NilValue;
+#endif
+
+#include <Rcpp/macros/debug.h>
+#include <Rcpp/macros/unroll.h>
+#include <Rcpp/macros/dispatch.h>
+#include <Rcpp/macros/xp.h>
+#include <Rcpp/macros/preprocessor_generated.h>
+#include <Rcpp/macros/traits.h>
+#include <Rcpp/macros/config.hpp>
+#include <Rcpp/macros/cat.hpp>
+
 
 #endif
