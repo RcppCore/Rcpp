d24ee1691c3f39e5a2d30a2a4af98fbeeb8df698
Romain François <romain@r-enthusiasts.com>
2012-12-15 22:06:56 +0000

    less going back to R on Evaluator::run

--- R/exceptions.R
+++ R/exceptions.R
@@ -15,43 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
 
-cpp_exception <- function( message = "C++ exception", class = NULL, cppstack = rcpp_get_current_stack_trace() ){
-    callstack <- sys.calls()
-    ncalls <- length(callstack)
-    call <- if( ncalls > 1L) callstack[[ ncalls - 1L ]] else match.call()
-    classes <- c( class, "C++Error", "error", "condition" )
-    condition <- structure(list( message = message, call = call, cppstack = cppstack ),
-                           class = classes )
-    setStackTrace( NULL )
-    stop( condition )
-}
-
-## Dummy versions
-rcpp_get_current_stack_trace <- function() "DUMMY STACK TRACE"
-rcpp_set_current_stack_trace <- function(s) NULL
-
-
-# used by Rcpp::Evaluator
-setCurrentError <- function( condition = NULL) .Call( rcpp_set_current_error, condition )
-getCurrentError <- function() .Call( rcpp_get_current_error )
-
-setErrorOccured <- function(error_occured = TRUE) .Call( rcpp_set_error_occured, error_occured )
-getErrorOccured <- function() .Call( rcpp_get_error_occured )
-
-setStackTrace <- function(trace = TRUE) .Call( rcpp_set_stack_trace, trace )
-getStackTrace <- function() .Call( rcpp_get_stack_trace)
-
-# all below are called from Evaluator::run
-# on the C++ side, don't change them unless you also change
-# Evaluator::run
-
-getCurrentErrorMessage <- function() conditionMessage( getCurrentError() )
 .rcpp_error_recorder <- function(e){  
     invisible( .Call( rcpp_error_recorder, e ) )
-}
-
-# for backwards compatibility, but not used anywhere
-rcpp_tryCatch <- function(expr){ 
-    tryCatch(expr, error = .rcpp_error_recorder )
 }
 
