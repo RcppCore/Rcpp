13edd234e5803261cd79275c9c2ea79260206acf
Romain François <romain@r-enthusiasts.com>
2013-09-12 10:02:11 +0000

    murray long long patch

--- inst/include/Rcpp/platform/compiler.h
+++ inst/include/Rcpp/platform/compiler.h
@@ -185,7 +185,7 @@
 #endif
 
 #ifdef __GNUC__
-#ifdef __GXX_EXPERIMENTAL_CXX0X__
+#if defined(__GXX_EXPERIMENTAL_CXX0X__) || defined(__LP64__)
 #ifdef __LONG_LONG_MAX__
     __extension__ typedef long long int rcpp_long_long_type;
     __extension__ typedef unsigned long long int rcpp_ulong_long_type;
