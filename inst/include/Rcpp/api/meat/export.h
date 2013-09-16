9c737db016edfec42b1e65f85ffd8b7017aedb84
Romain François <romain@r-enthusiasts.com>
2013-09-16 09:54:18 +0000

    faster as< std::vector<double> >

--- inst/include/Rcpp/api/meat/export.h
+++ inst/include/Rcpp/api/meat/export.h
@@ -35,6 +35,38 @@ namespace internal{
                 
     
 } // namespace internal
+
+    namespace traits{
+        
+        template < template <class, class> class ContainerTemplate, typename T > class ContainerExporter {
+        public:
+            typedef ContainerTemplate<T, std::allocator<T> > Container ;
+            const static int RTYPE = Rcpp::traits::r_sexptype_traits<T>::rtype ;
+            
+            ContainerExporter( SEXP x ) : object(x){}
+            ~ContainerExporter(){}
+            
+            Container get(){
+                if( TYPEOF(object) == RTYPE ){
+                   T* start = Rcpp::internal::r_vector_start<RTYPE>(object) ;
+                   return Container( start, start + Rf_length(object) ) ;
+                }
+                Container vec( ::Rf_length(object) );
+                ::Rcpp::internal::export_range( object, vec.begin() ) ;
+                return vec ;
+            }
+        
+        private:
+        		SEXP object ;
+        } ;
+        template < template<class,class> class Container > struct container_exporter< Container, int >{
+            typedef ContainerExporter< Container, int > type ;     
+        } ;
+        template < template<class,class> class Container > struct container_exporter< Container, double >{
+            typedef ContainerExporter< Container, double > type ;
+        } ;
+        
+    }
 } // namespace Rcpp
 
 #endif
