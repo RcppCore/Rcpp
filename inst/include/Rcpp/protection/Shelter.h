2dca5dc56f21499eb9772e92483ce93f3eb74a12
Romain François <romain@r-enthusiasts.com>
2013-10-14 17:58:22 +0000

    added Shield Armor and Shelter

--- inst/include/Rcpp/protection/Shelter.h
+++ inst/include/Rcpp/protection/Shelter.h
@@ -1,8 +1,4 @@
-// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
-//
-// meat.h: Rcpp R/C++ interface class library -- 
-//
-// Copyright (C) 2012 - 2013    Dirk Eddelbuettel and Romain Francois
+// Copyright (C) 2013    Romain Francois
 //
 // This file is part of Rcpp.
 //
@@ -19,18 +15,33 @@
 // You should have received a copy of the GNU General Public License
 // along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
 
-#ifndef Rcpp_api_meat_meat_h
-#define Rcpp_api_meat_meat_h
+#ifndef Rcpp_protection_Shelter_H
+#define Rcpp_protection_Shelter_H
 
-#include <Rcpp/api/meat/RObject.h>
-#include <Rcpp/api/meat/Environment.h>
-#include <Rcpp/api/meat/DottedPair.h>
-#include <Rcpp/api/meat/Vector.h>
-#include <Rcpp/api/meat/Matrix.h>
-#include <Rcpp/api/meat/Reference.h>
-#include <Rcpp/api/meat/is.h>
-#include <Rcpp/api/meat/export.h>
-#include <Rcpp/api/meat/protection.h>
-#include <Rcpp/api/meat/wrap.h>
+namespace Rcpp {
+    
+    template <class T>
+    class Shelter {
+    public:
+        Shelter() : nprotected(0){}
+        
+        inline SEXP operator()(SEXP x){
+            nprotected++;             
+            return PROTECT(x) ;
+        }
+        
+        ~Shelter(){
+            UNPROTECT(nprotected) ;
+            nprotected = 0 ;
+        }
+        
+    private:
+        int nprotected ;
+          
+        // not defined on purpose
+        Shelter(const Shelter&) ;
+        Shelter& operator=(const Shelter&) ; 
+    } ;
+}
 
 #endif
