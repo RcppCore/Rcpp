432e315705f883966b6a59de5b5593da940ac00a
Romain François <romain@r-enthusiasts.com>
2013-11-09 06:30:05 +0100

    abandon alternative protection stack code

--- inst/include/Rcpp/macros/debug.h
+++ inst/include/Rcpp/macros/debug.h
@@ -29,10 +29,6 @@
 
 const char* short_file_name(const char* file) ;
 
-namespace Rcpp{
-    void Rcpp_Stack_Debug() ;
-}
-
 #if RCPP_DEBUG_LEVEL > 0
     #define RCPP_DEBUG( MSG ) Rprintf( "%40s:%4d %s\n" , short_file_name(__FILE__), __LINE__, MSG ) ; 
     #define RCPP_DEBUG_1( fmt, MSG ) Rprintf( "%40s:%4d " fmt "\n" , short_file_name(__FILE__), __LINE__, MSG ) ; 
