bd724231aba659484f7dca96d88de2fcc422eb1b
Romain François <romain@r-enthusiasts.com>
2013-11-12 10:25:50 +0100

    using Shield

--- inst/include/Rcpp/vector/vector_from_string.h
+++ inst/include/Rcpp/vector/vector_from_string.h
@@ -25,22 +25,20 @@
 namespace internal{
     
     template <int RTYPE>
-    SEXP vector_from_string( const std::string& st ) {
+    inline SEXP vector_from_string( const std::string& st ) {
         return r_cast<RTYPE>( Rf_mkString( st.c_str() ) ) ;
     }
         
     template <int RTYPE>
     SEXP vector_from_string_expr( const std::string& code) {
         ParseStatus status;
-        SEXP expr = PROTECT( ::Rf_mkString( code.c_str() ) );
-        SEXP res  = PROTECT( ::R_ParseVector(expr, -1, &status, R_NilValue));
+        Shield<SEXP> expr = Rf_mkString( code.c_str() ) ;
+        Shield<SEXP> res  = R_ParseVector(expr, -1, &status, R_NilValue) ;
         switch( status ){
         case PARSE_OK:
-            UNPROTECT( 2) ;
             return(res) ;
             break;
         default:
-            UNPROTECT(2) ;
             throw parse_error() ;
         }
         return R_NilValue ; /* -Wall */
