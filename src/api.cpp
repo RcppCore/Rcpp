0b03244c76709227117072281ea0de41c5e9b38a
Romain François <romain@r-enthusiasts.com>
2013-11-12 12:09:07 +0100

    setup stack_trace to be registered

--- src/api.cpp
+++ src/api.cpp
@@ -23,14 +23,31 @@
 #include <Rcpp.h>
 
 #include "internal.h" 
-
-// for R_ObjectTable
-#include <R_ext/Callbacks.h>
 #include <R_ext/PrtUtil.h>
 
 #ifdef RCPP_HAS_DEMANGLING
 #include <cxxabi.h>
 #endif
+
+#if defined(__GNUC__)
+    #if defined(WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun)
+    #else
+        #include <execinfo.h>
+        
+        static std::string demangler_one( const char* input){
+            static std::string buffer ;
+            buffer = input ;
+            buffer.resize( buffer.find_last_of( '+' ) - 1 ) ;
+            buffer.erase(
+                buffer.begin(), 
+                buffer.begin() + buffer.find_last_of( ' ' ) + 1
+            ) ;
+            return demangle( buffer) ;
+        }
+
+    #endif
+#endif
+
 
 namespace Rcpp {
      
@@ -253,6 +270,7 @@ namespace Rcpp {
         return Rcpp::wrap( (const char*)buffer ) ;
     }
       
+    // [[Rcpp::register]]
     const char* short_file_name(const char* file){
         std::string f(file) ;
         size_t index = f.find("/include/") ;
@@ -341,67 +359,50 @@ namespace Rcpp {
 // }}}
 
 
-// {{{ utilities (from RcppCommon.cpp)
-
-#if defined(__GNUC__)
-#if defined(WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun)
-// Simpler version for Windows and *BSD 
+// [[Rcpp::register]]
 SEXP stack_trace( const char* file, int line ){
-    Rcpp::List trace = Rcpp::List::create( 
-    	Rcpp::Named( "file"  ) = file, 
-    	Rcpp::Named( "line"  ) = line, 
-    	Rcpp::Named( "stack" ) = "C++ stack not available on this system" ) ;
-    trace.attr("class") = "Rcpp_stack_trace" ;
-    return trace ;
+    #if defined(__GNUC__)
+        #if defined(WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun)
+            // Simpler version for Windows and *BSD 
+            Rcpp::List trace = Rcpp::List::create( 
+            	Rcpp::Named( "file"  ) = file, 
+            	Rcpp::Named( "line"  ) = line, 
+            	Rcpp::Named( "stack" ) = "C++ stack not available on this system" ) ;
+            trace.attr("class") = "Rcpp_stack_trace" ;
+            return trace ;
+        }
+        #else // ! (defined(WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun)
+        
+            /* inspired from http://tombarta.wordpress.com/2008/08/01/c-stack-traces-with-gcc/  */ 
+            const size_t max_depth = 100;
+            size_t stack_depth;
+            void *stack_addrs[max_depth];
+            char **stack_strings;
+            
+            stack_depth = backtrace(stack_addrs, max_depth);
+            stack_strings = backtrace_symbols(stack_addrs, stack_depth);
+            
+            std::string current_line ;
+            
+            Rcpp::CharacterVector res( stack_depth - 1) ;
+            std::transform( 
+            	stack_strings + 1, stack_strings + stack_depth, 
+            	res.begin(), 
+            	demangler_one 
+            	) ;
+            free(stack_strings); // malloc()ed by backtrace_symbols
+            
+            Rcpp::List trace = Rcpp::List::create( 
+            	Rcpp::Named( "file"  ) = file, 
+            	Rcpp::Named( "line"  ) = line, 
+            	Rcpp::Named( "stack" ) = res ) ;
+            trace.attr("class") = "Rcpp_stack_trace" ;
+            return trace ;
+        #endif 
+    #else /* !defined( __GNUC__ ) */
+	    return R_NilValue ;
+	#endif 
 }
-#else // ! (defined(WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__CYGWIN__) || defined(__sun)
-#include <execinfo.h>
-#include <cxxabi.h>
-
-static std::string demangler_one( const char* input){
-    static std::string buffer ;
-    buffer = input ;
-    buffer.resize( buffer.find_last_of( '+' ) - 1 ) ;
-    buffer.erase(
-        buffer.begin(), 
-        buffer.begin() + buffer.find_last_of( ' ' ) + 1
-    ) ;
-    return demangle( buffer) ;
-}
-
-/* inspired from http://tombarta.wordpress.com/2008/08/01/c-stack-traces-with-gcc/  */ 
-SEXP stack_trace( const char *file, int line) {
-    const size_t max_depth = 100;
-    size_t stack_depth;
-    void *stack_addrs[max_depth];
-    char **stack_strings;
-
-    stack_depth = backtrace(stack_addrs, max_depth);
-    stack_strings = backtrace_symbols(stack_addrs, stack_depth);
-
-    std::string current_line ;
-    
-    Rcpp::CharacterVector res( stack_depth - 1) ;
-    std::transform( 
-    	stack_strings + 1, stack_strings + stack_depth, 
-    	res.begin(), 
-    	demangler_one 
-    	) ;
-    free(stack_strings); // malloc()ed by backtrace_symbols
-    
-    Rcpp::List trace = Rcpp::List::create( 
-    	Rcpp::Named( "file"  ) = file, 
-    	Rcpp::Named( "line"  ) = line, 
-    	Rcpp::Named( "stack" ) = res ) ;
-    trace.attr("class") = "Rcpp_stack_trace" ;
-    return trace ;
-}
-#endif 
-#else /* !defined( __GNUC__ ) */
-SEXP stack_trace( const char *file, int line) {
-	return R_NilValue ;
-}
-#endif   
 // }}}
 
 // {{{ coercion
