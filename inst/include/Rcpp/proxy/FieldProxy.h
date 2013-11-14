706b6994b0ec172e1cb46a5040f6c0cd1a76ef51
Romain François <romain@r-enthusiasts.com>
2013-11-14 01:22:17 +0100

    FieldProxy was incomplete

--- inst/include/Rcpp/proxy/FieldProxy.h
+++ inst/include/Rcpp/proxy/FieldProxy.h
@@ -27,40 +27,61 @@ public:
     class FieldProxy {
     public:
         FieldProxy( CLASS& v, const std::string& name) : 
-            parent(v), field_name( Rf_mkChar(name.c_str())) {}
+            parent(v), field_name(name) {}
         
-        FieldProxy& operator=(const FieldProxy& rhs) ;
+        FieldProxy& operator=(const FieldProxy& rhs){
+            if( this != &rhs ) set( rhs.get() ) ;
+            return *this ;
+        }
               
         template <typename T> FieldProxy& operator=(const T& rhs) ;
             
         template <typename T> operator T() const ;
-        inline operator SEXP() const { return get() ; }
+        inline operator SEXP() const { 
+            return get() ; 
+        }
         
     private:
         CLASS& parent; 
-        SEXP field_name ;
+        const std::string& field_name ;
             
-        SEXP get() const ;
-        void set(SEXP x ) ;
+        SEXP get() const {
+            Shield<SEXP> call( Rf_lang3( R_DollarSymbol, parent, Rf_mkString(field_name.c_str()) ) ) ;
+            return Rf_eval( call, R_GlobalEnv ) ;    
+        }
+        void set(SEXP x ) {
+            SEXP dollarGetsSym = Rf_install( "$<-");
+            Shield<SEXP> call( Rf_lang4( dollarGetsSym, parent, Rf_mkString(field_name.c_str()) , x ) ) ;
+            parent.set__( Rf_eval( call, R_GlobalEnv ) );     
+        }
     } ;
     
     class const_FieldProxy {
     public:
-        const_FieldProxy( const CLASS& v, const std::string& name) ;
-        const_FieldProxy& operator=(const const_FieldProxy& rhs) ;
+        const_FieldProxy( const CLASS& v, const std::string& name) :
+            parent(v), field_name(name) {}
               
         template <typename T> operator T() const ;
-        inline operator SEXP() const { return get() ; }
+        inline operator SEXP() const { 
+            return get() ; 
+        }
         
     private:
         const CLASS& parent; 
-        SEXP field_name ;
+        const std::string& field_name ;
             
-        SEXP get() const ;
+        SEXP get() const {
+            Shield<SEXP> call( Rf_lang3( R_DollarSymbol, parent, Rf_mkString(field_name.c_str()) ) ) ;
+            return Rf_eval( call, R_GlobalEnv ) ;
+        }
     } ;
     
-    FieldProxy field(const std::string& name) ;
-    const_FieldProxy field(const std::string& name) const ;
+    FieldProxy field(const std::string& name){
+        return FieldProxy( static_cast<CLASS&>(*this), name ) ;    
+    }
+    const_FieldProxy field(const std::string& name) const {
+        return const_FieldProxy( static_cast<const CLASS&>(*this), name ) ;
+    }
     
 } ;
 
