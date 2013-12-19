2e68b2d855792be704786d3678bd78b3e238db84
Kevin Ushey <kevinushey@gmail.com>
2013-12-19 13:50:25 -0800

    add unit tests

---
+++ inst/unitTests/runit.table.R
@@ -0,0 +1,48 @@
+#!/usr/bin/r -t
+#
+# Copyright (C) 2013  Dirk Eddelbuettel and Romain Francois
+#
+# This file is part of Rcpp.
+#
+# Rcpp is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Rcpp is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
+.runThisTest <- Sys.getenv("RunAllRcppTests") == "yes"
+
+if (.runThisTest) {
+  
+  .setUp <- Rcpp:::unit_test_setup("table.cpp")
+  
+  table <- function(x) base::table(x, useNA="ifany")
+    
+  test.table.numeric <- function() {
+    x <- c(1, 2, NA, NaN, -Inf, Inf)
+    checkEquals( RcppTable(x), c(table(x)))
+  }
+  
+  test.table.integer <- function() {
+    x <- c(-1L, 1L, NA_integer_, NA_integer_, 100L, 1L)
+    checkEquals( RcppTable(x), c(table(x)))
+  }
+  
+  test.table.logical <- function() {
+    x <- c(TRUE, TRUE, FALSE, NA)
+    checkEquals( RcppTable(x), c(table(x)))
+  }
+  
+  test.table.character <- function() {
+    x <- c("a", "a", "b", "a", NA, NA)
+    checkEquals( RcppTable(x), c(table(x)))
+  }
+  
+}
