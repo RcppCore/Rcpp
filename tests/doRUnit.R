a944046d089bd5cbdcc53bc4783f88f86195ed2a
Dirk Eddelbuettel <edd@debian.org>
2014-02-01 09:05:57 -0600

    the tests/doRUnit.R wrapper was rewritten

--- tests/doRUnit.R
+++ tests/doRUnit.R
@@ -1,5 +1,5 @@
-# Copyright (C) 2009 - 2012 Dirk Eddelbuettel and Romain Francois
-# Earlier copyrights Gregor Gorjanc and Martin Maechler as detailed below
+# Copyright (C) 2009 - 2014 Dirk Eddelbuettel and Romain Francois
+# Earlier copyrights Gregor Gorjanc, Martin Maechler and Murray Stokely as detailed below
 #
 # This file is part of Rcpp.
 #
@@ -16,50 +16,49 @@
 # You should have received a copy of the GNU General Public License
 # along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
 
-## Unit test wrapper invoked from tests/
-
-##
-## Based on a file written by Martin Maechler for the Rmetrics
-## packages which itself was based on earlier work by Gregor Gorjanc
-## and documented on the R Wiki.
-
 ## doRUnit.R --- Run RUnit tests
-## ------------------------------------------------------------------------
 ##
-## borrowed from package fUtilities in RMetrics
-## http://r-forge.r-project.org/plugins/scmsvn/viewcvs.php/pkg/fUtilities/tests/doRUnit.R?rev=1958&root=rmetrics&view=markup
+## with credits to package fUtilities in RMetrics
+## which credits Gregor Gojanc's example in CRAN package  'gdata'
+## as per the R Wiki http://wiki.r-project.org/rwiki/doku.php?id=developers:runit
+## and changed further by Martin Maechler
+## and more changes by Murray Stokely in HistogramTools
+## and then used adapted in RProtoBuf
+## and now used here with two additional env var setters/getters
 ##
-## Originally follows Gregor Gorjanc's example in CRAN package  'gdata'
-## and the corresponding section in the R Wiki:
-## http://wiki.r-project.org/rwiki/doku.php?id=developers:runit
-##
-## MM: Vastly changed:  This should also be "runnable" for *installed*
-##     package which has no ./tests/
-##     --> put the bulk of the code e.g. in  ../inst/unitTests/runTests.R :
+## Dirk Eddelbuettel, Feb 2014
 
-if (require("RUnit", quietly = TRUE)) {
+stopifnot(require(RUnit, quietly=TRUE))
+stopifnot(require(Rcpp, quietly=TRUE))
 
-    pkg <- "Rcpp"                           # code below for Rcpp
-    require(pkg, character.only=TRUE)
-    pathRcppTests <<- system.file("unitTests", package = pkg)
-    stopifnot(file.exists(pathRcppTests), file.info(path.expand(pathRcppTests))$isdir)
+## Set a seed to make the test deterministic
+set.seed(42)
 
-    ## without this, we get unit test failures
-    Sys.setenv( R_TESTS = "" )
+## Define tests
+testSuite <- defineTestSuite(name="Rcpp Unit Tests",
+                             dirs=system.file("unitTests", package = "Rcpp"),
+                             testFuncRegexp = "^[Tt]est.+")
 
-    ## force tests to be executed if in dev release which we define as
-    ## having a sub-release, eg 0.9.15.5 is one whereas 0.9.16 is not
-    if (length(strsplit(packageDescription(pkg)$Version, "\\.")[[1]]) > 3) {	# dev release, and
-        if (Sys.getenv("RunAllRcppTests") != "no") { 				# if env.var not yet set
-            message("Setting \"RunAllRcppTests\"=\"yes\" for development release\n")
-            Sys.setenv("RunAllRcppTests"="yes")
-        }
+## without this, we get (or used to get) unit test failures
+Sys.setenv("R_TESTS"="")
+
+## force tests to be executed if in dev release which we define as
+## having a sub-release, eg 0.9.15.5 is one whereas 0.9.16 is not
+if (length(strsplit(packageDescription("Rcpp")$Version, "\\.")[[1]]) > 3) {	# dev release, and
+    if (Sys.getenv("RunAllRcppTests") != "no") { 				# if env.var not yet set
+        message("Setting \"RunAllRcppTests\"=\"yes\" for development release\n")
+        Sys.setenv("RunAllRcppTests"="yes")
     }
-
-    Rcpp.unit.test.output.dir <- getwd()
-
-    source(file.path(pathRcppTests, "runTests.R"), echo = TRUE)
-
-} else {
-    print("package RUnit not available, cannot run unit tests")
 }
+
+## Run tests
+tests <- runTestSuite(testSuite)
+
+## Print results
+printTextProtocol(tests)
+
+## Return success or failure to R CMD CHECK
+if (getErrors(tests)$nFail > 0) {
+    stop("TEST FAILED!")
+}
+
