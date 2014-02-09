7efd2cd8670ce1f37375fbcefbaefd20d1be868b
Kevin Ushey <kevinushey@gmail.com>
2014-02-08 20:11:40 -0800

    Sort behaves like R's 'sort(..., na.last=TRUE)'

--- inst/include/Rcpp/traits/comparator_type.h
+++ inst/include/Rcpp/traits/comparator_type.h
@@ -3,7 +3,7 @@
 //
 // comparator_type.h: Rcpp R/C++ interface class library -- comparator
 //
-// Copyright (C) 2012 Dirk Eddelbuettel and Romain Francois
+// Copyright (C) 2012-2014 Dirk Eddelbuettel, Romain Francois and Kevin Ushey
 //
 // This file is part of Rcpp.
 //
@@ -26,20 +26,72 @@
 namespace Rcpp{
 namespace traits{
    
-class StringCompare {
-public:
-    inline bool operator()( SEXP x, SEXP y) const {
-        return strcmp( char_nocheck(x), char_nocheck(y) ) < 0 ; 
+inline bool Rcpp_IsNA(double x) {
+  return memcmp(
+    &x,
+    &NA_REAL,
+    sizeof(double)
+  ) == 0;
+}
+
+inline bool Rcpp_IsNaN(double x) {
+  return memcmp(
+    &x,
+    &R_NaN,
+    sizeof(double)
+  ) == 0;
+}
+
+inline int StrCmp(SEXP x, SEXP y) {
+    if (x == NA_STRING) return (y == NA_STRING ? 0 : 1);
+    if (y == NA_STRING) return -1;
+    if (x == y) return 0;  // same string in cache
+    return strcmp(CHAR(x), CHAR(y));
+}
+
+template <typename T>
+struct comparator_type {
+  inline bool operator()(T left, T right) const {
+    return left < right;
+  }
+};
+
+template <>
+struct comparator_type<int> {
+  inline bool operator()(int left, int right) const {
+    if (left == NA_INTEGER) return false;
+    if (right == NA_INTEGER) return true;
+    return left < right;
+  }
+};
+
+template <>
+struct comparator_type<double> {  
+  inline bool operator()(double left, double right) const {
+        
+    bool leftNaN = (left != left);
+    bool rightNaN = (right != right);
+    
+    // this branch inspired by data.table: see
+    // https://github.com/arunsrinivasan/datatable/commit/1a3e476d3f746e18261662f484d2afa84ac7a146#commitcomment-4885242
+    if (Rcpp_IsNaN(right) and Rcpp_IsNA(left)) return true;
+    
+    if (leftNaN != rightNaN) {
+      return leftNaN < rightNaN;
+    } else {
+      return left < right;
     }
-} ;
-	
-template <int RTYPE> struct comparator_type {
-	typedef std::less< typename storage_type<RTYPE>::type > type ;	
-} ;
-template <> struct comparator_type<STRSXP>{
-	typedef StringCompare type ;
-} ;
-   
+
+  }
+  
+};
+
+template <>
+struct comparator_type<SEXP> {
+  inline bool operator()(SEXP left, SEXP right) const {
+    return StrCmp(left, right) < 0;
+  }
+};   
 
 }
 }     
