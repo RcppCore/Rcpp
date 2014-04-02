00b1d6be2383ed4910874a5351e890c68952790c
Romain François <romain@r-enthusiasts.com>
2014-04-02 08:25:55 +0200

    actually need to use abs because R is not consistent internally

--- inst/include/Rcpp/DataFrame.h
+++ inst/include/Rcpp/DataFrame.h
@@ -79,7 +79,7 @@ namespace Rcpp{
             if (Rf_isNull(rn))
                 return 0;
             if (TYPEOF(rn) == INTSXP && LENGTH(rn) == 2 && INTEGER(rn)[0] == NA_INTEGER)
-                return -INTEGER(rn)[1];
+                return abs(INTEGER(rn)[1]);
             return LENGTH(rn);
         }
 
