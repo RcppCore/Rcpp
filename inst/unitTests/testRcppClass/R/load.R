73f7de3cc6045c7782c9312c188cf60f486c4868
Dirk Eddelbuettel <edd@debian.org>
2014-09-14 12:18:55 -0500

    update testRcppClass as well to cleanly build without NOTES or WARNINGS

--- inst/unitTests/testRcppClass/R/load.R
+++ inst/unitTests/testRcppClass/R/load.R
@@ -1,7 +1,7 @@
 setRcppClass("World", module = "yada", fields = list(more = "character"),
-         methods = list(test = function(what) message("Testing: ", what, "; ", more)),
-         saveAs = "genWorld"
-         )
+             methods = list(test = function(what) message("Testing: ", what, "; ", more)),
+             saveAs = "genWorld"
+             )
 
 setRcppClass("stdNumeric", "vec", "stdVector")
 
@@ -46,6 +46,6 @@ evalqOnLoad({
 
 ## For R 2.15.1 and later this also works. Note that calling loadModule() triggers
 ## a load action, so this does not have to be placed in .onLoad() or evalqOnLoad().
-#loadModule("NumEx", TRUE)
-#loadModule("yada", TRUE)
-#loadModule("stdVector", TRUE)
+loadModule("NumEx", TRUE)
+loadModule("yada", TRUE)
+loadModule("stdVector", TRUE)
