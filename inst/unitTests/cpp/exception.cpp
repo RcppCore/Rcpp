251993f1e897f5ff64fe0c570d0a869eefce4116
Wush Wu <wush978@gmail.com>
2015-01-11 16:13:18 +0800

    rename and add copyright header of unit test of "exception to stop"

--- inst/unitTests/cpp/exception.cpp
+++ inst/unitTests/cpp/exception.cpp
@@ -1,3 +1,24 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 8 -*-
+//
+// exception.cpp: Rcpp R/C++ interface class library -- exception to stop unit tests
+//
+// Copyright (C) 2015 Wush Wu
+//
+// This file is part of Rcpp.
+//
+// Rcpp is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Rcpp is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
 #include <Rcpp.h>
 using namespace Rcpp;
 
@@ -5,10 +26,10 @@ class DeleteNotifier : public std::exception {
   int _id;
 public:
   DeleteNotifier(int id) : std::exception(), _id(id) { }
-  virtual ~DeleteNotifier() _NOEXCEPT {
+  virtual ~DeleteNotifier() throw () {
     Rprintf("deleting DeleteNotifier(%d)\n", _id);
   }
-  virtual const char* what() const _NOEXCEPT {
+  virtual const char* what() const throw () {
     return "test notifier";
   }
 };
@@ -23,7 +44,7 @@ void testMemory2() {
   try {
     DeleteNotifier dn(2);
     throw std::logic_error("test dn2");
-  } 
+  }
   catch(std::exception& e) {
   }
 }
