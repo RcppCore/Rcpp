d60200f75321b825bc70c7b578cf882bc829261c
Dirk Eddelbuettel <edd@debian.org>
2015-01-19 07:49:58 -0600

    update bibliography

---
+++ vignettes/getCurrentVersionsOfCitedPackages.R
@@ -0,0 +1,15 @@
+#/usr/bin/r
+
+setwd("~/git/rcpp/vignettes")
+cmd <- "grep package= Rcpp.bib | cut -c27- | sed -e 's/\"$//' | sort | uniq"
+con <- pipe(cmd)
+pkgs <- readLines(pipe(cmd))
+close(con)
+
+repo <- "http://cran.rstudio.com"
+AP <- available.packages(contrib.url(repo),filter=list())	# available package at CRAN
+
+res <- as.data.frame(do.call(rbind, lapply(pkgs, function(p) {
+    ind <- which(AP[,"Package"] == p)
+    AP[ind, c("Package", "Version")]
+})))
