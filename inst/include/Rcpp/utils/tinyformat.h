31222713168248666e55fd798702e63c4ea7b1c8
Kevin Ushey <kevinushey@gmail.com>
2015-01-25 18:39:55 -0800

    stripping out more 'assert' bits

--- inst/include/Rcpp/utils/tinyformat.h
+++ inst/include/Rcpp/utils/tinyformat.h
@@ -859,7 +859,7 @@ class FormatListN : public FormatList
         template<TINYFORMAT_ARGTYPES(n)>                       \
         FormatListN(TINYFORMAT_VARARGS(n))                     \
             : FormatList(&m_formatterStore[0], n)              \
-        { assert(n == N); init(0, TINYFORMAT_PASSARGS(n)); }   \
+        { init(0, TINYFORMAT_PASSARGS(n)); }                   \
                                                                \
         template<TINYFORMAT_ARGTYPES(n)>                       \
         void init(int i, TINYFORMAT_VARARGS(n))                \
