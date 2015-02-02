d94a295b351f9a369a6c7b4a18fea92bff37451c
JJ Allaire <jj@rstudio.org>
2015-02-02 13:09:53 -0500

    handle warnings in Rcpp_eval

--- R/exceptions.R
+++ R/exceptions.R
@@ -19,3 +19,18 @@
     invisible( .Call( rcpp_error_recorder, e ) )
 }
 
+.warningsEnv <- new.env()
+.warningsEnv$warnings <- character()
+
+.rcpp_warning_recorder <- function(w){
+    .warningsEnv$warnings <- append(.warningsEnv$warnings, w$message)
+    invokeRestart("muffleWarning")
+}
+
+.rcpp_collect_warnings <- function() {
+    warnings <- .warningsEnv$warnings
+    .warningsEnv$warnings <- character()
+    warnings
+}
+
+
