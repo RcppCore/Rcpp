5b599a02c95e27fbc6c669abd6056005950852fc
thirdwing <qkou@umail.iu.edu>
2015-05-31 02:12:26 -0400

    modified:   inst/include/Rcpp/vector/Vector.h

--- inst/include/Rcpp/vector/Vector.h
+++ inst/include/Rcpp/vector/Vector.h
@@ -91,7 +91,6 @@ public:
         fill( u ) ;
     }
 
-    template<>
     Vector( const int& size, const stored_type& u) {
         RCPP_DEBUG_2( "Vector<%d>( const int& size = %d, const stored_type& u )", RTYPE, size)
         Storage::set__( Rf_allocVector( RTYPE, size) ) ;
