bf34e7b10492b4e078b881ae196a911435133e99
Florian Plaza Oñate <florian.plaza@jouy.inra.fr>
2015-08-19 09:38:45 +0200

    Uncomment at accessors

--- inst/include/Rcpp/vector/Vector.h
+++ inst/include/Rcpp/vector/Vector.h
@@ -318,7 +318,7 @@ public:
 
     inline iterator begin() { return cache.get() ; }
     inline iterator end() { return cache.get() + size() ; }
-	inline const_iterator begin() const{ return cache.get_const() ; }
+    inline const_iterator begin() const{ return cache.get_const() ; }
     inline const_iterator end() const{ return cache.get_const() + size() ; }
 
     inline Proxy operator[]( R_xlen_t i ){ return cache.ref(i) ; }
@@ -330,14 +330,14 @@ public:
     inline const_Proxy operator()( const size_t& i) const {
         return cache.ref( offset(i) ) ;
     }
-    /*
+
     inline Proxy at( const size_t& i) {
        return cache.ref( offset(i) ) ;
     }
     inline const_Proxy at( const size_t& i) const {
        return cache.ref( offset(i) ) ;
     }
-    */
+
     inline Proxy operator()( const size_t& i, const size_t& j) {
         return cache.ref( offset(i,j) ) ;
     }
