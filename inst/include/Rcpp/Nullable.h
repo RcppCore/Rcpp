ac7defc1da4b9da1cefa52854578c07ad54c18e4
Dirk Eddelbuettel <edd@debian.org>
2015-09-06 20:20:40 -0500

    new class Nullable

---
+++ inst/include/Rcpp/Nullable.h
@@ -0,0 +1,105 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+//
+// Nullable.h: Rcpp R/C++ interface class library -- SEXP container which can be NULL
+//
+// Copyright (C) 2015         Dirk Eddelbuettel
+//
+// This file is part of Rcpp.
+//
+// Rcpp is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Rcpp is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef Rcpp_Nullable_h
+#define Rcpp_Nullable_h
+
+namespace Rcpp {
+    class Nullable {
+    public:
+
+        /**
+         * Empty no-argument constructor of a Nullable object
+         *
+         * set validator to FALSE
+         */
+        inline Nullable() : m_sexp(NULL), m_set(false) {}
+
+        /**
+         * Standard constructor of a Nullable object
+         *
+         * @param SEXP is stored
+         */
+        inline Nullable(SEXP t) : m_sexp(t), m_set(true) {}
+
+        /**
+         * Copy constructor for Nullable object
+         *
+         * @param SEXP is used to update internal copy
+         */
+        inline Nullable &operator=(SEXP sexp) {
+            m_sexp = sexp;
+            m_set = true;
+            return *this;
+        }
+
+        /**
+         * operator SEXP() to return nullable object
+         *
+         * @throw 'not initialized' if object has not been set
+         */
+        inline operator SEXP() {
+            checkIfSet();
+            return m_sexp;
+        }
+
+        /**
+         * get() accessor for object
+         *
+         * @throw 'not initialized' if object has not been set
+         */
+        inline SEXP get() {
+            checkIfSet();
+            return m_sexp;
+        }
+
+        /**
+         * boolean test for NULL
+         *
+         * @throw 'not initialized' if object has not been set
+         */
+        inline bool isNull() {
+            checkIfSet();
+            return Rf_isNull(m_sexp);
+        }
+
+        /**
+         * boolean test for not NULL
+         *
+         * @throw 'not initialized' if object has not been set
+         */
+        inline bool isNotNull() {
+            return ! isNull();
+        }
+
+    private:
+        SEXP m_sexp;
+        bool m_set;
+
+        inline void checkIfSet(void) {
+            if (!m_set) {
+                throw ::Rcpp::exception("Not initialized");
+            }
+        }
+    };
+}
+
+#endif
