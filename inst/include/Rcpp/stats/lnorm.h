42c14f8199a71e1dbee8035d4c9fd2bfc334cace
Mattias Ellert <mattias.ellert@fysast.uu.se>
2016-02-20 10:05:40 +0100

    Resolve misleading-indentation warnings from gcc 6

--- inst/include/Rcpp/stats/lnorm.h
+++ inst/include/Rcpp/stats/lnorm.h
@@ -33,15 +33,15 @@ inline double dlnorm_0(double x, int log_p){
 
 #ifdef IEEE_754
     if (ISNAN(x))
-	return x + 1.0 ;
+        return x + 1.0;
 #endif
 
-    if(x <= 0) return R_D__0;
+    if (x <= 0) return R_D__0;
 
-    y = ::log(x) ;
+    y = ::log(x);
     return (log_p ?
-	    -(M_LN_SQRT_2PI   + 0.5 * y * y + ::log(x)) :
-	    M_1_SQRT_2PI * ::exp(-0.5 * y * y)  / x );
+            -(M_LN_SQRT_2PI + 0.5 * y * y + ::log(x)) :
+            M_1_SQRT_2PI * ::exp(-0.5 * y * y) / x);
     /* M_1_SQRT_2PI = 1 / sqrt(2 * pi) */
 
 }
@@ -50,16 +50,16 @@ inline double dlnorm_1(double x, double meanlog, int log_p){
     double y;
 
 #ifdef IEEE_754
-    if (ISNAN(x) || ISNAN(meanlog) )
-	return x + meanlog + 1.0 ;
+    if (ISNAN(x) || ISNAN(meanlog))
+        return x + meanlog + 1.0;
 #endif
 
-    if(x <= 0) return R_D__0;
+    if (x <= 0) return R_D__0;
 
     y = (::log(x) - meanlog);
     return (log_p ?
-	    -(M_LN_SQRT_2PI   + 0.5 * y * y + ::log(x)) :
-	    M_1_SQRT_2PI * ::exp(-0.5 * y * y)  / x );
+            -(M_LN_SQRT_2PI + 0.5 * y * y + ::log(x)) :
+            M_1_SQRT_2PI * ::exp(-0.5 * y * y) / x);
     /* M_1_SQRT_2PI = 1 / sqrt(2 * pi) */
 
 }
@@ -67,30 +67,30 @@ inline double dlnorm_1(double x, double meanlog, int log_p){
 
 inline double plnorm_0(double x, int lower_tail, int log_p){
 #ifdef IEEE_754
-    if (ISNAN(x)  )
-	return x + 1.0 ;
+    if (ISNAN(x))
+        return x + 1.0;
 #endif
 
     if (x > 0)
-    	return Rcpp::stats::pnorm_0(::log(x), lower_tail, log_p);
+        return Rcpp::stats::pnorm_0(::log(x), lower_tail, log_p);
     return R_DT_0;
 }
 
 inline double plnorm_1(double x, double meanlog, int lower_tail, int log_p) {
 #ifdef IEEE_754
-    if (ISNAN(x) || ISNAN(meanlog) )
-	return x + meanlog + 1.0 ;
+    if (ISNAN(x) || ISNAN(meanlog))
+        return x + meanlog + 1.0;
 #endif
 
-	if (x > 0)
-    	return Rcpp::stats::pnorm_1(::log(x), meanlog, lower_tail, log_p);
+    if (x > 0)
+        return Rcpp::stats::pnorm_1(::log(x), meanlog, lower_tail, log_p);
     return R_DT_0;
 }
 
 inline double qlnorm_0(double p, int lower_tail, int log_p){
 #ifdef IEEE_754
-    if (ISNAN(p) )
-	return p + 1.0 ;
+    if (ISNAN(p))
+        return p + 1.0;
 #endif
     R_Q_P01_boundaries(p, 0, ML_POSINF);
 
@@ -100,7 +100,7 @@ inline double qlnorm_0(double p, int lower_tail, int log_p){
 inline double qlnorm_1(double p, double meanlog, int lower_tail, int log_p){
 #ifdef IEEE_754
     if (ISNAN(p) || ISNAN(meanlog))
-	return p + meanlog + 1.0 ;
+        return p + meanlog + 1.0;
 #endif
     R_Q_P01_boundaries(p, 0, ML_POSINF);
 
--- inst/include/Rcpp/stats/logis.h
+++ inst/include/Rcpp/stats/logis.h
@@ -29,11 +29,11 @@ inline double dlogis_0(double x /*, double location [=0.0], double scale [=1.0] 
     double e, f;
 #ifdef IEEE_754
     if (ISNAN(x))
-	return x + 1.0 ;
+        return x + 1.0;
 #endif
 
-	e = ::exp(-::fabs(x));
-    f = 1.0 + e ;
+    e = ::exp(-::fabs(x));
+    f = 1.0 + e;
     return give_log ? -(x + ::log(f * f)) : e / (f * f);
 }
 
@@ -41,10 +41,10 @@ inline double dlogis_1(double x, double location /*, double scale [=1.0] */, int
     double e, f;
 #ifdef IEEE_754
     if (ISNAN(x) || ISNAN(location))
-	return x + location + 1.0;
+        return x + location + 1.0;
 #endif
 
-    x = ::fabs((x - location) );
+    x = ::fabs((x - location));
     e = ::exp(-x);
     f = 1.0 + e;
     return give_log ? -(x + ::log(f * f)) : e / (f * f);
@@ -52,13 +52,13 @@ inline double dlogis_1(double x, double location /*, double scale [=1.0] */, int
 
 
 inline double plogis_0(double x /*, double location [=0.0] , double scale [=1.0] */,
-	      int lower_tail, int log_p) {
+                       int lower_tail, int log_p) {
 #ifdef IEEE_754
-    if (ISNAN(x) )
-	return x + 1.0 ;
+    if (ISNAN(x))
+        return x + 1.0;
 #endif
 
-    if (ISNAN(x))	return R_NaN ;
+    if (ISNAN(x)) return R_NaN;
     R_P_bounds_Inf_01(x);
 
     x = ::exp(lower_tail ? -x : x);
@@ -67,14 +67,14 @@ inline double plogis_0(double x /*, double location [=0.0] , double scale [=1.0]
 
 
 inline double plogis_1(double x, double location /*, double scale [=1.0] */,
-	      int lower_tail, int log_p) {
+                       int lower_tail, int log_p) {
 #ifdef IEEE_754
-    if (ISNAN(x) || ISNAN(location) )
-	return x + location + 1.0 ;
+    if (ISNAN(x) || ISNAN(location))
+        return x + location + 1.0;
 #endif
 
-    x = (x - location) ;
-    if (ISNAN(x))	return R_NaN ;
+    x = (x - location);
+    if (ISNAN(x)) return R_NaN;
     R_P_bounds_Inf_01(x);
 
     x = ::exp(lower_tail ? -x : x);
@@ -86,19 +86,19 @@ inline double qlogis_0(double p /*, double location [=0.0], double scale [=1.0] 
 {
 #ifdef IEEE_754
     if (ISNAN(p))
-	return p + 1.0 ;
+        return p + 1.0;
 #endif
     R_Q_P01_boundaries(p, ML_NEGINF, ML_POSINF);
 
-    /* p := logit(p) = log( p / (1-p) )	 : */
-    if(log_p) {
-	if(lower_tail)
-	    p = p - ::log1p(- ::exp(p));
-	else
-	    p = ::log1p(- ::exp(p)) - p;
+    /* p := logit(p) = log(p / (1. - p))         : */
+    if (log_p) {
+        if (lower_tail)
+            p = p - ::log1p(- ::exp(p));
+        else
+            p = ::log1p(- ::exp(p)) - p;
     }
     else
-    	p = ::log(lower_tail ? (p / (1. - p)) : ((1. - p) / p));
+        p = ::log(lower_tail ? (p / (1. - p)) : ((1. - p) / p));
 
     return p;
 }
@@ -108,19 +108,19 @@ inline double qlogis_1(double p, double location /*, double scale [=1.0] */, int
 {
 #ifdef IEEE_754
     if (ISNAN(p) || ISNAN(location))
-	return p + location + 1.0 ;
+        return p + location + 1.0;
 #endif
     R_Q_P01_boundaries(p, ML_NEGINF, ML_POSINF);
 
-    /* p := logit(p) = log( p / (1-p) )	 : */
-    if(log_p) {
-	if(lower_tail)
-	    p = p - ::log1p(- ::exp(p));
-	else
-	    p = ::log1p(- ::exp(p)) - p;
+    /* p := logit(p) = log(p / (1. - p))         : */
+    if (log_p) {
+        if (lower_tail)
+            p = p - ::log1p(- ::exp(p));
+        else
+            p = ::log1p(- ::exp(p)) - p;
     }
     else
-    	p = ::log(lower_tail ? (p / (1. - p)) : ((1. - p) / p));
+        p = ::log(lower_tail ? (p / (1. - p)) : ((1. - p) / p));
 
     return location + p;
 }
