1850d8c0e120b5ff6844b16f02220256cef48cf9
Dirk Eddelbuettel <edd@debian.org>
2017-01-13 15:59:24 -0500

    more #nocov

--- inst/include/Rcpp.h
+++ inst/include/Rcpp.h
@@ -57,6 +57,7 @@
 #include <Rcpp/DataFrame.h>
 
 //  #define RCPP_NEW_DATE_DATETIME_VECTORS 1
+#define RCPP_NEW_DATE_DATETIME_VECTORS 1
 #include <Rcpp/date_datetime/date_datetime.h>
 
 #include <Rcpp/Na_Proxy.h>
