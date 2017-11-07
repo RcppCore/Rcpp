f91cd2e83c56d2c02fa2934bbfdc5fe2daada167
Dirk Eddelbuettel <edd@debian.org>
2017-11-07 06:43:35 -0600

    one char typo, catch up with dev version macro

--- inst/include/Rcpp/config.h
+++ inst/include/Rcpp/config.h
@@ -30,7 +30,7 @@
 #define RCPP_VERSION Rcpp_Version(0,12,13)
 
 // the current source snapshot
-#define RCPP_DEV_VERSION RcppDevVersion(0,12,13,0)
+#define RCPP_DEV_VERSION RcppDevVersion(0,12,13,2)
 
 #endif
 
