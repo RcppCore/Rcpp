ad7e397e81bb81e4019204494bb9de478ccece8b
Lionel Henry <lionel.hry@gmail.com>
2017-12-14 13:26:44 +0100

    Add unit tests for stack unwinding

--- inst/unitTests/cpp/misc.cpp
+++ inst/unitTests/cpp/misc.cpp
@@ -224,3 +224,37 @@ String testNullableString(Rcpp::Nullable<Rcpp::String> param = R_NilValue) {
   else
     return String("");
 }
+
+// Class that indicates to R caller whether C++ stack was unwound
+struct unwindIndicator {
+    unwindIndicator(LogicalVector indicator_) {
+        // Reset the indicator to FALSE
+        indicator = indicator_;
+        *LOGICAL(indicator) = 0;
+    }
+
+    // Set indicator to TRUE when stack unwinds
+    ~unwindIndicator() {
+        *LOGICAL(indicator) = 1;
+    }
+
+    LogicalVector indicator;
+};
+
+// [[Rcpp::export]]
+SEXP testEvalUnwindImpl(RObject expr, Environment env, LogicalVector indicator) {
+    unwindIndicator my_data(indicator);
+    return Rcpp::Rcpp_fast_eval(expr, env);
+}
+
+// [[Rcpp::export]]
+SEXP testLongjumpException() {
+    throw Rcpp::internal::LongjumpException(R_NilValue);
+    return R_NilValue;
+}
+
+// [[Rcpp::export]]
+SEXP testSendInterrupt() {
+  Rf_onintr();
+  return R_NilValue;
+}
