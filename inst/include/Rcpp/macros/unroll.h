749175bc53a29a514fb3249246402186649f391e
Kirill Müller <krlmlr+r@mailbox.org>
2017-12-21 14:11:45 +0100

    fallthrough comments for gcc 7

--- inst/include/Rcpp/macros/unroll.h
+++ inst/include/Rcpp/macros/unroll.h
@@ -44,46 +44,46 @@ switch (n - i){                                \
 }
 
 
-#define RCPP_LOOP_UNROLL(TARGET,SOURCE)           \
-R_xlen_t __trip_count = n >> 2 ;                  \
-R_xlen_t i = 0 ;                                  \
-for ( ; __trip_count > 0 ; --__trip_count) {      \
-	TARGET[i] = SOURCE[i] ; i++ ;                 \
-	TARGET[i] = SOURCE[i] ; i++ ;                 \
-	TARGET[i] = SOURCE[i] ; i++ ;                 \
-	TARGET[i] = SOURCE[i] ; i++ ;                 \
-}                                                 \
-switch (n - i){                                   \
-  case 3:                                         \
-      TARGET[i] = SOURCE[i] ; i++ ;               \
-  case 2:                                         \
-      TARGET[i] = SOURCE[i] ; i++ ;               \
-  case 1:                                         \
-      TARGET[i] = SOURCE[i] ; i++ ;               \
-  case 0:                                         \
-  default:                                        \
-      {}                                          \
+#define RCPP_LOOP_UNROLL(TARGET,SOURCE)               \
+R_xlen_t __trip_count = n >> 2 ;                      \
+R_xlen_t i = 0 ;                                      \
+for ( ; __trip_count > 0 ; --__trip_count) {          \
+        TARGET[i] = SOURCE[i] ; i++ ;                 \
+        TARGET[i] = SOURCE[i] ; i++ ;                 \
+        TARGET[i] = SOURCE[i] ; i++ ;                 \
+        TARGET[i] = SOURCE[i] ; i++ ;                 \
+}                                                     \
+switch (n - i){                                       \
+  case 3:                                             \
+      TARGET[i] = SOURCE[i] ; i++ ; /* fallthrough */ \
+  case 2:                                             \
+      TARGET[i] = SOURCE[i] ; i++ ; /* fallthrough */ \
+  case 1:                                             \
+      TARGET[i] = SOURCE[i] ; i++ ; /* fallthrough */ \
+  case 0:                                             \
+  default:                                            \
+      {}                                              \
 }
 
-#define RCPP_LOOP_UNROLL_LHSFUN(TARGET,FUN,SOURCE) \
-R_xlen_t __trip_count = n >> 2 ;                  \
-R_xlen_t i = 0 ;                                  \
-for ( ; __trip_count > 0 ; --__trip_count) {      \
-	TARGET[FUN(i)] = SOURCE[i] ; i++ ;            \
-	TARGET[FUN(i)] = SOURCE[i] ; i++ ;            \
-	TARGET[FUN(i)] = SOURCE[i] ; i++ ;            \
-	TARGET[FUN(i)] = SOURCE[i] ; i++ ;            \
-}                                                 \
-switch (n - i){                                   \
-  case 3:                                         \
-      TARGET[FUN(i)] = SOURCE[i] ; i++ ;          \
-  case 2:                                         \
-      TARGET[FUN(i)] = SOURCE[i] ; i++ ;          \
-  case 1:                                         \
-      TARGET[FUN(i)] = SOURCE[i] ; i++ ;          \
-  case 0:                                         \
-  default:                                        \
-      {}                                          \
+#define RCPP_LOOP_UNROLL_LHSFUN(TARGET,FUN,SOURCE)         \
+R_xlen_t __trip_count = n >> 2 ;                           \
+R_xlen_t i = 0 ;                                           \
+for ( ; __trip_count > 0 ; --__trip_count) {               \
+    TARGET[FUN(i)] = SOURCE[i] ; i++ ;                     \
+    TARGET[FUN(i)] = SOURCE[i] ; i++ ;                     \
+    TARGET[FUN(i)] = SOURCE[i] ; i++ ;                     \
+    TARGET[FUN(i)] = SOURCE[i] ; i++ ;                     \
+}                                                          \
+switch (n - i){                                            \
+  case 3:                                                  \
+      TARGET[FUN(i)] = SOURCE[i] ; i++ ; /* fallthrough */ \
+  case 2:                                                  \
+      TARGET[FUN(i)] = SOURCE[i] ; i++ ; /* fallthrough */ \
+  case 1:                                                  \
+      TARGET[FUN(i)] = SOURCE[i] ; i++ ; /* fallthrough */ \
+  case 0:                                                  \
+  default:                                                 \
+      {}                                                   \
 }
 
 #endif
