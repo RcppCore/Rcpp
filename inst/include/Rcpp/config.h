7ad7dc11c8cbd088737098a50ba4ecfab7be798a
Dirk Eddelbuettel <edd@debian.org>
2019-03-17 08:28:32 -0500

    release 1.0.1

--- inst/include/Rcpp/config.h
+++ inst/include/Rcpp/config.h
@@ -26,9 +26,9 @@
 #define RcppDevVersion(maj, min, rev, dev)  (((maj)*1000000) + ((min)*10000) + ((rev)*100) + (dev))
 
 // the currently released version
-#define RCPP_VERSION Rcpp_Version(1,0,0)
+#define RCPP_VERSION Rcpp_Version(1,0,1)
 
 // the current source snapshot
-#define RCPP_DEV_VERSION RcppDevVersion(1,0,0,0)
+#define RCPP_DEV_VERSION RcppDevVersion(1,0,1,0)
 
 #endif
