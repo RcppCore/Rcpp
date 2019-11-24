2e18a7f0356a0876eb10571c88f295644a846475
Dirk Eddelbuettel <edd@debian.org>
2019-11-24 08:36:51 -0600

    tinytest step three: convert binary package and client package
    skipping both for now for side effects, and binary package input is stale

---
+++ inst/tinytest/test_attributes.R
@@ -0,0 +1,42 @@
+#!/usr/bin/env r
+# -*- mode: R; tab-width: 4; -*-
+#
+# Copyright (C) 2019  Dirk Eddelbuettel, Romain Francois, and Kevin Ushey
+#
+# This file is part of Rcpp.
+#
+# Rcpp is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Rcpp is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
+.runThisTest <- Sys.getenv("RunAllRcppTests") == "yes"
+
+if (.runThisTest) exit_file("Skipping 'test_attributes.R'")
+
+library(Rcpp)
+sourceCpp("cpp/attributes.cpp")
+
+#test.attributes <- function() {
+expect_equal(comments_test(),
+            "Start a C++ line comment with the characters \"//\"")
+
+expect_equal(parse_declaration_test(),
+            "Parse function declaration")
+
+expect_equal(parse_default_values_with_str_parenthesis(),
+            "Parse function header with parenthis inside default string values.")
+
+expect_equal(parse_default_values_with_chr_parenthesis(),
+            "Parse function header with parenthis inside default char values.")
+
+expect_equal(parse_default_values_with_chr_backslash(),
+            "Parse function header with backslash inside default char values.")
