24f8d0575777bf15fa770db8c4608af98e6755d1
Dirk Eddelbuettel <edd@debian.org>
2020-04-25 00:14:15 -0500

    initialize via no_init(n)

--- inst/include/Rcpp/sugar/functions/sample.h
+++ inst/include/Rcpp/sugar/functions/sample.h
@@ -348,7 +348,7 @@ inline Vector<INTSXP> EmpiricalSample(int n, int size, bool replace, bool one_ba
         return ans;
     }
 
-    IntegerVector x(n);
+    IntegerVector x = no_init(n);
     for (int i = 0; i < n; i++) {
         x[i] = i;
     }
@@ -378,7 +378,7 @@ inline Vector<RTYPE> EmpiricalSample(int size, bool replace, const Vector<RTYPE>
         return ans;
     }
 
-    IntegerVector x(n);
+    IntegerVector x = no_init(n);
     for (int i = 0; i < n; i++) {
         x[i] = i;
     }
