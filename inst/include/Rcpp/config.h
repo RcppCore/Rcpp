b1254701a899cb187f9a6917cb8d18c7f93290c7
Dirk Eddelbuettel <edd@debian.org>
2020-05-18 07:14:30 -0500

    Roll minor release, update NEWS

--- inst/include/Rcpp/config.h
+++ inst/include/Rcpp/config.h
@@ -30,7 +30,7 @@
 #define RCPP_VERSION_STRING     "1.0.4"
 
 // the current source snapshot
-#define RCPP_DEV_VERSION        RcppDevVersion(1,0,4,10)
-#define RCPP_DEV_VERSION_STRING "1.0.4.10"
+#define RCPP_DEV_VERSION        RcppDevVersion(1,0,4,11)
+#define RCPP_DEV_VERSION_STRING "1.0.4.11"
 
 #endif
