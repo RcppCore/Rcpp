ce0cec61c24bff010af3442eeb46e9e38c239500
Dirk Eddelbuettel <edd@debian.org>
2021-01-21 16:50:39 -0600

    roll minor version

--- inst/include/Rcpp/config.h
+++ inst/include/Rcpp/config.h
@@ -30,7 +30,7 @@
 #define RCPP_VERSION_STRING     "1.0.6"
 
 // the current source snapshot
-#define RCPP_DEV_VERSION        RcppDevVersion(1,0,6,3)
-#define RCPP_DEV_VERSION_STRING "1.0.6.3"
+#define RCPP_DEV_VERSION        RcppDevVersion(1,0,6,4)
+#define RCPP_DEV_VERSION_STRING "1.0.6.4"
 
 #endif
