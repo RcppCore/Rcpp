0196930168e3334df59dec7974d2a558a35c2ba2
Dirk Eddelbuettel <edd@debian.org>
2022-01-10 19:50:26 -0600

    updated references

--- vignettes/rmd/getCurrentVersionsOfCitedPackages.R
+++ vignettes/rmd/getCurrentVersionsOfCitedPackages.R
@@ -1,17 +1,17 @@
-#/usr/bin/r
+#!/usr/bin/r
 
-if (!file.exists("Rcpp.bib")) fsetwd("~/git/rcpp/vignettes/rmd")
-cmd <- "grep package= Rcpp.bib | cut -c27- | sed -e 's/\"$//' | sort | uniq"
+stopifnot(`Needs data.table` = requireNamespace("data.table", quietly = TRUE))
+library(data.table)
+
+if (!file.exists("Rcpp.bib")) setwd("~/git/rcpp/vignettes/rmd")
+
+cmd <- "awk '/package=/ { print($5) }' Rcpp.bib | sed -e 's/\"//g' - | awk -F= '{print($2)}' | sort | uniq"
 con <- pipe(cmd)
 pkgs <- readLines(con)
 close(con)
 
-repo <- "https://cloud.r-project.org"
-AP <- available.packages(contrib.url(repo),filter=list())	# available package at CRAN
+pkg <- data.table(Package=pkgs)
 
-res <- as.data.frame(do.call(rbind, lapply(pkgs, function(p) {
-    ind <- which(AP[,"Package"] == p)
-    if (length(ind) > 1) ind <- ind[1]
-    AP[ind, c("Package", "Version")]
-})))
-print(res)
+db <- data.table(tools::CRAN_package_db())
+
+print(db[pkg, on="Package"][, .(Package, Version, Date)])
