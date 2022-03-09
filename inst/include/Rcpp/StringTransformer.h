67cf65c443c3dc3631ce6a6e2df057e443159c88
Dirk Eddelbuettel <edd@debian.org>
2022-03-09 15:15:33 -0600

    replace std::(unary|binary)_function with std::function if >= C++11

--- inst/include/Rcpp/StringTransformer.h
+++ inst/include/Rcpp/StringTransformer.h
@@ -1,8 +1,7 @@
-// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 8 -*-
-//
+
 // clone.h: Rcpp R/C++ interface class library -- clone RObject's
 //
-// Copyright (C) 2010 - 2011 Dirk Eddelbuettel and Romain Francois
+// Copyright (C) 2010 - 2022 Dirk Eddelbuettel and Romain Francois
 //
 // This file is part of Rcpp.
 //
@@ -27,25 +26,29 @@
 namespace Rcpp{
 
 	template <typename UnaryOperator>
-	class StringTransformer : public std::unary_function<const char*, const char*>{
+#if __cplusplus < 201103L
+	    class StringTransformer : public std::unary_function<const char*, const char*> {
+#else
+        class StringTransformer : public std::function<const char*(const char*)> {
+#endif
 		public:
 		StringTransformer( const UnaryOperator& op_ ): op(op_), buffer(){}
 		~StringTransformer(){}
 
 		const char* operator()(const char* input ) {
-			buffer = input ;
-			std::transform( buffer.begin(), buffer.end(), buffer.begin(), op ) ;
-			return buffer.c_str() ;
+			buffer = input;
+			std::transform( buffer.begin(), buffer.end(), buffer.begin(), op );
+			return buffer.c_str();
 		}
 
 	private:
-		const UnaryOperator& op ;
-		std::string buffer ;
-	} ;
+		const UnaryOperator& op;
+		std::string buffer;
+	};
 
 	template <typename UnaryOperator>
 	StringTransformer<UnaryOperator> make_string_transformer( const UnaryOperator& fun){
-		return StringTransformer<UnaryOperator>( fun ) ;
+		return StringTransformer<UnaryOperator>( fun );
 	}
 
 }
