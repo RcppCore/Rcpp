9b2a18fc5fc1f37408e2818f596ba39d524782e9
Kevin Ushey <kevinushey@gmail.com>
2022-10-27 09:33:53 -0700

    add small unit test for coerce

---
+++ inst/tinytest/test_coerce.R
@@ -0,0 +1,23 @@
+
+##  Copyright (C) 2022  Dirk Eddelbuettel and Kevin Ushey
+##
+##  This file is part of Rcpp.
+##
+##  Rcpp is free software: you can redistribute it and/or modify it
+##  under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 2 of the License, or
+##  (at your option) any later version.
+##
+##  Rcpp is distributed in the hope that it will be useful, but
+##  WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+
+if (Sys.getenv("RunAllRcppTests") != "yes") exit_file("Set 'RunAllRcppTests' to 'yes' to run.")
+
+Rcpp::sourceCpp("cpp/coerce.cpp")
+
+expect_equal(coerce_raw_to_string(0xaa), "aa")
