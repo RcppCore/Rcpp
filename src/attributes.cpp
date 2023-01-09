d55c1edf0a82338b0026c9c452f904949a6f55f4
Dirk Eddelbuettel <edd@debian.org>
2023-01-09 10:56:32 -0600

    Address warnings from g++-12

--- src/attributes.cpp
+++ src/attributes.cpp
@@ -1621,7 +1621,7 @@ namespace attributes {
         // Look for the signature termination ({ or ; not inside quotes)
         // on this line and then subsequent lines if necessary
         std::string signature;
-        for (size_t i = lineNumber; i<lines_.size(); i++) {
+        for (size_t i = lineNumber; i < (size_t)lines_.size(); i++) {
             std::string line;
             line = lines_[i];
             bool insideQuotes = false;
