54ee8fdd91d954ce9458442f3afeeaac3522b337
Iñaki Úcar <iucar@fedoraproject.org>
2023-01-29 18:46:32 +0100

    fix a couple of tests by actually capturing stdout

--- inst/tinytest/test_xptr.R
+++ inst/tinytest/test_xptr.R
@@ -60,11 +60,9 @@ void test() {
 '
 
 writeLines(code, file_path)
-expect_silent(system(cmd), info="check that finalizer is NOT called on exit")
+expect_equal(system(cmd, intern=TRUE), character(0))
 
 if (packageVersion("tinytest") < "1.2.0") exit_file("Skip remainder on older test platform")
 
-if (Sys.getenv("RunVerboseRcppTests") == "yes") {
-    writeLines(c("#define RCPP_USE_FINALIZE_ON_EXIT", code), file_path)
-    expect_stdout(system(cmd), info="check that finalizer is called on exit")
-}
+writeLines(c("#define RCPP_USE_FINALIZE_ON_EXIT", code), file_path)
+expect_equal(system(cmd, intern=TRUE), "custom_finalizer was called")
