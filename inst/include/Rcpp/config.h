5beadc1a8003890cbf9bccde08fac09ad0c54a51
Dirk Eddelbuettel <edd@debian.org>
2025-01-12 10:14:17 -0600

    Release 1.0.14

--- inst/include/Rcpp/config.h
+++ inst/include/Rcpp/config.h
@@ -1,7 +1,7 @@
 
 // config.h: Rcpp R/C++ interface class library -- Rcpp configuration
 //
-// Copyright (C) 2010 - 2024  Dirk Eddelbuettel and Romain Francois
+// Copyright (C) 2010 - 2025  Dirk Eddelbuettel and Romain Francois
 //
 // This file is part of Rcpp.
 //
@@ -26,11 +26,11 @@
 #define RcppDevVersion(maj, min, rev, dev)  (((maj)*1000000) + ((min)*10000) + ((rev)*100) + (dev))
 
 // the currently released version
-#define RCPP_VERSION            Rcpp_Version(1,0,13)
-#define RCPP_VERSION_STRING     "1.0.13"
+#define RCPP_VERSION            Rcpp_Version(1,0,14)
+#define RCPP_VERSION_STRING     "1.0.14"
 
 // the current source snapshot (using four components, if a fifth is used in DESCRIPTION we ignore it)
-#define RCPP_DEV_VERSION        RcppDevVersion(1,0,13,6)
-#define RCPP_DEV_VERSION_STRING "1.0.13.6"
+#define RCPP_DEV_VERSION        RcppDevVersion(1,0,14,0)
+#define RCPP_DEV_VERSION_STRING "1.0.14.0"
 
 #endif
