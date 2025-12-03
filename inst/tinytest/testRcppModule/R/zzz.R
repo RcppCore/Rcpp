b83944e11fa937e65c97a29f06f806a47a4d5592
Dirk Eddelbuettel <edd@debian.org>
2025-12-03 17:01:47 -0600

    Remove deprecated loadRcppModules (closes #1415) (#1416)
    * Remove deprecated loadRcppModules (closes #1415)
    
    * Tweak ChangeLog whitespace following rebase following #1416 merge
    
    * Remove section about deprecated loadRcppModules()
    
    * Remove commented-out mentions of loadRcppModules()

--- inst/tinytest/testRcppModule/R/zzz.R
+++ inst/tinytest/testRcppModule/R/zzz.R
@@ -1,11 +1,6 @@
-#
-#.onLoad <- function(libname, pkgname){
-#    loadRcppModules()
-#}
-
-## For R 2.15.1 and later this also works. Note that calling loadModule() triggers
+## For R 2.15.1 and later the approach shown here works (as opposed to the now removed
+## and long-deprecated `loadRcppModules()`. Note that calling loadModule() triggers
 ## a load action, so this does not have to be placed in .onLoad() or evalqOnLoad().
 loadModule("RcppModuleNumEx", TRUE)
 loadModule("RcppModuleWorld", TRUE)
 loadModule("stdVector", TRUE)
-
