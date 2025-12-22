7d592e30d56c59965d5891862512d4c4d1a3a298
Iñaki Úcar <iucar@fedoraproject.org>
2025-12-22 15:33:51 +0100

    Revert "remove space"
    This reverts commit bb86312ad52d3dff396d287713359fe5cade6404.

--- inst/include/Rcpp/macros/mask.h
+++ inst/include/Rcpp/macros/mask.h
@@ -27,7 +27,7 @@ template<typename... Args>
 void Rf_error(const char *format, Args... args) {
     ::Rf_error(format, args...);
 #ifndef COMPILING_RCPP
-#pragma message "Use of Rf_error() instead of Rcpp::stop(). Calls \
+# pragma message "Use of Rf_error() instead of Rcpp::stop(). Calls \
 to Rf_error() in C++ contexts are unsafe: consider using Rcpp::stop() instead, \
 or define RCPP_NO_MASK_RF_ERROR if this is a false positive. More info:\n\
  - https://github.com/RcppCore/Rcpp/issues/1247\n\
