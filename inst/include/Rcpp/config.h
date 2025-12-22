06d684719d9d823be8c080160ba45df1773a0b5f
Dirk Eddelbuettel <edd@debian.org>
2025-12-22 15:11:45 -0600

    Roll micro version and date

--- inst/include/Rcpp/config.h
+++ inst/include/Rcpp/config.h
@@ -31,7 +31,7 @@
 #define RCPP_VERSION_STRING     "1.1.0"
 
 // the current source snapshot (using four components, if a fifth is used in DESCRIPTION we ignore it)
-#define RCPP_DEV_VERSION        RcppDevVersion(1,1,0,11)
-#define RCPP_DEV_VERSION_STRING "1.1.0.11"
+#define RCPP_DEV_VERSION        RcppDevVersion(1,1,0,12)
+#define RCPP_DEV_VERSION_STRING "1.1.0.12"
 
 #endif
