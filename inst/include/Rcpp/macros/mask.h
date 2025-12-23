f5857fbe5877a39b38fcebda94ad33077391b723
Iñaki Úcar <iucar@fedoraproject.org>
2025-12-23 13:37:27 +0100

    add on switch

--- inst/include/Rcpp/macros/mask.h
+++ inst/include/Rcpp/macros/mask.h
@@ -21,7 +21,7 @@
 #define Rcpp_macros_mask_h
 
 #ifndef RCPP_NO_MASK_RF_ERROR
-/** cannot emit a message, no warning yet
+#ifdef RCPP_MASK_RF_ERROR
 #define Rf_error(...) \
     _Pragma("GCC warning \"Use of Rf_error() instead of Rcpp::stop(). Calls \
 to Rf_error() in C++ contexts are unsafe: consider using Rcpp::stop() instead, \
@@ -29,7 +29,7 @@ or define RCPP_NO_MASK_RF_ERROR if this is a false positive. More info:\n\
  - https://github.com/RcppCore/Rcpp/issues/1247\n\
  - https://github.com/RcppCore/Rcpp/pull/1402\"") \
     Rf_error(__VA_ARGS__)
-**/
+#endif
 #endif
 
 #endif
