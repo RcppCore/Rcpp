894aa301c8497bf6e2a72aff5e6512aaacb4fe52
Dirk Eddelbuettel <edd@debian.org>
2026-04-02 20:15:11 -0500

    Sharpen version comparison with SVN commit reference

--- inst/include/Rcpp/Function.h
+++ inst/include/Rcpp/Function.h
@@ -69,7 +69,7 @@ namespace Rcpp{
         }
 
         Function_Impl(const std::string& name, const std::string& ns) {
-#if R_VERSION < R_Version(4,6,0)
+#if R_VERSION < R_Version(4,6,0) || R_SVN_REVISION < 89746
             Shield<SEXP> env(Rf_findVarInFrame(R_NamespaceRegistry, Rf_install(ns.c_str())));
             if (env == R_UnboundValue)
                 stop("there is no namespace called \"%s\"", ns);
